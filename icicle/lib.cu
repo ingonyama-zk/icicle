ffa82606bc1c1ef0f656482e9597e8212c7c368b
Jeremy Felder <jeremy.felder1@gmail.com>
2023-03-08 15:25:14 +0200

    flattening and condensing

---
+++ icicle/lib.cu
@@ -0,0 +1,5 @@
+#include "./appUtils/msm/msm.cuh"
+#include "./appUtils/ntt/ntt.cuh"
+#include "./appUtils/vector_manipulation/ve_mod_mult.cu"
+#include "./appUtils/vector_manipulation/ve_mod_mult.cuh"
+
