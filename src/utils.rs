96e3e564e8d4f3401c71473455a8353359d85086
Vitalii Hnatyk <vhnatyk@gmail.com>
2023-03-16 21:10:45 +0200

    wrapper update (#35)
    * changes for wrapper
    
    * removed  transmute
    
    * typo fixed
    
    * eq test moved to point

--- src/utils.rs
+++ src/utils.rs
@@ -1,51 +1,3 @@
-use std::{
-    fs::File,
-    io::{Read, Write},
-};
-
-use rand::{rngs::StdRng, RngCore, SeedableRng};
-
-pub(crate) fn get_fixed_limbs<const NUM_LIMBS: usize>(val: &[u32]) -> [u32; NUM_LIMBS] {
-    match val.len() {
-        n if n < NUM_LIMBS => {
-            let mut padded: [u32; NUM_LIMBS] = [0; NUM_LIMBS];
-            padded[..val.len()].copy_from_slice(&val);
-            padded
-        }
-        n if n == NUM_LIMBS => val.try_into().unwrap(),
-        _ => panic!("slice has to much elements"),
-    }
-}
-
-pub(crate) fn get_rng(seed: Option<u64>) -> Box<dyn RngCore> {
-    let rng: Box<dyn RngCore> = match seed {
-        Some(seed) => Box::new(StdRng::seed_from_u64(seed)),
-        None => Box::new(rand::thread_rng()),
-    };
-    rng
-}
-
-pub(crate) fn export_limbs(numbers: &[u32], file_path: &str) {
-    let mut file = File::create(file_path).expect("failed to create file");
-    for &number in numbers {
-        file.write_all(&number.to_le_bytes())
-            .expect("failed to write number to file");
-    }
-}
-
-pub(crate) fn import_limbs(file_path: &str) -> Vec<u32> {
-    let mut file = File::open(file_path).expect("failed to open file");
-    let mut buffer = [0; 4];
-    let mut result = Vec::new();
-    while let Ok(n) = file.read(&mut buffer) {
-        if n == 0 {
-            break;
-        }
-        result.push(u32::from_le_bytes(buffer));
-    }
-    result
-}
-
 pub fn from_limbs<T>(limbs: Vec<u32>, chunk_size: usize, f: fn(&[u32]) -> T) -> Vec<T> {
     let points = limbs
         .chunks(chunk_size)
@@ -54,112 +6,74 @@ pub fn from_limbs<T>(limbs: Vec<u32>, chunk_size: usize, f: fn(&[u32]) -> T) -> 
     points
 }
 
-fn hex_str_le_to_u32_vec(hex_str: &str, expected_lenght: usize) -> Vec<u32> {
-    let hex_str = hex_str.trim_start_matches("0x");
-    let rem = hex_str.len() % (8 * expected_lenght);
-    let r = if rem == 0 {
-        0
-    } else {
-        8 * expected_lenght - rem
-    };
-    let hex_str_padded = format!("{}{}", "0".repeat(r), hex_str);
+pub fn u32_vec_to_u64_vec(arr_u32: &[u32]) -> Vec<u64> {
+    let len = (arr_u32.len() / 2) as usize;
+    let mut arr_u64 = vec![0u64; len];
 
-    let ret = hex::decode(hex_str_padded)
-        .unwrap()
-        .chunks(4)
-        .map(|x| u32::from_be_bytes(x.try_into().unwrap()))
-        .collect();
-    ret
-}
-
-pub(crate) fn hex_str_be_to_u32_vec(hex_str: &str, limbsize: usize) -> Vec<u32> {
-    let mut ret: Vec<u32> = hex_str_le_to_u32_vec(hex_str, limbsize);
-    ret.reverse();
-    ret
-}
-
-pub fn csv_to_u32_limbs(csv_path: &str, limbsize: usize) -> Vec<u32> {
-    let mut hex_strings = Vec::new();
-    let file = File::open(csv_path).unwrap();
-    let reader = std::io::BufReader::new(file);
-    for line in std::io::BufRead::lines(reader) {
-        let row = line.unwrap();
-        for cell in row.split(',') {
-            hex_strings.push(cell.trim().to_string());
-        }
+    for i in 0..len {
+        arr_u64[i] = u64::from(arr_u32[i * 2]) | (u64::from(arr_u32[i * 2 + 1]) << 32);
     }
-    let u32_numbers: Vec<u32> = hex_strings
-        .iter()
-        .map(|s| hex_str_be_to_u32_vec(s, limbsize))
-        .flatten()
-        .collect();
 
-    return u32_numbers;
+    arr_u64
 }
 
-fn reverse_bit_order(n: u32, order: u32) -> u32 {
-    fn is_power_of_two(n: u32) -> bool {
-        n != 0 && n & (n - 1) == 0
+pub fn u64_vec_to_u32_vec(arr_u64: &[u64]) -> Vec<u32> {
+    let len = arr_u64.len() * 2;
+    let mut arr_u32 = vec![0u32; len];
+
+    for i in 0..arr_u64.len() {
+        arr_u32[i * 2] = arr_u64[i] as u32;
+        arr_u32[i * 2 + 1] = (arr_u64[i] >> 32) as u32;
     }
-    assert!(is_power_of_two(order));
-    let mask = order - 1;
-    let binary = format!(
-        "{:0width$b}",
-        n,
-        width = (32 - mask.leading_zeros()) as usize
-    );
-    let reversed = binary.chars().rev().collect::<String>();
-    u32::from_str_radix(&reversed, 2).unwrap()
-}
 
-pub fn list_to_reverse_bit_order<T: Copy>(l: &[T]) -> Vec<T> {
-    l.iter()
-        .enumerate()
-        .map(|(i, _)| l[reverse_bit_order(i as u32, l.len() as u32) as usize])
-        .collect()
+    arr_u32
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::field::SCALAR_LIMBS;
+    use ark_ff::BigInteger256;
+
+    use crate::field::ScalarField;
 
     use super::*;
 
     #[test]
-    fn test_hex_str_to_u32_vec() {
-        let hex_str = "32add98ba660b4dd9fcf0a899fc68273abe5fecc0b16f1240df1e93c8c84d";
-        let expected_u32_vec = vec![
-            0x00032add, 0x98ba660b, 0x4dd9fcf0, 0xa899fc68, 0x273abe5f, 0xecc0b16f, 0x1240df1e,
-            0x93c8c84d,
-        ];
-        let u32_vec = hex_str_le_to_u32_vec(hex_str, SCALAR_LIMBS);
-        assert_eq!(u32_vec, expected_u32_vec);
-        let hex_str = "0x12039d72be179e9cf6d56ce4a99bd4e8b8ff59367c57bf66f094d4143f7d4ade";
-        let expected_u32_vec = vec![
-            0x12039d72, 0xbe179e9c, 0xf6d56ce4, 0xa99bd4e8, 0xb8ff5936, 0x7c57bf66, 0xf094d414,
-            0x3f7d4ade,
-        ];
-        let u32_vec = hex_str_le_to_u32_vec(hex_str, SCALAR_LIMBS);
-        assert_eq!(u32_vec, expected_u32_vec);
-    }
+    fn test_u32_vec_to_u64_vec() {
+        let arr_u32 = [1, 0x0fffffff, 3, 0x2fffffff, 5, 0x4fffffff, 7, 0x6fffffff];
 
-    #[test]
-    fn test_reverse_bit_order() {
-        assert_eq!(reverse_bit_order(5, 32), 20);
-        assert_eq!(reverse_bit_order(7, 64), 56);
-        assert_eq!(reverse_bit_order(177, 512), 282);
-    }
+        let s = ScalarField::from_ark_transmute(BigInteger256::new(
+            u32_vec_to_u64_vec(&arr_u32).try_into().unwrap(),
+        ))
+        .limbs();
 
-    #[test]
-    fn test_list_to_reverse_bit_order() {
-        assert_eq!(list_to_reverse_bit_order(&[0, 1, 2, 3]), &[0, 2, 1, 3]);
-        let ll: Vec<u32> = (0..32).collect();
+        assert_eq!(arr_u32, s);
+
+        let arr_u64_expected = [
+            0x0FFFFFFF00000001,
+            0x2FFFFFFF00000003,
+            0x4FFFFFFF00000005,
+            0x6FFFFFFF00000007,
+        ];
+
         assert_eq!(
-            list_to_reverse_bit_order(&ll),
-            &[
-                0, 16, 8, 24, 4, 20, 12, 28, 2, 18, 10, 26, 6, 22, 14, 30, 1, 17, 9, 25, 5, 21, 13,
-                29, 3, 19, 11, 27, 7, 23, 15, 31
-            ]
+            u32_vec_to_u64_vec(&arr_u32),
+            arr_u64_expected,
+            "{:016X?}",
+            u32_vec_to_u64_vec(&arr_u32)
+        );
+    }
+
+    #[test]
+    fn test_u64_vec_to_u32_vec() {
+        let arr_u64 = [0x2FFFFFFF00000001, 0x4FFFFFFF00000003, 0x6FFFFFFF00000005, 0x8FFFFFFF00000007];
+
+        let arr_u32_expected = [1, 0x2fffffff, 3, 0x4fffffff, 5, 0x6fffffff, 7, 0x8fffffff];
+
+        assert_eq!(
+            u64_vec_to_u32_vec(&arr_u64),
+            arr_u32_expected,
+            "{:016X?}",
+            u64_vec_to_u32_vec(&arr_u64)
         );
     }
 }
