7ec3232d70f3bd2ef6ed843bdedc5447387e3d5a
Vitalii <vitalii@ingonyama.com>
2023-05-31 15:55:39 +0200

    revert with fix Revert "Revert "fix compilation - rust bench (#80)""
    This reverts commit 485ed4bfe84bb2e074df71c41d4861036ba065ae.

--- benches/ntt.rs
+++ benches/ntt.rs
@@ -2,7 +2,7 @@ extern crate criterion;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use icicle_utils::{interpolate_scalars_batch, interpolate_points_batch, set_up_scalars, set_up_points};
+use icicle_utils::test_bls12_381::{interpolate_scalars_batch_bls12_381, interpolate_points_batch_bls12_381, set_up_scalars_bls12_381, set_up_points_bls12_381};
 
 
 const LOG_NTT_SIZES: [usize; 1] = [15];
@@ -13,17 +13,17 @@ fn bench_ntt(c: &mut Criterion) {
     for log_ntt_size in LOG_NTT_SIZES {
         for batch_size in BATCH_SIZES {
             let ntt_size = 1 << log_ntt_size;
-            let (_, mut d_evals, mut d_domain) = set_up_scalars(ntt_size * batch_size, log_ntt_size, true);
-            let (_, mut d_points_evals, _) = set_up_points(ntt_size * batch_size, log_ntt_size, true);
+            let (_, mut d_evals, mut d_domain) = set_up_scalars_bls12_381(ntt_size * batch_size, log_ntt_size, true);
+            let (_, mut d_points_evals, _) = set_up_points_bls12_381(ntt_size * batch_size, log_ntt_size, true);
 
             group.sample_size(100).bench_function(
                 &format!("Scalar NTT of size 2^{} in batch {}", log_ntt_size, batch_size),
-                |b| b.iter(|| interpolate_scalars_batch(&mut d_evals, &mut d_domain, batch_size))
+                |b| b.iter(|| interpolate_scalars_batch_bls12_381(&mut d_evals, &mut d_domain, batch_size))
             );
 
             group.sample_size(10).bench_function(
                 &format!("EC NTT of size 2^{} in batch {}", log_ntt_size, batch_size),
-                |b| b.iter(|| interpolate_points_batch(&mut d_points_evals, &mut d_domain, batch_size))
+                |b| b.iter(|| interpolate_points_batch_bls12_381(&mut d_points_evals, &mut d_domain, batch_size))
             );
         }
     }
