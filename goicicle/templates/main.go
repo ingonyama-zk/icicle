337906bf2d59f873f994f91a3628707fc68219d8
ImmanuelSegol <3ditds@gmail.com>
2023-07-20 05:53:14 +0300

    update generate

--- goicicle/templates/main.go
+++ goicicle/templates/main.go
@@ -181,6 +181,40 @@ func genTestFiles() {
 	assertNoError(bgen.Generate(config.BLS_12_377, config.BLS_12_377.PackageName, "./curves/", bls12377_entries_g2_test...))
 	assertNoError(bgen.Generate(config.BN_254, config.BN_254.PackageName, "./curves/", bn254_entries_g2_test...))
 	assertNoError(bgen.Generate(config.BLS_12_381, config.BLS_12_381.PackageName, "./curves/", bls12381_entries_g2_test...))
+
+	// MSM TEST
+	bn254_entries_msm_test := []bavard.Entry{
+		{File: filepath.Join(baseDir, "bn254", "msm_test.go"), Templates: []string{"msm_test.go.impl", "imports.go.tmpl"}},
+	}
+
+	bls12377_entries_msm_test := []bavard.Entry{
+		{File: filepath.Join(baseDir, "bls12377", "msm_test.go"), Templates: []string{"msm_test.go.impl", "imports.go.tmpl"}},
+	}
+
+	bls12381_entries_msm_test := []bavard.Entry{
+		{File: filepath.Join(baseDir, "bls12381", "msm_test.go"), Templates: []string{"msm_test.go.impl", "imports.go.tmpl"}},
+	}
+
+	assertNoError(bgen.Generate(config.BLS_12_377, config.BLS_12_377.PackageName, "./curves/", bls12377_entries_msm_test...))
+	assertNoError(bgen.Generate(config.BN_254, config.BN_254.PackageName, "./curves/", bn254_entries_msm_test...))
+	assertNoError(bgen.Generate(config.BLS_12_381, config.BLS_12_381.PackageName, "./curves/", bls12381_entries_msm_test...))
+
+	// FFT TEST
+	bn254_entries_fft_test := []bavard.Entry{
+		{File: filepath.Join(baseDir, "bn254", "ntt_test.go"), Templates: []string{"ntt_test.go.impl", "imports.go.tmpl"}},
+	}
+
+	bls12377_entries_fft_test := []bavard.Entry{
+		{File: filepath.Join(baseDir, "bls12377", "ntt_test.go"), Templates: []string{"ntt_test.go.impl", "imports.go.tmpl"}},
+	}
+
+	bls12381_entries_fft_test := []bavard.Entry{
+		{File: filepath.Join(baseDir, "bls12381", "ntt_test.go"), Templates: []string{"ntt_test.go.impl", "imports.go.tmpl"}},
+	}
+
+	assertNoError(bgen.Generate(config.BLS_12_377, config.BLS_12_377.PackageName, "./curves/", bls12377_entries_fft_test...))
+	assertNoError(bgen.Generate(config.BN_254, config.BN_254.PackageName, "./curves/", bn254_entries_fft_test...))
+	assertNoError(bgen.Generate(config.BLS_12_381, config.BLS_12_381.PackageName, "./curves/", bls12381_entries_fft_test...))
 }
 
 func main() {
