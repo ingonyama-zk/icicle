2387086f4376372eb4f86b683697e094773b2d7d
stas <spolonsky@icloud.com>
2023-10-09 10:11:25 -0400

    TEST and PRODUCTION modes for testing CUDA errors

--- icicle/utils/error_handler.cuh
+++ icicle/utils/error_handler.cuh
@@ -30,3 +30,22 @@ void syncDevice(const char* const file, const int line)
     std::cerr << cudaGetErrorString(err) << std::endl;
   }
 }
+
+// Define ERROR_MODE
+#define TEST 1
+#define PRODUCTION 2
+
+#define ERROR_MODE TEST // Set to either TEST or PRODUCTION
+
+// Define CHECK_TEST and CHECK_PRODUCTION macros
+#define CHECK_TEST CHECK_SYNC_DEVICE_ERROR
+#define CHECK_PRODUCTION CHECK_LAST_CUDA_ERROR
+
+// Define CHECK based on MODE
+#if ERROR_MODE == TEST
+    #define CHECK_CUDA_ERROR CHECK_TEST
+#elif ERROR_MODE == PRODUCTION
+    #define CHECK_CUDA_ERROR CHECK_PRODUCTION
+#else
+    #error "Invalid ERROR_MODE!"
+#endif
