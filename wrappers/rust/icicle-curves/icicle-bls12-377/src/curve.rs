a7eb7ac0fe00ef2fc88f568d9962bf289c486b68
ChickenLover <romangg81@gmail.com>
2023-12-14 18:35:29 +0700

    add bls12-377

---
+++ wrappers/rust/icicle-curves/icicle-bls12-377/src/curve.rs
@@ -0,0 +1,22 @@
+#[cfg(feature = "arkworks")]
+use ark_bls12_377::{g1::Config as ArkG1Config, Fq, Fr};
+use icicle_core::curve::{Affine, CurveConfig, Projective};
+use icicle_core::field::{Field, FieldConfig};
+use std::ffi::{c_uint, c_void};
+
+impl_curve!(8, 12,);
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        generate_random_affine_points, generate_random_projective_points, generate_random_scalars, BaseField, G1Affine,
+        G1Projective, ScalarField, BASE_LIMBS,
+    };
+    use icicle_core::{impl_curve_ark_tests, impl_curve_tests, traits::ArkConvertible};
+
+    use ark_bls12_377::G1Affine as ArkG1Affine;
+
+    impl_curve_tests!();
+
+    impl_curve_ark_tests!();
+}
---
+++ wrappers/rust/icicle-curves/icicle-bls12-377/src/msm/mod.rs
@@ -0,0 +1,23 @@
+use crate::curve::{CurveCfg, G1Affine, G1Projective, ScalarField};
+use icicle_core::{
+    curve::CurveConfig,
+    impl_msm,
+    msm::{MSMConfig, MSM},
+};
+use icicle_cuda_runtime::error::{CudaError, CudaResult, CudaResultWrap};
+
+impl_msm!("bls12_377", CurveCfg);
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use ark_bls12_377::G1Projective as ArkG1Projective;
+    use ark_ec::scalar_mul::variable_base::VariableBaseMSM;
+    use icicle_core::{curve::CurveConfig, impl_msm_tests, msm::MSM};
+
+    use crate::curve::{generate_random_affine_points, generate_random_scalars, CurveCfg};
+    use icicle_core::traits::ArkConvertible;
+    use icicle_cuda_runtime::memory::DeviceSlice;
+    use icicle_cuda_runtime::stream::CudaStream;
+
+    impl_msm_tests!(CurveCfg);
+}
