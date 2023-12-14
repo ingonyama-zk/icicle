54a1f808f1b08341d4b675738895a5d65ce7d16a
ChickenLover <romangg81@gmail.com>
2023-12-14 17:31:18 +0700

    add bls12_381 rust

---
+++ wrappers/rust/icicle-curves/icicle-bls12-381/src/ntt/mod.rs
@@ -0,0 +1,28 @@
+use crate::curve::{CurveCfg, ScalarField};
+
+use icicle_core::curve::CurveConfig;
+use icicle_core::impl_ntt;
+use icicle_core::ntt::{NTTConfig, NTT};
+use icicle_cuda_runtime::device_context::DeviceContext;
+use icicle_cuda_runtime::error::{CudaError, CudaResult, CudaResultWrap};
+
+impl_ntt!("bls12_381", CurveCfg);
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use icicle_core::traits::ArkConvertible;
+    use icicle_core::{
+        curve::CurveConfig,
+        impl_ntt_tests,
+        ntt::{Ordering, NTT},
+    };
+    use icicle_cuda_runtime::device_context::get_default_device_context;
+
+    use crate::curve::{generate_random_scalars, CurveCfg};
+
+    use ark_bls12_381::Fr;
+    use ark_ff::FftField;
+    use ark_poly::{EvaluationDomain, GeneralEvaluationDomain};
+
+    impl_ntt_tests!(CurveCfg);
+}
