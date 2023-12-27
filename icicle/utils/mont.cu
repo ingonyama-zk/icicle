4c1442358487a9b0029c03a03fa050080cc3a59b
ChickenLover <romangg81@gmail.com>
2023-12-28 00:41:29 +0700

    add scalars montgomery conversions API

---
+++ icicle/utils/mont.cu
@@ -0,0 +1,35 @@
+#include "../curves/curve_config.cuh"
+#include "device_context.cuh"
+#include "mont.cuh"
+
+namespace mont {
+  extern "C" int ScalarConvertMontgomery(
+    curve_config::scalar_t* d_inout, size_t n, unsigned char is_into, device_context::DeviceContext& ctx)
+  {
+    if (is_into) {
+      return ToMontgomery(d_inout, n, ctx.stream);
+    } else {
+      return FromMontgomery(d_inout, n, ctx.stream);
+    }
+  }
+
+  extern "C" int AffineConvertMontgomery(
+    curve_config::affine_t* d_inout, size_t n, unsigned char is_into, device_context::DeviceContext& ctx)
+  {
+    if (is_into) {
+      return ToMontgomery(d_inout, n, ctx.stream);
+    } else {
+      return FromMontgomery(d_inout, n, ctx.stream);
+    }
+  }
+
+  extern "C" int ProjectiveConvertMontgomery(
+    curve_config::projective_t* d_inout, size_t n, unsigned char is_into, device_context::DeviceContext& ctx)
+  {
+    if (is_into) {
+      return ToMontgomery(d_inout, n, ctx.stream);
+    } else {
+      return FromMontgomery(d_inout, n, ctx.stream);
+    }
+  }
+} // namespace mont
