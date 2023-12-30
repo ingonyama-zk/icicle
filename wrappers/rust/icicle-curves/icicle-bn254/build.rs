d05f8ec80baec074b678cdbe5090800604041ba0
DmytroTym <dmytrotym1@gmail.com>
2023-12-31 01:14:16 +0200

    Fixing build scripts

--- wrappers/rust/icicle-curves/icicle-bn254/build.rs
+++ wrappers/rust/icicle-curves/icicle-bn254/build.rs
@@ -1,12 +1,8 @@
 use cmake::Config;
-use std::env::var;
 
 fn main() {
     println!("cargo:rerun-if-env-changed=CXXFLAGS");
     println!("cargo:rerun-if-changed=../../../../icicle");
-
-    let cargo_dir = var("CARGO_MANIFEST_DIR").unwrap();
-    let profile = var("PROFILE").unwrap();
 
     let out_dir = Config::new("../../../../icicle")
                 .define("BUILD_TESTS", "OFF") //TODO: feature
