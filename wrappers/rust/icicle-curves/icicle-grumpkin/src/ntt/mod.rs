b47e65e1ab15c5e10cea055985873726088ae18e
nonam3e <lendedland@gmail.com>
2024-01-22 14:23:51 +0000

    grumpkin init

---
+++ wrappers/rust/icicle-curves/icicle-grumpkin/src/ntt/mod.rs
@@ -0,0 +1,21 @@
+use crate::curve::{ScalarCfg, ScalarField};
+
+use icicle_core::error::IcicleResult;
+use icicle_core::impl_ntt;
+use icicle_core::ntt::{NTTConfig, NTTDir, NTT};
+use icicle_core::traits::IcicleResultWrap;
+use icicle_cuda_runtime::device_context::DeviceContext;
+use icicle_cuda_runtime::error::CudaError;
+use icicle_cuda_runtime::memory::HostOrDeviceSlice;
+
+impl_ntt!("grumpkin", ScalarField, ScalarCfg);
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use crate::curve::ScalarField;
+    use icicle_core::impl_ntt_tests;
+    use icicle_core::ntt::tests::*;
+    use std::sync::OnceLock;
+
+    impl_ntt_tests!(ScalarField);
+}
