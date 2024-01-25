94f2d702ea8c5a81e1582b6807d3f2e0204995c0
Yuval Shekel <yshekel@gmail.com>
2024-01-25 18:11:18 +0200

    large kernel verification: avoid managed memory

--- icicle/appUtils/large_ntt/verification.cu
+++ icicle/appUtils/large_ntt/verification.cu
@@ -1,5 +1,5 @@
 
-#define CURVE_ID 2 // TODO Yuval: move to makefile
+#define CURVE_ID 1 // TODO Yuval: move to makefile
 
 #include "../../primitives/field.cuh"
 #include "../../primitives/projective.cuh"
@@ -11,8 +11,9 @@
 #include "curves/curve_config.cuh"
 #include "ntt/ntt.cu"
 #include "large_ntt/large_ntt.cuh"
+#include <memory>
 
-// #define PERFORMANCE
+#define PERFORMANCE
 
 typedef curve_config::scalar_t test_scalar;
 #include "kernel_ntt.cu"
@@ -42,9 +43,9 @@ int main()
   float icicle_time, new_time;
 #endif
 
-  int NTT_LOG_SIZE = 4;
+  int NTT_LOG_SIZE = 21;
   int NTT_SIZE = 1 << NTT_LOG_SIZE;
-  int INV = true;
+  int INV = false;
   const ntt::Ordering ordering = ntt::Ordering::kNN;
   const char* ordering_str = ordering == ntt::Ordering::kNN   ? "NN"
                              : ordering == ntt::Ordering::kNR ? "NR"
@@ -54,23 +55,22 @@ int main()
   printf("running ntt 2^%d, INV=%d, ordering=%s\n", NTT_LOG_SIZE, INV, ordering_str);
 
   // cpu allocation
-  test_scalar* cpuScalars = (test_scalar*)malloc(sizeof(test_scalar) * NTT_SIZE);
-  if (cpuScalars == NULL) {
-    fprintf(stderr, "Malloc failed\n");
-    exit(1);
-  }
+  auto CpuScalars = std::make_unique<test_scalar[]>(NTT_SIZE);
+  auto CpuOutputOld = std::make_unique<test_scalar[]>(NTT_SIZE);
+  auto CpuOutputNew = std::make_unique<test_scalar[]>(NTT_SIZE);
 
   // gpu allocation
-  test_scalar *gpuScalars, *GpuOutputOld, *GpuOutputNew;
-  $CUDA(cudaMallocManaged((void**)&gpuScalars, sizeof(test_scalar) * NTT_SIZE));
-  $CUDA(cudaMallocManaged((void**)&GpuOutputOld, sizeof(test_scalar) * NTT_SIZE));
-  $CUDA(cudaMallocManaged((void**)&GpuOutputNew, sizeof(test_scalar) * NTT_SIZE));
+  test_scalar *GpuScalars, *GpuOutputOld, *GpuOutputNew;
+  $CUDA(cudaMalloc(&GpuScalars, sizeof(test_scalar) * NTT_SIZE));
+  $CUDA(cudaMalloc(&GpuOutputOld, sizeof(test_scalar) * NTT_SIZE));
+  $CUDA(cudaMalloc(&GpuOutputNew, sizeof(test_scalar) * NTT_SIZE));
 
   // init inputs
-  random_samples(gpuScalars, NTT_SIZE);
+  random_samples(CpuScalars.get(), NTT_SIZE);
+  $CUDA(cudaMemcpy(GpuScalars, CpuScalars.get(), NTT_SIZE, cudaMemcpyHostToDevice));
 
   // new algorithm init
-  ntt::MixedRadixNTT new_ntt(NTT_SIZE, INV, ordering);
+  // ntt::MixedRadixNTT new_ntt(NTT_SIZE, INV, ordering);
   // old algorithm init
   auto ntt_config = ntt::DefaultNTTConfig<test_scalar>();
   ntt_config.ordering = ordering;
@@ -86,50 +86,68 @@ int main()
   $CUDA(cudaEventCreate(&new_start));
   $CUDA(cudaEventCreate(&new_stop));
 
-  // run ntts
-  int count = 100;
-  $CUDA(cudaEventRecord(new_start, 0));
-  for (size_t i = 0; i < count; i++) {
-    new_ntt(gpuScalars, GpuOutputNew);
-  }
-  $CUDA(cudaEventRecord(new_stop, 0));
-  $CUDA(cudaDeviceSynchronize());
-  $CUDA(cudaEventElapsedTime(&new_time, new_start, new_stop));
-  cudaDeviceSynchronize();
-  printf("cuda err %d\n", cudaGetLastError());
+  // run ntt
+  auto benchmark = [&](bool is_print, int iterations) {
+    $CUDA(cudaEventRecord(new_start, 0));
+    for (size_t i = 0; i < iterations; i++) {
+      // Note: measuring construction/destruction everytime since this is what real usecase is doing
+      ntt::MixedRadixNTT new_ntt(NTT_SIZE, INV, ordering);
+      new_ntt(GpuScalars, GpuOutputNew);
+    }
+    $CUDA(cudaEventRecord(new_stop, 0));
+    $CUDA(cudaDeviceSynchronize());
+    $CUDA(cudaEventElapsedTime(&new_time, new_start, new_stop));
+    cudaDeviceSynchronize();
+    if (is_print) { printf("cuda err %d\n", cudaGetLastError()); }
 
-  $CUDA(cudaEventRecord(icicle_start, 0));
-  for (size_t i = 0; i < count; i++) {
-    ntt::NTT(gpuScalars, NTT_SIZE, INV ? ntt::NTTDir::kInverse : ntt::NTTDir::kForward, ntt_config, GpuOutputOld);
-  }
-  $CUDA(cudaEventRecord(icicle_stop, 0));
-  $CUDA(cudaDeviceSynchronize());
-  $CUDA(cudaEventElapsedTime(&icicle_time, icicle_start, icicle_stop));
-  cudaDeviceSynchronize();
-  printf("cuda err %d\n", cudaGetLastError());
-  fprintf(stderr, "Old Runtime=%0.3f MS\n", icicle_time / count);
-  fprintf(stderr, "New Runtime=%0.3f MS\n", new_time / count);
+    $CUDA(cudaEventRecord(icicle_start, 0));
+    for (size_t i = 0; i < iterations; i++) {
+      ntt::NTT(GpuScalars, NTT_SIZE, INV ? ntt::NTTDir::kInverse : ntt::NTTDir::kForward, ntt_config, GpuOutputOld);
+    }
+    $CUDA(cudaEventRecord(icicle_stop, 0));
+    $CUDA(cudaDeviceSynchronize());
+    $CUDA(cudaEventElapsedTime(&icicle_time, icicle_start, icicle_stop));
+    cudaDeviceSynchronize();
+    if (is_print) {
+      printf("cuda err %d\n", cudaGetLastError());
+      fprintf(stderr, "Old Runtime=%0.3f MS\n", icicle_time / iterations);
+      fprintf(stderr, "New Runtime=%0.3f MS\n", new_time / iterations);
+    }
+  };
+
+  int count = 1;
+  benchmark(false /*=print*/, 1); // warmup - is this applicable to real usecase??
+  benchmark(true /*=print*/, count);
 #else
-  new_ntt(gpuScalars, GpuOutputNew);
+  new_ntt(GpuScalars, GpuOutputNew);
+  cudaDeviceSynchronize();
   printf("finished new\n");
 
-  ntt::NTT(gpuScalars, NTT_SIZE, INV ? ntt::NTTDir::kInverse : ntt::NTTDir::kForward, ntt_config, GpuOutputOld);
+  ntt::NTT(GpuScalars, NTT_SIZE, INV ? ntt::NTTDir::kInverse : ntt::NTTDir::kForward, ntt_config, GpuOutputOld);
   printf("finished old\n");
 
   // verify
+  $CUDA(cudaMemcpy(CpuOutputNew.get(), GpuOutputNew, NTT_SIZE, cudaMemcpyDeviceToHost));
+  $CUDA(cudaMemcpy(CpuOutputOld.get(), GpuOutputOld, NTT_SIZE, cudaMemcpyDeviceToHost));
+
   bool success = true;
   for (int i = 0; i < NTT_SIZE; i++) {
-    if (GpuOutputNew[i] != GpuOutputOld[i]) {
+    if (CpuOutputNew[i] != CpuOutputOld[i]) {
       success = false;
-      std::cout << i << " ref " << GpuOutputOld[i] << " != " << GpuOutputNew[i] << std::endl;
+      std::cout << i << " ref " << CpuOutputOld[i] << " != " << CpuOutputNew[i] << std::endl;
       // break;
     } else {
-      // std::cout << i << " ref " << icicle_temp << " == " << new_temp << std::endl;
+      // std::cout << i << " ref " << CpuOutputOld[i] << " == " << CpuOutputNew[i] << std::endl;
+      // break;
     }
   }
   const char* success_str = success ? "SUCCESS!" : "FAIL!";
   printf("%s\n", success_str);
 #endif
+
+  $CUDA(cudaFree(GpuScalars));
+  $CUDA(cudaFree(GpuOutputOld));
+  $CUDA(cudaFree(GpuOutputNew));
 
   return 0;
 }
