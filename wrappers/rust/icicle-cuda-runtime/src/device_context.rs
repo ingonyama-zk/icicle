08e68e01c75b726b46933eca75d29c394a54a11f
Vitalii <vitalii@ingonyama.com>
2024-01-27 15:21:10 +0100

    minor

--- wrappers/rust/icicle-cuda-runtime/src/device_context.rs
+++ wrappers/rust/icicle-cuda-runtime/src/device_context.rs
@@ -1,17 +1,17 @@
 use crate::memory::CudaMemPool;
 use crate::stream::CudaStream;
 
-/// Properties of the device used in icicle functions.
+/// Properties of the device used in Icicle functions.
 #[repr(C)]
 #[derive(Debug, Clone)]
 pub struct DeviceContext<'a> {
-    /// Stream to use. Default value: 0.
+    /// Stream to use. Default value: 0. //TODO: multiple streams per device ?
     pub stream: &'a CudaStream, // Assuming the type is provided by a CUDA binding crate
 
     /// Index of the currently used GPU. Default value: 0.
     pub device_id: usize,
 
-    /// Mempool to use. Default value: 0.
+    /// Mempool to use. Default value: 0. //TODO: multiple mempools per device ?
     pub mempool: CudaMemPool, // Assuming the type is provided by a CUDA binding crate
 }
 
