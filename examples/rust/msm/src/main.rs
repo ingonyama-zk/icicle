c67d77c95db01849a2fcae5fb316934b1a882397
LeonHibnik <leon@ingonyama.com>
2024-01-30 11:13:59 +0200

    fix ark check

--- examples/rust/msm/src/main.rs
+++ examples/rust/msm/src/main.rs
@@ -152,11 +152,11 @@ fn main() {
         #[cfg(feature = "arkworks")]
         {
             println!("Checking against arkworks...");
-            let ark_points: Vec<Bn254G1Affine> = points.iter().map(|&point| point.to_ark()).collect();
-            let ark_scalars: Vec<Bn254Fr> = scalars.iter().map(|scalar| scalar.to_ark()).collect();
+            let ark_points: Vec<Bn254G1Affine> = points.as_slice().iter().map(|&point| point.to_ark()).collect();
+            let ark_scalars: Vec<Bn254Fr> = scalars.as_slice().iter().map(|scalar| scalar.to_ark()).collect();
 
-            let ark_points_bls12377: Vec<Bls12377G1Affine> = points_bls12377.iter().map(|point| point.to_ark()).collect();
-            let ark_scalars_bls12377: Vec<Bls12377Fr> = scalars_bls12377.iter().map(|scalar| scalar.to_ark()).collect();
+            let ark_points_bls12377: Vec<Bls12377G1Affine> = points_bls12377.as_slice().iter().map(|point| point.to_ark()).collect();
+            let ark_scalars_bls12377: Vec<Bls12377Fr> = scalars_bls12377.as_slice().iter().map(|scalar| scalar.to_ark()).collect();
 
             #[cfg(feature = "profile")]
             let start = Instant::now();
