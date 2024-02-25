f877a80c235c294909bd0b1d32426053e1d82ff4
Yuval Shekel <yshekel@gmail.com>
2024-02-25 13:37:43 +0000

    split backend to context object for state management

--- icicle/polynomials/polynomials.cpp
+++ icicle/polynomials/polynomials.cpp
@@ -7,7 +7,8 @@ namespace polynomials {
   template <typename C, typename D, typename I>
   Polynomial<C, D, I>::Polynomial()
   {
-    // TODO Yuval: how to choose backend
+    // TODO Yuval: how to choose backend and context types
+    m_context = std::make_unique<GPUPolynomialContext<C, D, I>>();
     m_backend = std::make_unique<GPUPolynomialBackend<C, D, I>>();
   }
 
@@ -15,7 +16,7 @@ namespace polynomials {
   Polynomial<C, D, I> Polynomial<C, D, I>::from_coefficients(const C* coefficients, uint32_t nof_coefficients)
   {
     Polynomial P = {};
-    P.m_backend->init_from_coefficients(coefficients, nof_coefficients);
+    P.m_context->init_from_coefficients(nof_coefficients, coefficients);
     return P;
   }
 
@@ -23,7 +24,7 @@ namespace polynomials {
   Polynomial<C, D, I> Polynomial<C, D, I>::from_rou_evaluations(const I* evaluations, uint32_t nof_evaluations)
   {
     Polynomial P = {};
-    P.m_backend->init_from_rou_evaluations(evaluations, nof_evaluations);
+    P.m_backend->init_from_rou_evaluations(nof_evaluations, evaluations);
     return P;
   }
 
@@ -31,7 +32,7 @@ namespace polynomials {
   Polynomial<C, D, I> Polynomial<C, D, I>::operator+(const Polynomial<C, D, I>& rhs) const
   {
     Polynomial<C, D, I> res = {};
-    m_backend->add(res, *this, rhs);
+    m_backend->add(*res.m_context.get(), *m_context.get(), *rhs.m_context);
     return res;
   }
 
@@ -39,7 +40,7 @@ namespace polynomials {
   Polynomial<C, D, I> Polynomial<C, D, I>::operator-(const Polynomial<C, D, I>& rhs) const
   {
     Polynomial<C, D, I> res = {};
-    m_backend->subtract(res, *this, rhs);
+    m_backend->subtract(*res.m_context.get(), *m_context.get(), *rhs.m_context);
     return res;
   }
 
@@ -47,39 +48,39 @@ namespace polynomials {
   std::pair<Polynomial<C, D, I>, Polynomial<C, D, I>> Polynomial<C, D, I>::divide(const Polynomial<C, D, I>& rhs) const
   {
     Polynomial<C, D, I> Q = {}, R = {};
-    m_backend->divide(Q, R, *this, rhs);
+    m_backend->divide(*Q.m_context.get(), *R.m_context.get(), *m_context.get(), *rhs.m_context.get());
     return std::make_pair(Q, R);
   }
 
   template <typename C, typename D, typename I>
   Polynomial<C, D, I>& Polynomial<C, D, I>::add_monomial_inplace(C monomial_coeff, uint32_t monomial) const
   {
-    m_backend->add_monomial_in_place(*this, monomial_coeff, monomial);
+    m_backend->add_monomial_in_place(*m_context.get(), monomial_coeff, monomial);
     return *this;
   }
 
   template <typename C, typename D, typename I>
   I Polynomial<C, D, I>::operator()(const D& x) const
   {
-    return m_backend->evaluate(*this, x);
+    return m_backend->evaluate(*m_context.get(), x);
   }
 
   template <typename C, typename D, typename I>
   int32_t Polynomial<C, D, I>::degree()
   {
-    return m_backend->degree(*this);
+    return m_backend->degree(*m_context.get());
   }
 
   template <typename C, typename D, typename I>
   C Polynomial<C, D, I>::get_coefficient(uint32_t idx) const
   {
-    return m_backend->get_coefficient(*this, idx);
+    return m_backend->get_coefficient(*m_context.get(), idx);
   }
 
   template <typename C, typename D, typename I>
-  void Polynomial<C, D, I>::get_coefficients(C* coeff, uint32_t& nof_coeff) const
+  uint32_t Polynomial<C, D, I>::get_coefficients(C* coeff) const
   {
-    m_backend->get_coefficients(*this, coeff, nof_coeff);
+    return m_backend->get_coefficients(*m_context.get(), coeff);
   }
 
 } // namespace polynomials
