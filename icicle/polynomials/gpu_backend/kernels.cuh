fe816c179c18b1eee91e16367f06fb61f2bb45cc
Yuval Shekel <yshekel@gmail.com>
2024-02-26 15:41:20 +0000

    polynomial evaluation - dummy reduction

--- icicle/polynomials/gpu_backend/kernels.cuh
+++ icicle/polynomials/gpu_backend/kernels.cuh
@@ -3,27 +3,62 @@
 
 namespace polynomials {
 
-  template <typename E>
-  __global__ void AddSubKernel(E* a_vec, E* b_vec, int a_len, int b_len, bool add1_sub0, E* result)
+  /*============================== add/sub ==============================*/
+  template <typename T>
+  __global__ void AddSubKernel(T* a_vec, T* b_vec, int a_len, int b_len, bool add1_sub0, T* result)
   {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    const int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid >= max(a_len, b_len)) return;
 
-    E a = tid >= a_len ? E::zero() : a_vec[tid];
-    E b = tid >= b_len ? E::zero() : b_vec[tid];
+    T a = tid >= a_len ? T::zero() : a_vec[tid];
+    T b = tid >= b_len ? T::zero() : b_vec[tid];
     result[tid] = add1_sub0 ? a + b : a - b;
   }
 
-  // assuming 1 thread. TODO: parallellize
-  template <typename E>
-  __global__ void HighestNonZeroIdx(E* vec, int len, int32_t* idx)
+  /*============================== degree ==============================*/
+  template <typename T>
+  __global__ void HighestNonZeroIdx(T* vec, int len, int32_t* idx)
   {
     *idx = -1;
     for (int i = len - 1; len >= 0; --i) {
-      if (vec[i] != E::zero()) {
+      if (vec[i] != T::zero()) {
         *idx = i;
         return;
       }
     }
   }
+
+  /*============================== evaluate ==============================*/
+  template <typename T>
+  __device__ T pow(T base, int exp)
+  {
+    T result = T::one();
+    while (exp > 0) {
+      if (exp & 1) result = result * base;
+      base = base * base;
+      exp >>= 1;
+    }
+    return result;
+  }
+
+  // TODO Yuval: implement efficient reduction and support batch evaluation
+  template <typename T>
+  __global__ void dummyReduce(T* arr, int size, T* output)
+  {
+    const int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid > 0) return;
+
+    *output = arr[0];
+    for (int i = 1; i < size; ++i) {
+      *output = *output + arr[i];
+    }
+  }
+
+  template <typename T>
+  __global__ void evalutePolynomialWithoutReduction(T x, T* coeffs, int num_coeffs, T* tmp)
+  {
+    const int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid <= num_coeffs) { tmp[tid] = coeffs[tid] * pow(x, tid); }
+  }
+
 } // namespace polynomials
