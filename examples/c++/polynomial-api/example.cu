4d359e7b92d85446ab3643e5f4a9de78d2073ac4
stas <spolonsky@icloud.com>
2024-03-10 15:57:25 -0400

    clean-up

--- examples/c++/polynomial-api/example.cu
+++ examples/c++/polynomial-api/example.cu
@@ -1,10 +1,23 @@
+#include <iostream>
 #define CURVE_ID BN254
 #include "curves/curve_config.cuh"
 #include "polynomials/polynomials.cpp"
+#include "appUtils/ntt/ntt.cuh"
 using namespace curve_config;
 using namespace polynomials;
 
 typedef Polynomial<scalar_t> Polynomial_t;
+
+void example1() {
+  std::cout << "Polynomial evaluation on random value" << std::endl;
+  const scalar_t coeffs[3] = {scalar_t::one(), scalar_t::from(2), scalar_t::from(3)};
+  auto f = Polynomial_t::from_coefficients(coeffs, 3);
+  std::cout << "f = " << f << std::endl;
+  scalar_t x = scalar_t::rand_host();
+  std::cout << "x = " << x << std::endl;
+  auto f_x = f(x); // evaluation
+  std::cout << "f(x) = " << f_x << std::endl;
+}
 
 void example2(int size) {
   std::cout << "Polynomial evaluation on roots of unity" << std::endl;
@@ -25,18 +38,19 @@ void example2(int size) {
     x = x * omega;
   }
   // reconstruct f from evaluations
-  // TODO: I need to initiaqlize NTT to execute the rest of the code
   auto fr = Polynomial_t::from_rou_evaluations(evals, nof_evals);
   // make sure they are equal, that is f-fr=0
   auto h = f - fr;
+  
+
   std::cout << "degree of f - fr = " << h.degree() << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-  const static auto one = scalar_t::one();
-  const static auto two = scalar_t::from(2);
-  const static auto three = scalar_t::from(3);
+  // const static auto one = scalar_t::one();
+  // const static auto two = scalar_t::from(2);
+  // const static auto three = scalar_t::from(3);
   // init NTT domain: TODO: can we hide this in the library?
   static const int MAX_NTT_LOG_SIZE = 24;
   auto ntt_config = ntt::DefaultNTTConfig<scalar_t>();
@@ -46,14 +60,16 @@ int main(int argc, char** argv)
   // initializing polynomimals factory for CUDA backend
   Polynomial_t::initialize(std::make_unique<CUDAPolynomialFactory<>>());
 
-  std::cout << "Polynomial evaluation on random value" << std::endl;
-  const scalar_t coeffs[3] = {one, two, three};
-  auto f = Polynomial_t::from_coefficients(coeffs, 3);
-  std::cout << "f = " << f << std::endl;
-  scalar_t x = scalar_t::rand_host();
-  std::cout << "x = " << x << std::endl;
-  auto f_x = f(x); // evaluation
-  std::cout << "f(x) = " << f_x << std::endl;
+
+  example1();
+  // std::cout << "Polynomial evaluation on random value" << std::endl;
+  // const scalar_t coeffs[3] = {one, two, three};
+  // auto f = Polynomial_t::from_coefficients(coeffs, 3);
+  // std::cout << "f = " << f << std::endl;
+  // scalar_t x = scalar_t::rand_host();
+  // std::cout << "x = " << x << std::endl;
+  // auto f_x = f(x); // evaluation
+  // std::cout << "f(x) = " << f_x << std::endl;
 
   
   example2(100);
