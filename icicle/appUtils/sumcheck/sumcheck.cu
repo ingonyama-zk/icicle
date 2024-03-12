2ffda0ffac6b0ba7982be7e05fe7203be0ff57c5
hadaringonyama <hadar@ingonyama.com>
2024-03-12 15:50:30 +0200

    accumulate function verified

--- icicle/appUtils/sumcheck/sumcheck.cu
+++ icicle/appUtils/sumcheck/sumcheck.cu
@@ -1,19 +1,123 @@
 
 
+#define SHMEM_SIZE 64
+#define MAX_SHMEM_LOG_SIZE 6
+
+template <typename S>
+__global__ void sum_reduction(S *v, S *v_r) {
+	// Allocate shared memory
+	__shared__ S partial_sum[SHMEM_SIZE];
+
+	// Calculate thread ID
+	int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+	// Load elements AND do first add of reduction
+	// Vector now 2x as long as number of threads, so scale i
+	int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
+
+	// Store first partial result instead of just the elements
+	partial_sum[threadIdx.x] = v[i] + v[i + blockDim.x];
+	__syncthreads();
+
+	// Start at 1/2 block stride and divide by two each iteration
+	for (int s = blockDim.x / 2; s > 1; s >>= 1) {
+		// Each thread does work unless it is further than the stride
+		if (threadIdx.x < s) {
+			partial_sum[threadIdx.x] += partial_sum[threadIdx.x + s];
+		}
+		__syncthreads();
+	}
+
+	// Let the thread 0 for this block write it's result to main memory
+	// Result is inexed by this block
+	if (threadIdx.x == 0 || threadIdx.x == 1) {
+		v_r[2*blockIdx.x + threadIdx.x] = partial_sum[threadIdx.x];
+	}
+}
+
+template <typename S>
+__global__ void update_evals_kernel(S* evals, S alpha){
+  int tid = blockIdx.x * blockDim.x + threadIdx.x;
+  // evals[tid] = (S::one() - alpha) * evals[2*tid] + alpha * evals[2*tid+1];
+  evals[tid] = (1 - alpha) * evals[2*tid] + alpha * evals[2*tid+1];
+}
+
+template <typename S>
+void accumulate(S* in, S* out, uint32_t log_size){
+  uint32_t nof_steps = (log_size - 1) / MAX_SHMEM_LOG_SIZE;
+  uint32_t last_step_size = (log_size - 1) % MAX_SHMEM_LOG_SIZE;
+	printf("nof steps %d last size %d\n", nof_steps, last_step_size);
+  for (int i = 0; i < nof_steps; i++)
+  {
+    sum_reduction<<<1<<(log_size - 1 -(MAX_SHMEM_LOG_SIZE)*(i+1)), SHMEM_SIZE>>>(i? out : in, out);
+		printf("nof blocks %d\n", 1<<(log_size-(MAX_SHMEM_LOG_SIZE)*(i+1)-1));
+		cudaDeviceSynchronize();
+  	printf("cuda err %d\n", cudaGetLastError());
+  }
+  if (last_step_size) sum_reduction<<<1, 1<<last_step_size>>>(out, out);
+	cudaDeviceSynchronize();
+  printf("cuda err last %d\n", cudaGetLastError());
+}
+
+
+
+template <typename S>
+S my_hash(){
+	return 1;
+}
 
 template <typename S>
 void sumcheck_alg1(S* evals, S* t, S* T, S C, int n){
-  S alpha = my_hash(T, C);
+	S alpha;
+  // S alpha = my_hash(/*T, C*/);
   // S rp_even, rp_odd;
   for (int p = 0; p < n; p++)
   {
     int nof_threads = 1<<(n-1-p);
     // move update kernel here and unify
-    reduction_kernel<<<nof_threads>>>(evals, t, n-p); //accumulation
+    // reduction_kernel<<<nof_threads>>>(evals, t, n-p); //accumulation
+    accumulate(evals, t, n-p); //accumulation
     T[2*p+1] = t[0];
     T[2*p+2] = t[1];
-    alpha = my_hash(alpha, t[0], t[1]); //phase 2
-    update_evals_kernel<<<nof_threads>>>(evals, alpha); //phase 3
+    // alpha = my_hash(/*alpha, t[0], t[1]*/); //phase 2
+		int NOF_THREADS = 256;
+		int NOF_BLOCKS = (nof_threads + NOF_THREADS - 1) / NOF_THREADS;
+    update_evals_kernel<<<NOF_BLOCKS, NOF_THREADS>>>(evals, alpha); //phase 3
   }
-  
+
+}
+
+template <typename S>
+void sumcheck_alg1_ref(S* evals, S* t, S* T, S C, int n){
+  // S alpha = my_hash(/*T, C*/);
+	S alpha = 1;
+  S rp_even, rp_odd;
+  for (int p = 0; p < n; p++)
+  {
+		rp_even = 0; rp_odd = 0;
+		// rp_even = S::zero(); rp_odd = S::zero();
+		printf("evals\n");
+		for (int i = 0; i < 1<<(n-p); i++)
+		{
+			printf("%d, ",evals[i]);
+		}
+		printf("\n");
+		for (int i = 0; i < 1<<(n-1-p); i++)
+		{
+			rp_even = rp_even + evals[2*i];
+			rp_odd = rp_odd + evals[2*i+1];
+		}
+    T[2*p+1] = rp_even;
+    T[2*p+2] = rp_odd;
+    // alpha = my_hash(/*alpha, t[0], t[1]*/); //phase 2
+		alpha = 1;
+		for (int i = 0; i < 1<<(n-1-p); i++)
+		{
+			t[i] = (1-alpha)*evals[2*i] + alpha*evals[2*i+1];
+		}
+		for (int i = 0; i < 1<<(n-1-p); i++)
+		{
+			evals[i] = t[i];
+		}
+  }
 }
---
+++ icicle/appUtils/sumcheck/tests/verification.cu
@@ -0,0 +1,75 @@
+#define CURVE_ID BLS12_381
+
+#include "primitives/field.cuh"
+#include "primitives/projective.cuh"
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+#include "curves/curve_config.cuh"
+#include "sumcheck/sumcheck.cu"
+#include <memory>
+
+typedef curve_config::scalar_t test_scalar;
+typedef curve_config::scalar_t test_data;
+
+
+int main(){
+
+  int evals[8] = {0,1,2,3,4,5,6,7};
+  int t[8] = {0,0,0,0,0,0,0,0};
+  int T[8] = {45,0,0,0,0,0,0,0};
+  int C = 33;
+  int n = 3;
+
+  int *d_evals;
+  int *d_t;
+  int *d_T;
+  
+  int log_size = 17;
+  int size = 1<<log_size;
+  auto largEvals = std::make_unique<int[]>(size);
+  for (int i = 0; i < size; i++)
+  {
+    largEvals[i] = i%2? 1 : 2;
+  }
+  
+
+  cudaMalloc(&d_evals, sizeof(int) * size);
+  cudaMalloc(&d_t, sizeof(int) * 8);
+  cudaMalloc(&d_T, sizeof(int) * 8);
+  cudaMemcpy(d_evals, largEvals.get(), sizeof(int) * size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_t, t, sizeof(int) * 8, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_T, T, sizeof(int) * 8, cudaMemcpyHostToDevice);
+
+  for (int i = 0; i < 8; i++)
+  {
+    std::cout << largEvals[i] <<std::endl;
+  }
+  accumulate(d_evals, d_evals, log_size);
+  // cudaDeviceSynchronize();
+  // printf("cuda err %d\n", cudaGetLastError());
+  cudaMemcpy(largEvals.get(), d_evals, sizeof(int) * size, cudaMemcpyDeviceToHost);
+  for (int i = 0; i < 8; i++)
+  {
+    std::cout << largEvals[i] <<std::endl;
+  }
+
+
+  // sumcheck_alg1(d_evals, t, T, C, n);
+  // sumcheck_alg1_ref(evals, t, T, C, n);
+
+  // for (int i = 0; i < 8; i++)
+  // {
+  //   std::cout << T[i] <<std::endl;
+  // }
+
+  // cudaMemcpy(T, d_T, sizeof(int) * 8, cudaMemcpyDeviceToHost);
+
+  // for (int i = 0; i < 8; i++)
+  // {
+  //   std::cout << T[i] <<std::endl;
+  // }
+
+  return 0;
+}
