792a2e5f9718d6a657966499cffc89f59f685a63
hadaringonyama <hadar@ingonyama.com>
2024-03-17 14:02:43 +0200

    updated verification env

--- icicle/appUtils/sumcheck/sumcheck.cu
+++ icicle/appUtils/sumcheck/sumcheck.cu
@@ -23,7 +23,7 @@ __global__ void sum_reduction(S *v, S *v_r) {
 	for (int s = blockDim.x / 2; s > 1; s >>= 1) {
 		// Each thread does work unless it is further than the stride
 		if (threadIdx.x < s) {
-			partial_sum[threadIdx.x] += partial_sum[threadIdx.x + s];
+			partial_sum[threadIdx.x] = partial_sum[threadIdx.x] + partial_sum[threadIdx.x + s];
 		}
 		__syncthreads();
 	}
@@ -31,7 +31,7 @@ __global__ void sum_reduction(S *v, S *v_r) {
 	// Let the thread 0 for this block write it's result to main memory
 	// Result is inexed by this block
 	if (threadIdx.x == 0 || threadIdx.x == 1) {
-		printf("debug tid %d, val %d\n", threadIdx.x, partial_sum[threadIdx.x]);
+		// printf("debug tid %d, val %d\n", threadIdx.x, partial_sum[threadIdx.x]);
 		v_r[2*blockIdx.x + threadIdx.x] = partial_sum[threadIdx.x];
 	}
 }
@@ -39,32 +39,32 @@ __global__ void sum_reduction(S *v, S *v_r) {
 template <typename S>
 __global__ void update_evals_kernel(S* evals, S alpha){
   int tid = blockIdx.x * blockDim.x + threadIdx.x;
-  // evals[tid] = (S::one() - alpha) * evals[2*tid] + alpha * evals[2*tid+1];
-  evals[tid] = (1 - alpha) * evals[2*tid] + alpha * evals[2*tid+1];
+  evals[tid] = (S::one() - alpha) * evals[2*tid] + alpha * evals[2*tid+1];
+  // evals[tid] = (1 - alpha) * evals[2*tid] + alpha * evals[2*tid+1];
 }
 
 template <typename S>
-void accumulate(S* in, S* out, uint32_t log_size){
+void accumulate(S* in, S* out, uint32_t log_size, cudaStream_t stream){
   uint32_t nof_steps = (log_size - 1) / MAX_SHMEM_LOG_SIZE;
   uint32_t last_step_size = (log_size - 1) % MAX_SHMEM_LOG_SIZE;
 	printf("nof steps %d last size %d\n", nof_steps, last_step_size);
   for (int i = 0; i < nof_steps; i++)
   {
-    sum_reduction<<<1<<(log_size - 1 -(MAX_SHMEM_LOG_SIZE)*(i+1)), SHMEM_SIZE>>>(i? out : in, out);
+    sum_reduction<<<1<<(log_size - 1 -(MAX_SHMEM_LOG_SIZE)*(i+1)), SHMEM_SIZE,0,stream>>>(i? out : in, out);
 		printf("nof blocks %d\n", 1<<(log_size-(MAX_SHMEM_LOG_SIZE)*(i+1)-1));
-		cudaDeviceSynchronize();
-  	printf("cuda err %d\n", cudaGetLastError());
+		// cudaDeviceSynchronize();
+  	// printf("cuda err %d\n", cudaGetLastError());
   }
-  if (last_step_size) sum_reduction<<<1, 1<<last_step_size>>>(nof_steps? out : in, out);
-	cudaDeviceSynchronize();
-  printf("cuda err last %d\n", cudaGetLastError());
+  if (last_step_size) sum_reduction<<<1, 1<<last_step_size, 0,stream>>>(nof_steps? out : in, out);
+	// cudaDeviceSynchronize();
+  // printf("cuda err last %d\n", cudaGetLastError());
 }
 
 template <typename S>
 __global__ void add_to_trace(S* trace, S* vals, int p){
 	  trace[2*p+1] = vals[0];
     trace[2*p+2] = vals[1];
-		printf("%d  %d\n", vals[0], vals[1]);
+		// printf("%d  %d\n", vals[0], vals[1]);
 }
 
 template <typename S>
@@ -73,8 +73,9 @@ S my_hash(){
 }
 
 template <typename S>
-void sumcheck_alg1(S* evals, S* t, S* T, S C, int n){
-	S alpha = 1;
+void sumcheck_alg1(S* evals, S* t, S* T, S C, int n, cudaStream_t stream){
+	// S alpha = 1;
+	S alpha = S::one();
   // S alpha = my_hash(/*T, C*/);
   // S rp_even, rp_odd;
   for (int p = 0; p < n-1; p++)
@@ -82,34 +83,35 @@ void sumcheck_alg1(S* evals, S* t, S* T, S C, int n){
     int nof_threads = 1<<(n-1-p);
     // move update kernel here and unify
     // reduction_kernel<<<nof_threads>>>(evals, t, n-p); //accumulation
-    accumulate(evals, t, n-p); //accumulation
-		add_to_trace<<<1,1>>>(T, t, p);
+    accumulate(evals, t, n-p, stream); //accumulation
+		add_to_trace<<<1,1,0,stream>>>(T, t, p);
     // T[2*p+1] = t[0];
     // T[2*p+2] = t[1];
     // alpha = my_hash(/*alpha, t[0], t[1]*/); //phase 2
 		int NOF_THREADS = 256;
 		int NOF_BLOCKS = (nof_threads + NOF_THREADS - 1) / NOF_THREADS;
-    update_evals_kernel<<<NOF_BLOCKS, NOF_THREADS>>>(evals, alpha); //phase 3
+    update_evals_kernel<<<NOF_BLOCKS, NOF_THREADS,0, stream>>>(evals, alpha); //phase 3
   }
-	add_to_trace<<<1,1>>>(T, evals, n-1);
+	add_to_trace<<<1,1,0,stream>>>(T, evals, n-1);
 
 }
 
 template <typename S>
 void sumcheck_alg1_ref(S* evals, S* t, S* T, S C, int n){
   // S alpha = my_hash(/*T, C*/);
-	S alpha = 1;
+	// S alpha = 1;
+	S alpha = S::one();
   S rp_even, rp_odd;
   for (int p = 0; p < n; p++)
   {
-		rp_even = 0; rp_odd = 0;
-		// rp_even = S::zero(); rp_odd = S::zero();
-		printf("evals\n");
-		for (int i = 0; i < 1<<(n-p); i++)
-		{
-			printf("%d, ",evals[i]);
-		}
-		printf("\n");
+		// rp_even = 0; rp_odd = 0;
+		rp_even = S::zero(); rp_odd = S::zero();
+		// printf("evals\n");
+		// for (int i = 0; i < 1<<(n-p); i++)
+		// {
+		// 	printf("%d, ",evals[i]);
+		// }
+		// printf("\n");
 		for (int i = 0; i < 1<<(n-1-p); i++)
 		{
 			rp_even = rp_even + evals[2*i];
@@ -118,10 +120,12 @@ void sumcheck_alg1_ref(S* evals, S* t, S* T, S C, int n){
     T[2*p+1] = rp_even;
     T[2*p+2] = rp_odd;
     // alpha = my_hash(/*alpha, t[0], t[1]*/); //phase 2
-		alpha = 1;
+		// alpha = 1;
+		alpha = S::one();
 		for (int i = 0; i < 1<<(n-1-p); i++)
 		{
-			t[i] = (1-alpha)*evals[2*i] + alpha*evals[2*i+1];
+			t[i] = (S::one() - alpha) * evals[2*i] + alpha * evals[2*i+1];
+			// t[i] = (1-alpha)*evals[2*i] + alpha*evals[2*i+1];
 		}
 		for (int i = 0; i < 1<<(n-1-p); i++)
 		{
--- icicle/appUtils/sumcheck/tests/verification.cu
+++ icicle/appUtils/sumcheck/tests/verification.cu
@@ -11,67 +11,169 @@
 #include <memory>
 
 typedef curve_config::scalar_t test_scalar;
-typedef curve_config::scalar_t test_data;
 
+void random_samples(test_scalar* res, uint32_t count)
+{
+  for (int i = 0; i < count; i++)
+    res[i] = i < 1000 ? test_scalar::rand_host() : res[i - 1000];
+}
 
 int main(){
 
-  int evals[8] = {0,1,2,3,4,5,6,7};
-  int t[8] = {0,0,0,0,0,0,0,0};
-  int T[8] = {45,0,0,0,0,0,0,0};
-  int C = 33;
-  int n = 3;
+  //decleration
+  test_scalar *d_transcript;
+  test_scalar *d_evals;
+  test_scalar *d_temp;
+  test_scalar *d_transcript2;
+  test_scalar *d_evals2;
+  test_scalar *d_temp2;
+  test_scalar C;
 
-  int *d_evals;
-  int *d_t;
-  int *d_T;
+  cudaEvent_t gpu_start, gpu_stop;
+  float gpu_time;
+
+  int n = 26;
+  int size = 1 << n;
+
+  cudaStream_t stream1, stream2;
+  cudaStreamCreate(&stream1);
+  cudaStreamCreate(&stream2);
+
+  //allocation
+  auto h_evals = std::make_unique<test_scalar[]>(size);
+  auto h_temp = std::make_unique<test_scalar[]>(size);
+  auto h_transcript = std::make_unique<test_scalar[]>(2*n+1);
+  auto h_transcript_ref = std::make_unique<test_scalar[]>(2*n+1);
   
-  int log_size = 17;
-  int size = 1<<log_size;
-  auto largEvals = std::make_unique<int[]>(size);
-  for (int i = 0; i < size; i++)
-  {
-    largEvals[i] = i%2? 1 : 2;
-  }
+  cudaMalloc(&d_transcript, sizeof(test_scalar) * (2*n+1));
+  cudaMalloc(&d_evals, sizeof(test_scalar) * size);
+  cudaMalloc(&d_temp, sizeof(test_scalar) * size);
+  cudaMalloc(&d_transcript2, sizeof(test_scalar) * (2*n+1));
+  cudaMalloc(&d_evals2, sizeof(test_scalar) * size);
+  cudaMalloc(&d_temp2, sizeof(test_scalar) * size);
   
+  cudaEventCreate(&gpu_start);
+  cudaEventCreate(&gpu_stop);
 
-  // cudaMalloc(&d_evals, sizeof(int) * size);
-  // cudaMemcpy(d_evals, largEvals.get(), sizeof(int) * size, cudaMemcpyHostToDevice);
-  cudaMalloc(&d_evals, sizeof(int) * 8);
-  cudaMemcpy(d_evals, evals, sizeof(int) * 8, cudaMemcpyHostToDevice);
-  cudaMalloc(&d_t, sizeof(int) * 8);
-  cudaMalloc(&d_T, sizeof(int) * 8);
-  cudaMemcpy(d_t, t, sizeof(int) * 8, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_T, T, sizeof(int) * 8, cudaMemcpyHostToDevice);
+  //input init
+  random_samples(h_evals.get(), size);
+  C = test_scalar::rand_host();
+  h_transcript[0] = test_scalar::rand_host();
+  h_transcript_ref[0] = h_transcript[0];
 
-  for (int i = 0; i < 8; i++)
-  {
-    std::cout << evals[i] <<std::endl;
+  //warm up run
+  cudaMemcpy(d_evals, h_evals.get(), sizeof(test_scalar) * size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_transcript, h_transcript.get(), sizeof(test_scalar), cudaMemcpyHostToDevice);
+  sumcheck_alg1(d_evals, d_temp, d_transcript, C, n, stream1);
+  // sumcheck_alg1(d_evals2, d_temp2, d_transcript2, C, n, stream2);
+  cudaDeviceSynchronize();
+  cudaMemcpy(d_evals, h_evals.get(), sizeof(test_scalar) * size, cudaMemcpyHostToDevice);
+
+  //run
+  cudaEventRecord(gpu_start, 0);
+  sumcheck_alg1(d_evals, d_temp, d_transcript, C, n, stream1);
+  // sumcheck_alg1(d_evals2, d_temp2, d_transcript2, C, n, stream2);
+  cudaEventRecord(gpu_stop, 0);
+  cudaDeviceSynchronize();
+  cudaEventElapsedTime(&gpu_time, gpu_start, gpu_stop);
+  cudaStreamSynchronize(stream1);
+  cudaStreamSynchronize(stream2);
+  cudaStreamDestroy(stream1);
+  cudaStreamDestroy(stream2);
+
+  //run reference
+  auto cpu_start = std::chrono::high_resolution_clock::now();
+  // sumcheck_alg1_ref(h_evals.get(), h_temp.get(), h_transcript_ref.get(), C, n);
+  auto cpu_stop = std::chrono::high_resolution_clock::now();
+  auto cpu_time = std::chrono::duration_cast<std::chrono::microseconds>(cpu_stop - cpu_start).count();
+
+  //verify
+  cudaMemcpy(h_transcript.get(), d_transcript, sizeof(test_scalar) * (2*n+1), cudaMemcpyDeviceToHost);
+  
+  bool success = true;
+  for (int i = 0; i < 2*n+1; i++) {
+    if (h_transcript[i] != h_transcript_ref[i]) {
+      success = false;
+      std::cout << i << " ref " << h_transcript_ref[i] << " != " << h_transcript[i] << std::endl;
+    } else {
+      std::cout << i << " ref " << h_transcript_ref[i] << " == " << h_transcript[i] << std::endl;
+    }
   }
-  // accumulate(d_evals, d_evals, log_size);
-  // cudaDeviceSynchronize();
-  // printf("cuda err %d\n", cudaGetLastError());
-  // cudaMemcpy(largEvals.get(), d_evals, sizeof(int) * size, cudaMemcpyDeviceToHost);
-  // for (int i = 0; i < 8; i++)
-  // {
-  //   std::cout << largEvals[i] <<std::endl;
-  // }
+  const char* success_str = success ? "SUCCESS!" : "FAIL!";
+  printf("%s\n", success_str);
+  
+  //print times
+  printf("GPU Runtime=%0.3f MS\n", gpu_time);
+  std::cout << "CPU Runtime=" << cpu_time / 1000 << " MS" << std::endl;
+  // printf("CPU Runtime=%0.3f MS\n", cpu_time);
+
+  //free
+  cudaFree(d_evals);
+  cudaFree(d_temp);
+  cudaFree(d_transcript);
 
 
-  sumcheck_alg1(d_evals, d_t, d_T, C, n);
-  // sumcheck_alg1_ref(evals, t, T, C, n);
-
-  // for (int i = 0; i < 8; i++)
-  // {
-  //   std::cout << T[i] <<std::endl;
-  // }
-
-  cudaMemcpy(T, d_T, sizeof(int) * 8, cudaMemcpyDeviceToHost);
-
-  for (int i = 0; i < 8; i++)
-  {
-    std::cout << T[i] <<std::endl;
-  }
-
-  return 0;
 }
+
+
+// int main(){
+
+//   int evals[8] = {0,1,2,3,4,5,6,7};
+//   int t[8] = {0,0,0,0,0,0,0,0};
+//   int T[8] = {45,0,0,0,0,0,0,0};
+//   int C = 33;
+//   int n = 3;
+
+//   int *d_evals;
+//   int *d_t;
+//   int *d_T;
+  
+//   int log_size = 17;
+//   int size = 1<<log_size;
+//   auto largEvals = std::make_unique<int[]>(size);
+//   for (int i = 0; i < size; i++)
+//   {
+//     largEvals[i] = i%2? 1 : 2;
+//   }
+  
+
+//   // cudaMalloc(&d_evals, sizeof(int) * size);
+//   // cudaMemcpy(d_evals, largEvals.get(), sizeof(int) * size, cudaMemcpyHostToDevice);
+//   cudaMalloc(&d_evals, sizeof(int) * 8);
+//   cudaMemcpy(d_evals, evals, sizeof(int) * 8, cudaMemcpyHostToDevice);
+//   cudaMalloc(&d_t, sizeof(int) * 8);
+//   cudaMalloc(&d_T, sizeof(int) * 8);
+//   cudaMemcpy(d_t, t, sizeof(int) * 8, cudaMemcpyHostToDevice);
+//   cudaMemcpy(d_T, T, sizeof(int) * 8, cudaMemcpyHostToDevice);
+
+//   for (int i = 0; i < 8; i++)
+//   {
+//     std::cout << evals[i] <<std::endl;
+//   }
+//   // accumulate(d_evals, d_evals, log_size);
+//   // cudaDeviceSynchronize();
+//   // printf("cuda err %d\n", cudaGetLastError());
+//   // cudaMemcpy(largEvals.get(), d_evals, sizeof(int) * size, cudaMemcpyDeviceToHost);
+//   // for (int i = 0; i < 8; i++)
+//   // {
+//   //   std::cout << largEvals[i] <<std::endl;
+//   // }
+
+
+//   sumcheck_alg1(d_evals, d_t, d_T, C, n);
+//   // sumcheck_alg1_ref(evals, t, T, C, n);
+
+//   // for (int i = 0; i < 8; i++)
+//   // {
+//   //   std::cout << T[i] <<std::endl;
+//   // }
+
+//   cudaMemcpy(T, d_T, sizeof(int) * 8, cudaMemcpyDeviceToHost);
+
+//   for (int i = 0; i < 8; i++)
+//   {
+//     std::cout << T[i] <<std::endl;
+//   }
+
+//   return 0;
+// }
