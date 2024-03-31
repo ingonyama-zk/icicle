64f3fc7e1b0f4a87af2c2cdb54a22b60bbc93020
VitaliiH <Vitaliy@ingo>
2024-03-31 05:07:52 +0200

    now test draft builds/runs ok

--- wrappers/rust/icicle-curves/icicle-bls12-381/src/ecntt /mod.rs
+++ wrappers/rust/icicle-curves/icicle-bls12-381/src/ecntt /mod.rs
@@ -1,15 +1,23 @@
-use crate::curve::{ScalarCfg, ScalarField};
+use crate::curve::{CurveCfg, ScalarCfg, ScalarField};
 
 use icicle_core::error::IcicleResult;
 use icicle_core::ntt::{NTTConfig, NTTDir, NTT};
 use icicle_core::traits::IcicleResultWrap;
-use icicle_core::impl_ntt;
+use icicle_core::{impl_ecntt, impl_ntt};
 use icicle_cuda_runtime::device_context::DeviceContext;
 use icicle_cuda_runtime::device_context::DEFAULT_DEVICE_ID;
 use icicle_cuda_runtime::error::CudaError;
 use icicle_cuda_runtime::memory::HostOrDeviceSlice;
+use icicle_core::curve::Projective;
 
-impl_ntt!("bls12_381", bls12_381, ScalarField, ScalarCfg);
+impl_ecntt!(
+    "bls12_381",
+    bls12_381,
+    ScalarField,
+    ScalarCfg,
+    //
+    CurveCfg
+);
 
 #[cfg(test)]
 pub(crate) mod tests {
@@ -19,5 +27,5 @@ pub(crate) mod tests {
     use icicle_core::ntt::tests::*;
     use std::sync::OnceLock;
 
-    impl_ntt_tests!(ScalarField);
+    impl_ecntt_tests!(ScalarField);
 }
