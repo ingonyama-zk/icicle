7789ed3f31abae6edb7e74f9e231c487e5b7d3a7
stas <spolonsky@icloud.com>
2024-04-03 13:02:08 -0600

    review-ready example

--- examples/c++/risc0/README.md
+++ examples/c++/risc0/README.md
@@ -1,5 +1,33 @@
-# ICICLE example: use Polynomial API to implement Fast Reed-Solomon Interactive Oracle Proof (FRI)
+# ICICLE example: computationally-intensive elements of RISC0 protocol
 
-The primary goal of FRI is to enable the efficient verification of polynomial evaluations over large fields, which is a crucial aspect of many cryptographic protocols, especially those involving polynomial commitment schemes.
-Integrating FRI within RISC0 or similar systems can significantly improve their performance by reducing the computational and storage requirements for handling polynomial commitments.
+## Why RISC0?
 
+[RISC0 Protocol](https://www.risczero.com/) creates computational integrity proofs (a.k.a. Zero Knowledge Proofs) for programs executing on RISC-V architecture.
+The proofs are created for sequences of values in RISC-V registers, called execution traces.
+This approach is tranparent to developers and enables the use of general purpose languages.
+
+Our [analysis of RISC0 protocol](https://www.ingonyama.com/blog/risc-zero-prover-protocol-analysis) shows that the most computationally-intensive part of the protocol is Fast Reed-Solomon Interactive Oracle Proof (FRI).
+Our example will focus on FRI.
+
+## Best-Practices
+
+This example builds on [ICICLE Polynomial API](../polynomial-api/README.md) and [Poseidon hashes](../poseidon/README.md) so we recommend to run them first.
+
+## Key-Takeaway
+
+RISC0 encodes execution traces into very large polynomials and commits them using Merkle trees.
+FRI speeds-up validation of such commitments by recursively generating smaller polynomials (and trees) from larger ones.
+The key enabler for *recursion* is the *redundancy* of polynomial commitments, hence the use of Reed-Solomon codes.
+
+## What's in the example
+
+This is a **toy** example executing just the first round of FRI
+
+1. Initialize ICICLE: NTT, Polynomials, Poseidon, and Merkle tree
+2. Generate random execution trace data
+3. Reconstruct polynomial from the trace data
+4. Generate Reed-Solomon codeword by interpolating the trace
+5. Reconstruct polynomial from the codeword
+6. Commit to the codeword polynomial: evaluate at shifted points and encode into Merkle tree
+7. FRI Protocol (Commit Phase)
+8. FRI Protocol (Query Phase)
--- examples/c++/risc0/example.cu
+++ examples/c++/risc0/example.cu
@@ -14,8 +14,7 @@ using namespace poseidon;
 using namespace merkle;
 
 // define the polynomial type
-typedef scalar_t T;
-typedef Polynomial<T> Polynomial_t;
+typedef Polynomial<scalar_t> Polynomial_t;
 
 // Merkle tree arity
 #define A 2
@@ -24,112 +23,99 @@ typedef Polynomial<T> Polynomial_t;
 int main(int argc, char** argv)
 {
 
-  const int logn=3;
+  const int logn=14;
   const int n = 1 << logn;
 
-  // FRI blow-up factor: 2
-
-  // int logblowup = 1;
-  // int blowup = 1 << logblowup;
-
-  // Finite field indexing: function omega() gives the root of unity for the given size
-
-  // message domain (trace column in risc0 parlance)
-  scalar_t w = scalar_t::omega(logn);
-  
-  // codeword (trace block) in expanded (2x) domain
-  scalar_t w2 = scalar_t::omega(1+logn);
-
-  // Initialize NTT
+  std::cout << std::endl << "1. Initialize ICICLE" << std::endl;
+  std::cout << "NTT" << std::endl;
   static const int MAX_NTT_LOG_SIZE = 24;
   auto ntt_config = ntt::DefaultNTTConfig<scalar_t>();
   const scalar_t basic_root = scalar_t::omega(MAX_NTT_LOG_SIZE);
   ntt::InitDomain(basic_root, ntt_config.ctx);
-
+  std::cout << "Polynomials" << std::endl;
   // Virtual factory design pattern: initializing polynomimals factory for CUDA backend
   Polynomial_t::initialize(std::make_unique<CUDAPolynomialFactory<>>());
-
-
   // Initialize Poseidon
+  std::cout << "Poseidon" << std::endl;
   device_context::DeviceContext ctx = device_context::get_default_device_context();
   PoseidonConstants<scalar_t> constants;
   init_optimized_poseidon_constants<scalar_t>(A, ctx, &constants);
 
-  std::cout << "Generate message (trace column)" << std::endl;
+  // FRI blow-up factor: 2. RISC0 uses 4.
+
+  std::cout << std::endl << "2. Generate random execution trace data" << std::endl;
   scalar_t* trace = new scalar_t[n];
   scalar_t::RandHostMany(trace, n);
 
-  std::cout << std::endl << "Reconstruct polynomial from values at roots of unity" << std::endl;
+  std::cout << std::endl << "3. Reconstruct polynomial from trace data" << std::endl;
   auto f = Polynomial_t::from_rou_evaluations(trace, n);
   auto d = f.degree();
   std::cout << "Degree: " << d << std::endl;
   auto x = scalar_t::one();
-  auto omega = w;
+  auto omega = scalar_t::omega(logn);
   for (int i = 0; i < n; ++i) {
-    std::cout << "i: " << f(x) << " trace: " << trace[i] << std::endl;
+    // std::cout << "i: " << f(x) << " trace: " << trace[i] << std::endl;
     x = x * omega;
   }
 
-  std::cout << std::endl << "Interpolate codeword (trace block) at larger domain" << std::endl;
+  std::cout << std::endl << "4. Generate Reed-Solomon codeword" << std::endl;
   scalar_t* codeword = new scalar_t[2*n];
   auto x2 = scalar_t::one();
-  auto omega2 = w2;
+  auto omega2 = scalar_t::omega(1+logn);
   for (int i = 0; i < 2*n; ++i) {
     codeword[i] = f(x2);
-    std::cout << i << " : " << codeword[i] << std::endl;
+    // std::cout << i << " : " << codeword[i] << std::endl;
     x2 = x2 * omega2;
   }
-  std::cout << std::endl << "Reconstruct polynomial for the codeword" << std::endl;
+  std::cout << std::endl << "5. Reconstruct polynomial for the codeword" << std::endl;
   auto f2 = Polynomial_t::from_rou_evaluations(codeword, 2*n);
   auto d2 = f2.degree();
-  std::cout << "Degree: " << d2 << " expected: " << d << std::endl;
+  std::cout << "Degree: " << d2 << ", expected: " << d << std::endl;
 
   std::cout << std::endl << "Introduce error in codeword" << std::endl;
   codeword[0] = codeword[0] + scalar_t::one();
   auto f3 = Polynomial_t::from_rou_evaluations(codeword, 2*n);
   auto d3 = f3.degree();
-  std::cout << "Degree: " << d3 << " was: " << d2 << std::endl;
+  std::cout << "Degree: " << d3 << ", was: " << d2 << std::endl;
 
-
-  std::cout << std::endl << "Commitment to the trace polynomial" << std::endl;
+  std::cout << std::endl << "6. Commit to the codeword polynomial" << std::endl;
   scalar_t* commitment = new scalar_t[2*n];
-  std::cout << "Evaluate on larger domain with a shift: " << std::endl;
-  const int shift = 1; // next power of 2 for the rou
-  scalar_t xs = scalar_t::omega(shift + 1 + logn);
+  std::cout << "Evaluate with a shift " << std::endl;
+  scalar_t xs = basic_root;
   
   for (int i = 0; i < 2*n; ++i) {
     commitment[i] = f(xs);
-    std::cout << i << " : " << commitment[i] << std::endl;
+    // std::cout << i << " : " << commitment[i] << std::endl;
     xs = xs * omega2;
   }
+  std::cout << "Build Merkle Tree" << std::endl;
   uint32_t tree_height = (logn + 1) + 1; // extra +1 for larger domain
   size_t digests_len = get_digests_len<scalar_t>(tree_height, A);
-  std::cout << "Digests length: " << digests_len << std::endl;
+  // std::cout << "Digests length: " << digests_len << std::endl;
   scalar_t* digests = new scalar_t[digests_len];
   TreeBuilderConfig config = default_merkle_config<scalar_t>();
   build_merkle_tree<scalar_t, T>(commitment, digests, tree_height, constants, config);
   std::cout << "Root: " << digests[0] << std::endl;
 
-  std::cout << std::endl << "FRI Protocol (Commit Phase)" << std::endl;
+  std::cout << std::endl << "7. FRI Protocol (Commit Phase)" << std::endl;
   const int m = 2*n;
-  std::cout << std::endl << "Split" << std::endl;
+  std::cout << "Split" << std::endl;
   scalar_t f0_coeffs[m] = {0};
   scalar_t f0even_coeffs[m/2] = {0};
   scalar_t f0odd_coeffs[m/2] = {0};
   auto f0 = f2.clone();
 
-  
-
   auto cc = f0.copy_coefficients_to_host(f0_coeffs, 0, -1);
-  std::cout << "Coefficients: " << cc << std::endl;
-  for (int i = 0; i < m; ++i) {
-    std::cout << i << ": " << f0_coeffs[i] << std::endl;
-  }
+  // std::cout << "Coefficients: " << cc << std::endl;
+  // for (int i = 0; i < m; ++i) {
+  //   std::cout << i << ": " << f0_coeffs[i] << std::endl;
+  // }
+  std::cout << "Merge" << std::endl;
   for (int i = 0; i < m/2; ++i) {
     f0even_coeffs[i] = f0_coeffs[2*i];
     f0odd_coeffs[i] = f0_coeffs[2*i+1];
-    std::cout << i << ": even: " << f0even_coeffs[i] << std::endl;
-    std::cout << i << ": odd:  " << f0odd_coeffs[i] << std::endl;
+    // std::cout << i << ": even: " << f0even_coeffs[i] << std::endl;
+    // std::cout << i << ": odd:  " << f0odd_coeffs[i] << std::endl;
   }
   auto f0even = Polynomial_t::from_coefficients(f0even_coeffs, m/2);
   auto f0odd = Polynomial_t::from_coefficients(f0odd_coeffs, m/2);
@@ -137,8 +123,8 @@ int main(int argc, char** argv)
   auto r1 = scalar_t::rand_host();
   // Round 1 polynomial
   auto f1 = f0even + r1 * f0odd;
-  std::cout << std::endl << "FRI Protocol (Query Phase)" << std::endl;
-  std::cout << "Checking the Evaluations are Consistent" << std::endl;
+  std::cout << std::endl << "8. FRI Protocol (Query Phase)" << std::endl;
+  std::cout << "Check for consistency" << std::endl;
   scalar_t xp = scalar_t::rand_host();
   scalar_t xm = scalar_t::zero() - xp;
   auto rhs = (r1+xp)*f0(xp)*scalar_t::inverse(scalar_t::from(2)*xp) + (r1+xm)*f0(xm)*scalar_t::inverse(scalar_t::from(2)*xm);
@@ -151,6 +137,6 @@ int main(int argc, char** argv)
   }
   auto d1 = f1.degree();
   auto d0 = f0.degree();
-  std::cout << "Degree before: " << d1 << " degree before: " << d0 << std::endl;
+  std::cout << "Degree: " << d1 << ", degree before: " << d0 << std::endl;
   return 0;
 }
