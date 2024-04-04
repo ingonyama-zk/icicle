11f28d17c1eeb3119e163f88dff7021750b03656
ChickenLover <romangg81@gmail.com>
2024-04-04 15:29:04 +0700

    pragma once + enable ext field by default

--- icicle/include/curve_or_field.cuh
+++ icicle/include/curve_or_field.cuh
@@ -1,7 +1,13 @@
+#pragma once
+#ifndef CURVE_OR_FIELD_H
+#define CURVE_OR_FIELD_H
+
 #ifdef CURVE_ID
 #include "curves/curve_config.cuh"
 using namespace curve_config;
 #else
 #include "fields/field_config.cuh"
 using namespace field_config;
+#endif
+
 #endif
