4a6b818d008374eadca4bae00382440020a7832a
LeonHibnik <leon@ingonyama.com>
2024-04-08 18:30:44 +0000

    arg parser to build script

--- wrappers/golang/build.sh
+++ wrappers/golang/build.sh
@@ -2,20 +2,13 @@
 
 G2_DEFINED=OFF
 ECNTT_DEFINED=OFF
+CUDA_COMPILER_PATH=/usr/local/cuda/bin/nvcc
+DEVMODE=OFF
 
-if [[ $2 == "ON" ]]
-then
-  G2_DEFINED=ON
-fi
-
-if [[ $3 ]]
-then
-  ECNTT_DEFINED=ON
-fi
-
-BUILD_DIR=$(realpath "$PWD/../../icicle/build")
 SUPPORTED_CURVES=("bn254" "bls12_377" "bls12_381" "bw6_761")
 
+# Parse arguments
+# Handle curve
 if [[ $1 == "all" ]]
 then
   BUILD_CURVES=("${SUPPORTED_CURVES[@]}")
@@ -23,11 +16,43 @@ else
   BUILD_CURVES=( $1 )
 fi
 
+shift # skip curve argument
+for arg in "$@"
+do
+    arg_lower=$(echo "$arg" | tr '[:upper:]' '[:lower:]')
+    case "$arg_lower" in
+        -cuda_version=*)
+            cuda_version=$(echo "$arg" | cut -d'=' -f2)
+            CUDA_COMPILER_PATH=/usr/local/cuda-$cuda_version/bin/nvcc
+            ;;
+        -ecntt=*)
+            ECNTT_DEFINED=$(echo "$arg" | cut -d'=' -f2)
+            ;;
+        -g2=*)
+            G2_DEFINED=$(echo "$arg" | cut -d'=' -f2)
+            ;;
+        -devmode=*)
+            DEVMODE=$(echo "$arg" | cut -d'=' -f2)
+            ;;
+        *)
+            echo "Unknown argument: $arg"
+            exit 1
+            ;;
+    esac
+done
+
+BUILD_DIR=$(realpath "$PWD/../../icicle/build")
+
 cd ../../icicle
 mkdir -p build
 
 for CURVE in "${BUILD_CURVES[@]}"
 do
-  cmake -DCURVE=$CURVE -DG2_DEFINED=$G2_DEFINED -DECNTT_DEFINED=$ECNTT_DEFINED  -DCMAKE_BUILD_TYPE=Release -S . -B build
+  echo "CURVE=${CURVE}" > build_config.txt
+  echo "ECNTT_DEFINED=${ECNTT_DEFINED}" >> build_config.txt
+  echo "G2_DEFINED=${G2_DEFINED}" >> build_config.txt
+  echo "DEVMODE=${DEVMODE}" >> build_config.txt
+  cmake -DCMAKE_CUDA_COMPILER=$CUDA_COMPILER_PATH -DCURVE=$CURVE -DG2_DEFINED=$G2_DEFINED -DECNTT_DEFINED=$ECNTT_DEFINED -DDEVMODE=$DEVMODE -DCMAKE_BUILD_TYPE=Release -S . -B build
   cmake --build build -j8
+  rm build_config.txt
 done
