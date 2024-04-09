12a82e3de8005169fcc909904c0674651be918bb
Leon Hibnik <107353745+LeonHibnik@users.noreply.github.com>
2024-04-09 12:58:19 +0300

    [FEAT] ReleaseDomain API (#465)
    ## Describe the changes
    
    This PR adds a NTT ReleaseDomain API in Golang and Rust
    
    ## Linked Issues
    
    Resolves #
    
    ---------
    
    Co-authored-by: Yuval Shekel <yshekel@gmail.com>

--- wrappers/golang/curves/bls12377/include/ntt.h
+++ wrappers/golang/curves/bls12377/include/ntt.h
@@ -12,6 +12,7 @@ extern "C" {
 cudaError_t bls12_377NTTCuda(const scalar_t* input, int size, int dir, NTTConfig* config, scalar_t* output);
 cudaError_t bls12_377ECNTTCuda(const projective_t* input, int size, int dir, NTTConfig* config, projective_t* output);
 cudaError_t bls12_377InitializeDomain(scalar_t* primitive_root, DeviceContext* ctx, bool fast_twiddles);
+cudaError_t bls12_377ReleaseDomain(DeviceContext* ctx);
 
 #ifdef __cplusplus
 }
