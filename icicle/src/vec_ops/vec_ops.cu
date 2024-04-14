6e3eecd8ca2ea7002b87bf04358260af59aada07
yshekel <yshekel@gmail.com>
2024-04-14 16:31:49 +0300

    Polynomial API and CUDA backend
    (1) C++ template API that is backend (GPU, ZPU or other) agnostic
    (polynomials.h)
    (2) concrete CUDA implementation (polynomial_cuda_backend.cu/cuh)
    (3) C API for FFI
    (4) Groth16 example (test)
    (5) icicle library is now built for tests as well. Polynomial tests are
    linked to icicle lib.

--- icicle/src/vec_ops/vec_ops.cu
+++ icicle/src/vec_ops/vec_ops.cu
@@ -12,21 +12,36 @@ namespace vec_ops {
 #define MAX_THREADS_PER_BLOCK 256
 
     template <typename E>
-    __global__ void MulKernel(E* scalar_vec, E* element_vec, int n, E* result)
+    __global__ void MulKernel(const E* scalar_vec, const E* element_vec, int n, E* result)
     {
       int tid = blockDim.x * blockIdx.x + threadIdx.x;
       if (tid < n) { result[tid] = scalar_vec[tid] * element_vec[tid]; }
     }
 
+    template <typename E, typename S>
+    __global__ void MulScalarKernel(const E* element_vec, const S scalar, int n, E* result)
+    {
+      int tid = blockIdx.x * blockDim.x + threadIdx.x;
+      if (tid < n) { result[tid] = element_vec[tid] * (scalar); }
+    }
+
     template <typename E>
-    __global__ void AddKernel(E* element_vec1, E* element_vec2, int n, E* result)
+    __global__ void DivElementWiseKernel(const E* element_vec1, const E* element_vec2, int n, E* result)
+    {
+      // TODO:implement better based on https://eprint.iacr.org/2008/199
+      int tid = blockIdx.x * blockDim.x + threadIdx.x;
+      if (tid < n) { result[tid] = element_vec1[tid] * E::inverse(element_vec2[tid]); }
+    }
+
+    template <typename E>
+    __global__ void AddKernel(const E* element_vec1, const E* element_vec2, int n, E* result)
     {
       int tid = blockIdx.x * blockDim.x + threadIdx.x;
       if (tid < n) { result[tid] = element_vec1[tid] + element_vec2[tid]; }
     }
 
     template <typename E>
-    __global__ void SubKernel(E* element_vec1, E* element_vec2, int n, E* result)
+    __global__ void SubKernel(const E* element_vec1, const E* element_vec2, int n, E* result)
     {
       int tid = blockIdx.x * blockDim.x + threadIdx.x;
       if (tid < n) { result[tid] = element_vec1[tid] - element_vec2[tid]; }
@@ -41,8 +56,8 @@ namespace vec_ops {
     }
   } // namespace
 
-  template <typename E, void (*Kernel)(E*, E*, int, E*)>
-  cudaError_t VecOp(E* vec_a, E* vec_b, int n, VecOpsConfig<E>& config, E* result)
+  template <typename E, void (*Kernel)(const E*, const E*, int, E*)>
+  cudaError_t VecOp(const E* vec_a, const E* vec_b, int n, VecOpsConfig<E>& config, E* result)
   {
     CHK_INIT_IF_RETURN();
 
@@ -50,17 +65,20 @@ namespace vec_ops {
     int num_threads = MAX_THREADS_PER_BLOCK;
     int num_blocks = (n + num_threads - 1) / num_threads;
 
-    E *d_vec_a, *d_vec_b, *d_result;
+    E *d_result, *d_alloc_vec_a, *d_alloc_vec_b;
+    const E *d_vec_a, *d_vec_b;
     if (!config.is_a_on_device) {
-      CHK_IF_RETURN(cudaMallocAsync(&d_vec_a, n * sizeof(E), config.ctx.stream));
-      CHK_IF_RETURN(cudaMemcpyAsync(d_vec_a, vec_a, n * sizeof(E), cudaMemcpyHostToDevice, config.ctx.stream));
+      CHK_IF_RETURN(cudaMallocAsync(&d_alloc_vec_a, n * sizeof(E), config.ctx.stream));
+      CHK_IF_RETURN(cudaMemcpyAsync(d_alloc_vec_a, vec_a, n * sizeof(E), cudaMemcpyHostToDevice, config.ctx.stream));
+      d_vec_a = d_alloc_vec_a;
     } else {
       d_vec_a = vec_a;
     }
 
     if (!config.is_b_on_device) {
-      CHK_IF_RETURN(cudaMallocAsync(&d_vec_b, n * sizeof(E), config.ctx.stream));
-      CHK_IF_RETURN(cudaMemcpyAsync(d_vec_b, vec_b, n * sizeof(E), cudaMemcpyHostToDevice, config.ctx.stream));
+      CHK_IF_RETURN(cudaMallocAsync(&d_alloc_vec_b, n * sizeof(E), config.ctx.stream));
+      CHK_IF_RETURN(cudaMemcpyAsync(d_alloc_vec_b, vec_b, n * sizeof(E), cudaMemcpyHostToDevice, config.ctx.stream));
+      d_vec_b = d_alloc_vec_b;
     } else {
       d_vec_b = vec_b;
     }
@@ -74,9 +92,8 @@ namespace vec_ops {
     // Call the kernel to perform element-wise operation
     Kernel<<<num_blocks, num_threads, 0, config.ctx.stream>>>(d_vec_a, d_vec_b, n, d_result);
 
-    if (!config.is_a_on_device) { CHK_IF_RETURN(cudaFreeAsync(d_vec_a, config.ctx.stream)); }
-
-    if (!config.is_b_on_device) { CHK_IF_RETURN(cudaFreeAsync(d_vec_b, config.ctx.stream)); }
+    if (!config.is_a_on_device) { CHK_IF_RETURN(cudaFreeAsync(d_alloc_vec_a, config.ctx.stream)); }
+    if (!config.is_b_on_device) { CHK_IF_RETURN(cudaFreeAsync(d_alloc_vec_b, config.ctx.stream)); }
 
     if (!config.is_result_on_device) {
       CHK_IF_RETURN(cudaMemcpyAsync(result, d_result, n * sizeof(E), cudaMemcpyDeviceToHost, config.ctx.stream));
@@ -89,19 +106,19 @@ namespace vec_ops {
   }
 
   template <typename E>
-  cudaError_t Mul(E* vec_a, E* vec_b, int n, VecOpsConfig<E>& config, E* result)
+  cudaError_t Mul(const E* vec_a, const E* vec_b, int n, VecOpsConfig<E>& config, E* result)
   {
     return VecOp<E, MulKernel>(vec_a, vec_b, n, config, result);
   }
 
   template <typename E>
-  cudaError_t Add(E* vec_a, E* vec_b, int n, VecOpsConfig<E>& config, E* result)
+  cudaError_t Add(const E* vec_a, const E* vec_b, int n, VecOpsConfig<E>& config, E* result)
   {
     return VecOp<E, AddKernel>(vec_a, vec_b, n, config, result);
   }
 
   template <typename E>
-  cudaError_t Sub(E* vec_a, E* vec_b, int n, VecOpsConfig<E>& config, E* result)
+  cudaError_t Sub(const E* vec_a, const E* vec_b, int n, VecOpsConfig<E>& config, E* result)
   {
     return VecOp<E, SubKernel>(vec_a, vec_b, n, config, result);
   }
