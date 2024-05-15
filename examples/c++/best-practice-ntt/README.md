7edfa4dd9e9f561ff098bfd14b18a42bc630fa01
stas <spolonsky@icloud.com>
2024-05-15 14:07:35 -0600

    README and cleaner code

--- examples/c++/best-practice-ntt/README.md
+++ examples/c++/best-practice-ntt/README.md
@@ -1,14 +1,28 @@
-# Icicle benchmark: Number Theoretic Transform
+# Icicle best practices: Concurrent Data Transfer and NTT Computation
 
-The benchmark measures the runtime of [Number Theoretic Transform](https://dev.ingonyama.com/icicle/primitives/ntt).
+The [Number Theoretic Transform (NTT)](https://dev.ingonyama.com/icicle/primitives/ntt) is an integral component of many cryptographic algorithms, such as polynomial multiplication in Zero Knowledge Proofs. The performance bottleneck of NTT on GPUs is the data transfer between the host (CPU) and the device (GPU). In a typical NVIDIA GPU this transfer dominates the total NTT execution time.
+
+## Key-Takeaway
+
+When you have to run several NTTs, consider Concurrent Data Download, Upload, and Computation to improve data bus (PCIe) and GPU utilization, and get better total execution time.
+
+Typically, you concurrently
+
+1. Downlod the output of a previous NTT back to the host
+2. Upload the input for a next NTT on the device
+3. Run current NTT
+
+Drawback: The approach requires two on-device memory vectors, decreasing the maximum size of NTT by 2x.
 
 ## Best-Practices
 
-We recommend to run the benchmarks in [ZK-containers](../ZK-containers.md) to save your time and mental energy.
+1. Use three separate CUDA streams for Download, Upload, and Compute operations
+2. Use pinned (page-locked) memory on host to speed data bus transfers. Calling `cudaHostAlloc` allocates pinned memory.
+3. Use in-place NTT to save on device memory.
 
-## Run benchmark
+## Running the example
 
-Inside the container,
+To change teh defaul curve BN254, edit `compile.sh` and `CMakeLists.txt`
 
 ```sh
 ./compile.sh
--- examples/c++/best-practice-ntt/example.cu
+++ examples/c++/best-practice-ntt/example.cu
@@ -8,14 +8,12 @@
 using namespace bn254;
 using namespace ntt;
 
-
 const std::string curve = "BN254";
 
 typedef scalar_t S;
 typedef scalar_t E;
 
 const unsigned max_log_ntt_size = 27;
-
 
 void initialize_input(const unsigned ntt_size, const unsigned nof_ntts, E * elements ) {
   for (unsigned i = 0; i < ntt_size * nof_ntts; i++) {
@@ -27,7 +25,6 @@ using FpMilliseconds = std::chrono::duration<float, std::chrono::milliseconds::p
 #define START_TIMER(timer) auto timer##_start = std::chrono::high_resolution_clock::now();
 #define END_TIMER(timer, msg) printf("%s: %.0f ms\n", msg, FpMilliseconds(std::chrono::high_resolution_clock::now() - timer##_start).count());
 
-
 int main(int argc, char** argv) {
   cudaDeviceReset();
   cudaDeviceProp deviceProperties;
@@ -36,14 +33,12 @@ int main(int argc, char** argv) {
   std::string gpu_full_name = deviceProperties.name;
   std::cout << gpu_full_name << std::endl;
   std::string gpu_name = gpu_full_name;
-  int gpu_clock_mhz = deviceProperties.clockRate/1000.;
 
   std::cout << "Curve: " << curve << std::endl;
 
-  
-  // auto ctx = device_context::get_default_device_context();
   S basic_root = S::omega(max_log_ntt_size);
-  
+
+  // change these parameters to match the desired NTT size and batch size  
   const unsigned log_ntt_size = 22;
   const unsigned nof_ntts = 16;
 
@@ -60,7 +55,6 @@ int main(int argc, char** argv) {
     0,              // device_id
     0,              // mempool
   };
-  
 
   bn254_initialize_domain(&basic_root, ctx_compute, /* fast twiddles */ true);
   NTTConfig<S> config_compute = default_ntt_config<S>(ctx_compute);
@@ -75,8 +69,7 @@ int main(int argc, char** argv) {
   cudaStreamCreate(&stream_h2d);
   cudaStreamCreate(&stream_d2h);
 
-
-  std::cout << "Full Duplex Interleaving In-place NTT" << std::endl;
+  std::cout << "Concurrent Download, Upload, and Compute In-place NTT" << std::endl;
   int nof_blocks = 32;
   std::cout << "Number of blocks: " << nof_blocks << std::endl;
   int block_size = ntt_size*nof_ntts/nof_blocks;
@@ -84,65 +77,60 @@ int main(int argc, char** argv) {
   // on-host pinned data
   E * h_inp[2];
   E * h_out[2];
-  cudaHostAlloc((void**)&h_inp[0], sizeof(E)*ntt_size*nof_ntts, cudaHostAllocDefault);
-  cudaHostAlloc((void**)&h_inp[1], sizeof(E)*ntt_size*nof_ntts, cudaHostAllocDefault);
-  cudaHostAlloc((void**)&h_out[0], sizeof(E)*ntt_size*nof_ntts, cudaHostAllocDefault);
-  cudaHostAlloc((void**)&h_out[1], sizeof(E)*ntt_size*nof_ntts, cudaHostAllocDefault);
-
+  for (int i = 0; i < 2; i++) {
+    cudaHostAlloc((void**)&h_inp[i], sizeof(E)*ntt_size*nof_ntts, cudaHostAllocDefault);
+    cudaHostAlloc((void**)&h_out[i], sizeof(E)*ntt_size*nof_ntts, cudaHostAllocDefault);
+  }
+  
   // on-device in-place data
+  // we need two on-device vectors to overlap data transfers with NTT kernel execution
   E * d_vec[2];
-  cudaMalloc((void**)&d_vec[0], sizeof(E)*ntt_size*nof_ntts);
-  cudaMalloc((void**)&d_vec[1], sizeof(E)*ntt_size*nof_ntts);
-
+  for (int i = 0; i < 2; i++) {
+    cudaMalloc((void**)&d_vec[i], sizeof(E)*ntt_size*nof_ntts);
+  }
+  
   // initialize input data
   initialize_input(ntt_size, nof_ntts, h_inp[0]);
   initialize_input(ntt_size, nof_ntts, h_inp[1]);
 
-  
-
-  // Perform Asynchronous Memory Transfers and Kernel Execution
-  // [0] memory transfers
-  // [1] kernel execution
   cudaEvent_t compute_start, compute_stop;
   cudaEventCreate(&compute_start);
   cudaEventCreate(&compute_stop);
 
-  
-  for ( int run = 0; run < 10; run++ ) {
-    int buffer_compute = run % 2;
-    int buffer_transfer = (run + 1) % 2;
+  for ( int run = 0; run < 10; run++ ) {  
+    int vec_compute = run % 2;
+    int vec_transfer = (run + 1) % 2;
     std::cout << "Run: " << run << std::endl;
-    std::cout << "Buffer Compute: " << buffer_compute << std::endl;
-    std::cout << "Buffer Transfer: " << buffer_transfer << std::endl;
+    std::cout << "Compute Vector: " << vec_compute << std::endl;
+    std::cout << "Transfer Vector: " << vec_transfer << std::endl;
     START_TIMER(inplace);
     cudaEventRecord(compute_start, stream_compute);
-    bn254_ntt_cuda(d_vec[buffer_compute], ntt_size, NTTDir::kForward, config_compute, d_vec[buffer_compute]);
+    bn254_ntt_cuda(d_vec[vec_compute], ntt_size, NTTDir::kForward, config_compute, d_vec[vec_compute]);
     cudaEventRecord(compute_stop, stream_compute);
+    // we have to delay upload to device relative to download from device by one block: preserve write after read
     for (int i = 0; i <= nof_blocks; i++) {
       if (i < nof_blocks) {
-        cudaMemcpyAsync(&h_out[buffer_transfer][i*block_size], &d_vec[buffer_transfer][i*block_size], sizeof(E)*block_size, cudaMemcpyDeviceToHost, stream_d2h);    
+        cudaMemcpyAsync(&h_out[vec_transfer][i*block_size], &d_vec[vec_transfer][i*block_size], sizeof(E)*block_size, cudaMemcpyDeviceToHost, stream_d2h);    
       }
       if (i>0) {
-        cudaMemcpyAsync(&d_vec[buffer_transfer][(i-1)*block_size], &h_inp[buffer_transfer][(i-1)*block_size], sizeof(E)*block_size, cudaMemcpyHostToDevice, stream_h2d);
+        cudaMemcpyAsync(&d_vec[vec_transfer][(i-1)*block_size], &h_inp[vec_transfer][(i-1)*block_size], sizeof(E)*block_size, cudaMemcpyHostToDevice, stream_h2d);
       }
       cudaStreamSynchronize(stream_d2h); 
       cudaStreamSynchronize(stream_h2d); 
     }
-    // cudaStreamSynchronize(stream_compute);  
     cudaEventSynchronize(compute_stop);
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, compute_start, compute_stop);
-    END_TIMER(inplace, "Full Duplex Interleaved In-place  NTT");
+    END_TIMER(inplace, "Concurrent In-Place  NTT");
     std::cout << "NTT time: " << milliseconds << " ms" << std::endl;
   };
   
   // Clean-up
-  cudaFree(d_vec[0]); 
-  cudaFree(d_vec[1]);
-  cudaFreeHost(h_inp[0]); 
-  cudaFreeHost(h_inp[1]);
-  cudaFreeHost(h_out[0]); 
-  cudaFreeHost(h_out[1]);
+  for (int i = 0; i < 2; i++) {
+    cudaFree(d_vec[i]); 
+    cudaFreeHost(h_inp[i]); 
+    cudaFreeHost(h_out[i]); 
+  }
   cudaEventDestroy(compute_start);
   cudaEventDestroy(compute_stop);
   cudaStreamDestroy(stream_compute);
