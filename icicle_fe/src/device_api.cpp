b2fe795bb1b24c8ccba60c1f71b39616774dfbc3
Yuval Shekel <yshekel@gmail.com>
2024-05-16 19:43:32 +0300

    errors and device_api interface

---
+++ icicle_fe/src/device_api.cpp
@@ -0,0 +1,35 @@
+
+#include <memory>
+#include <unordered_map>
+#include <string>
+#include <stdexcept>
+
+#include "icicle/device.h"
+#include "icicle/device_api.h"
+
+using namespace icicle;
+
+class DeviceAPIRegistry {
+    static std::unordered_map<std::string, std::shared_ptr<DeviceAPI>> apiMap;
+
+public:
+    static void registerDeviceAPI(const std::string& deviceType, std::shared_ptr<DeviceAPI> api) {
+        if (apiMap.find(deviceType) != apiMap.end()) {
+            throw std::runtime_error("Attempting to register a duplicate API for device type: " + deviceType);
+        }
+        apiMap[deviceType] = api;
+    }
+
+    static std::shared_ptr<DeviceAPI> getDeviceAPI(const Device& device) {
+        auto it = apiMap.find(device.type);
+        if (it != apiMap.end()) {
+            return it->second;
+        } else {
+            throw std::runtime_error("Device API not found for type: " + std::string(device.type));
+        }
+    }
+};
+
+extern "C" DeviceAPI* getDeviceAPI(const Device* device) {
+	return DeviceAPIRegistry::getDeviceAPI(*device).get();
+}
