19a9b76d645f1a6f5625be1a2ecac3d17e2da12d
yshekel <yshekel@gmail.com>
2024-05-20 13:05:45 +0300

    fix:  cmake set_gpu_env() and windows build (#520)

--- wrappers/rust/icicle-curves/icicle-bn254/build.rs
+++ wrappers/rust/icicle-curves/icicle-bn254/build.rs
@@ -1,4 +1,5 @@
 use cmake::Config;
+use std::env;
 
 fn main() {
     println!("cargo:rerun-if-env-changed=CXXFLAGS");
@@ -19,6 +20,10 @@ fn main() {
 
     #[cfg(feature = "devmode")]
     config.define("DEVMODE", "ON");
+
+    if let Ok(cuda_arch) = env::var("CUDA_ARCH") {
+        config.define("CUDA_ARCH", &cuda_arch);
+    }
 
     // Build
     let out_dir = config
