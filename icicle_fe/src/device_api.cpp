25d334b62a6d1b6d1b6d11a09caa43d0ec9b4448
Yuval Shekel <yshekel@gmail.com>
2024-05-20 18:06:51 +0300

    mock cpu backend for device API

--- icicle_fe/src/device_api.cpp
+++ icicle_fe/src/device_api.cpp
@@ -3,33 +3,41 @@
 #include <unordered_map>
 #include <string>
 #include <stdexcept>
+#include <iostream>
 
 #include "icicle/device.h"
 #include "icicle/device_api.h"
 
 using namespace icicle;
 
-class DeviceAPIRegistry {
-    static std::unordered_map<std::string, std::shared_ptr<DeviceAPI>> apiMap;
+class DeviceAPIRegistry
+{
+  static inline std::unordered_map<std::string, std::shared_ptr<DeviceAPI>> apiMap;
 
 public:
-    static void registerDeviceAPI(const std::string& deviceType, std::shared_ptr<DeviceAPI> api) {
-        if (apiMap.find(deviceType) != apiMap.end()) {
-            throw std::runtime_error("Attempting to register a duplicate API for device type: " + deviceType);
-        }
-        apiMap[deviceType] = api;
+  static void registerDeviceAPI(const std::string& deviceType, std::shared_ptr<DeviceAPI> api)
+  {
+    if (apiMap.find(deviceType) != apiMap.end()) {
+      throw std::runtime_error("Attempting to register a duplicate API for device type: " + deviceType);
     }
+    apiMap[deviceType] = api;
+  }
 
-    static std::shared_ptr<DeviceAPI> getDeviceAPI(const Device& device) {
-        auto it = apiMap.find(device.type);
-        if (it != apiMap.end()) {
-            return it->second;
-        } else {
-            throw std::runtime_error("Device API not found for type: " + std::string(device.type));
-        }
+  static std::shared_ptr<DeviceAPI> getDeviceAPI(const Device& device)
+  {
+    auto it = apiMap.find(device.type);
+    if (it != apiMap.end()) {
+      return it->second;
+    } else {
+      throw std::runtime_error("Device API not found for type: " + std::string(device.type));
     }
+  }
 };
 
-extern "C" DeviceAPI* getDeviceAPI(const Device* device) {
-	return DeviceAPIRegistry::getDeviceAPI(*device).get();
+extern "C" DeviceAPI* getDeviceAPI(const Device* device) { return DeviceAPIRegistry::getDeviceAPI(*device).get(); }
+
+extern "C" void registerDeviceAPI(const std::string& deviceType, std::shared_ptr<DeviceAPI> api)
+{
+  std::cout << "deviceAPI registered for " << deviceType << std::endl;
+  DeviceAPIRegistry::registerDeviceAPI(deviceType, api);
 }
