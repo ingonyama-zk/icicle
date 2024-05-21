0af022ad49dc820b38809b412b313760fc99df1a
Yuval Shekel <yshekel@gmail.com>
2024-05-21 16:55:43 +0300

    verify valid device

--- icicle_fe/backend/cuda/cuda_device_api.cu
+++ icicle_fe/backend/cuda/cuda_device_api.cu
@@ -10,65 +10,72 @@ using namespace icicle;
 class CUDADeviceAPI : public DeviceAPI
 {
 public:
+    IcicleError setDevice(const Device& device) const {
+        cudaError_t err = cudaSetDevice(device.id);
+        return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::INVALID_DEVICE;
+    }
+
     // Memory management
     IcicleError allocateMemory(const Device& device, void** ptr, size_t size) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = cudaMalloc(ptr, size);
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::ALLOCATION_FAILED;
     }
 
     IcicleError allocateMemoryAsync(const Device& device, void** ptr, size_t size, IcicleStreamHandle stream) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = cudaMallocAsync(ptr, size, reinterpret_cast<cudaStream_t>(stream));
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::ALLOCATION_FAILED;
     }
 
     IcicleError freeMemory(const Device& device, void* ptr) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = cudaFree(ptr);
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::DEALLOCATION_FAILED;
     }
 
     IcicleError freeMemoryAsync(const Device& device, void* ptr, IcicleStreamHandle stream) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = cudaFreeAsync(ptr, reinterpret_cast<cudaStream_t>(stream));
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::DEALLOCATION_FAILED;
     }
 
     IcicleError getAvailableMemory(const Device& device, size_t& total /*OUT*/, size_t& free /*OUT*/) override
     {
-        size_t freeMem = 0, totalMem = 0;
-        cudaError_t err = cudaMemGetInfo(&freeMem, &totalMem);
-        if (err != cudaSuccess) {
-            return IcicleError::UNKNOWN_ERROR;
-        }
-
-        free = freeMem;
-        total = totalMem;
-        return IcicleError::SUCCESS;
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
+        cudaError_t err = cudaMemGetInfo(&free, &total);
+        return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::UNKNOWN_ERROR;
     }
 
     // Data transfer
     IcicleError copyToHost(const Device& device, void* dst, const void* src, size_t size) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = cudaMemcpy(dst, src, size, cudaMemcpyDeviceToHost);
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::COPY_FAILED;
     }
 
     IcicleError copyToHostAsync(const Device& device, void* dst, const void* src, size_t size, IcicleStreamHandle stream) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = cudaMemcpyAsync(dst, src, size, cudaMemcpyDeviceToHost, reinterpret_cast<cudaStream_t>(stream));
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::COPY_FAILED;
     }
 
     IcicleError copyToDevice(const Device& device, void* dst, const void* src, size_t size) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = cudaMemcpy(dst, src, size, cudaMemcpyHostToDevice);
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::COPY_FAILED;
     }
 
     IcicleError copyToDeviceAsync(const Device& device, void* dst, const void* src, size_t size, IcicleStreamHandle stream) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = cudaMemcpyAsync(dst, src, size, cudaMemcpyHostToDevice, reinterpret_cast<cudaStream_t>(stream));
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::COPY_FAILED;
     }
@@ -76,6 +83,7 @@ public:
     // Synchronization
     IcicleError synchronize(const Device& device, IcicleStreamHandle stream = nullptr) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = (stream == nullptr) ? cudaDeviceSynchronize() : cudaStreamSynchronize(reinterpret_cast<cudaStream_t>(stream));
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::SYNCHRONIZATION_FAILED;
     }
@@ -83,6 +91,7 @@ public:
     // Stream management
     IcicleError createStream(const Device& device, IcicleStreamHandle* stream) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaStream_t cudaStream;
         cudaError_t err = cudaStreamCreate(&cudaStream);
         *stream = reinterpret_cast<IcicleStreamHandle>(cudaStream);
@@ -91,6 +100,7 @@ public:
 
     IcicleError destroyStream(const Device& device, IcicleStreamHandle stream) override
     {
+        if(IcicleError err = setDevice(device); err != IcicleError::SUCCESS) return err;
         cudaError_t err = cudaStreamDestroy(reinterpret_cast<cudaStream_t>(stream));
         return (err == cudaSuccess) ? IcicleError::SUCCESS : IcicleError::STREAM_DESTRUCTION_FAILED;
     }
