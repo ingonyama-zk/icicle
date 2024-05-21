d5aac95ff9bbb76b5eb022738451f1266e9cd9fd
Yuval Shekel <yshekel@gmail.com>
2024-05-21 15:41:45 +0300

    CUDA backend device

--- icicle_fe/tests/test_device_api.cpp
+++ icicle_fe/tests/test_device_api.cpp
@@ -10,8 +10,12 @@ using namespace icicle;
 class DeviceApiTest : public ::testing::Test
 {
 public:
+  static inline std::list<std::string> s_regsitered_devices;
   // SetUpTestSuite/TearDownTestSuite are called once for the entire test suite
-  static void SetUpTestSuite() {}
+  static void SetUpTestSuite() {
+    s_regsitered_devices = getRegisteredDevices();
+    ASSERT_EQ(s_regsitered_devices.size(), 2); // "CPU" and "CUDA"
+  }
   static void TearDownTestSuite() {}
 
   // SetUp/TearDown are called before and after each test
@@ -29,50 +33,60 @@ TEST_F(DeviceApiTest, UnregisteredDeviceError)
 TEST_F(DeviceApiTest, MemoryCopySync)
 {
   int input[2] = {1, 2};
-  int output[2] = {0, 0};
 
-  icicle::Device dev = {"CPU", 0};
-  auto device_api = getDeviceAPI(&dev);
-  void* dev_mem = nullptr;
-  ICICLE_CHECK(device_api->allocateMemory(dev, &dev_mem, sizeof(input)));
-  ICICLE_CHECK(device_api->copyToDevice(dev, dev_mem, input, sizeof(input)));
-  ICICLE_CHECK(device_api->copyToHost(dev, output, dev_mem, sizeof(input)));
+  for (const auto& device_type : s_regsitered_devices) {
+    int output[2] = {0, 0};
 
-  ASSERT_EQ(0, memcmp(input, output, sizeof(input)));
+    icicle::Device dev = {device_type.c_str(), 0};
+    auto device_api = getDeviceAPI(&dev);
+    void* dev_mem = nullptr;
+    ICICLE_CHECK(device_api->allocateMemory(dev, &dev_mem, sizeof(input)));
+    ICICLE_CHECK(device_api->copyToDevice(dev, dev_mem, input, sizeof(input)));
+    ICICLE_CHECK(device_api->copyToHost(dev, output, dev_mem, sizeof(input)));
+
+    ASSERT_EQ(0, memcmp(input, output, sizeof(input)));
+  }
 }
 
 TEST_F(DeviceApiTest, MemoryCopyAsync)
 {
   int input[2] = {1, 2};
-  int output[2] = {0, 0};
+  for (const auto& device_type : s_regsitered_devices) {
+    int output[2] = {0, 0};
 
-  icicle::Device dev = {"CPU", 0};
-  auto device_api = getDeviceAPI(&dev);
-  void* dev_mem = nullptr;
+    icicle::Device dev = {device_type.c_str(), 0};
+    auto device_api = getDeviceAPI(&dev);
+    void* dev_mem = nullptr;
 
-  IcicleStreamHandle stream;
-  ICICLE_CHECK(device_api->createStream(dev, &stream));
-  ICICLE_CHECK(device_api->allocateMemoryAsync(dev, &dev_mem, sizeof(input), stream));
-  ICICLE_CHECK(device_api->copyToDeviceAsync(dev, dev_mem, input, sizeof(input), stream));
-  ICICLE_CHECK(device_api->copyToHostAsync(dev, output, dev_mem, sizeof(input), stream));
-  ICICLE_CHECK(device_api->synchronize(dev, stream));
+    IcicleStreamHandle stream;
+    ICICLE_CHECK(device_api->createStream(dev, &stream));
+    ICICLE_CHECK(device_api->allocateMemoryAsync(dev, &dev_mem, sizeof(input), stream));
+    ICICLE_CHECK(device_api->copyToDeviceAsync(dev, dev_mem, input, sizeof(input), stream));
+    ICICLE_CHECK(device_api->copyToHostAsync(dev, output, dev_mem, sizeof(input), stream));
+    ICICLE_CHECK(device_api->synchronize(dev, stream));
 
-  ASSERT_EQ(0, memcmp(input, output, sizeof(input)));
+    ASSERT_EQ(0, memcmp(input, output, sizeof(input)));
+  }
 }
 
 TEST_F(DeviceApiTest, ApiError) {
-  icicle::Device dev = {"CPU", 0};
-  auto device_api = getDeviceAPI(&dev);
-  void* dev_mem = nullptr;
-  EXPECT_ANY_THROW(ICICLE_CHECK(device_api->allocateMemory(dev, &dev_mem, -1)));
+  for (const auto& device_type : s_regsitered_devices) {    
+    icicle::Device dev = {device_type.c_str(), 0};
+    auto device_api = getDeviceAPI(&dev);
+    void* dev_mem = nullptr;
+    EXPECT_ANY_THROW(ICICLE_CHECK(device_api->allocateMemory(dev, &dev_mem, -1)));
+  }
 }
 
 TEST_F(DeviceApiTest, AvailableMemory) {
-  icicle::Device dev = {"CPU", 0};
+  icicle::Device dev = {"CUDA", 0}; // TODO Yuval: implement for CPU too
   auto device_api = getDeviceAPI(&dev);
-  size_t total, free;
-  // TODO Yuval:fix when implemented
-  ASSERT_EQ(IcicleError::API_NOT_IMPLEMENTED, device_api->getAvailableMemory(dev, total, free));
+  size_t total, free;  
+  ASSERT_EQ(IcicleError::SUCCESS, device_api->getAvailableMemory(dev, total, free));
+
+  double total_GB = double(total) / (1<<30);
+  double free_GB = double(free) / (1<<30);
+  std::cout << std::fixed << std::setprecision(2) << "total=" << total_GB << "[GB], free=" << free_GB << "[GB]" << std::endl;
 }
 
 int main(int argc, char** argv)
