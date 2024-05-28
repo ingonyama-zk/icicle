d909124d60e57c531426951e2ffc45fb92abd6ea
Yuval Shekel <yshekel@gmail.com>
2024-05-28 16:02:19 +0300

    add vec-sub and vec-mul apis

--- icicle_v3/backend/cpu/src/field/cpu_vec_ops.cpp
+++ icicle_v3/backend/cpu/src/field/cpu_vec_ops.cpp
@@ -8,6 +8,7 @@
 using namespace field_config;
 using namespace icicle;
 
+/*********************************** ADD ***********************************/
 template <typename T>
 eIcicleError
 cpu_vector_add(const Device& device, const T* vec_a, const T* vec_b, int n, const VecOpsConfig& config, T* output)
@@ -19,3 +20,29 @@ cpu_vector_add(const Device& device, const T* vec_a, const T* vec_b, int n, cons
 }
 
 REGISTER_VECTOR_ADD_BACKEND("CPU", cpu_vector_add<scalar_t>);
+
+/*********************************** SUB ***********************************/
+template <typename T>
+eIcicleError
+cpu_vector_sub(const Device& device, const T* vec_a, const T* vec_b, int n, const VecOpsConfig& config, T* output)
+{
+  for (int i = 0; i < n; ++i) {
+    output[i] = vec_a[i] - vec_b[i];
+  }
+  return eIcicleError::SUCCESS;
+}
+
+REGISTER_VECTOR_SUB_BACKEND("CPU", cpu_vector_sub<scalar_t>);
+
+/*********************************** MUL ***********************************/
+template <typename T>
+eIcicleError
+cpu_vector_mul(const Device& device, const T* vec_a, const T* vec_b, int n, const VecOpsConfig& config, T* output)
+{
+  for (int i = 0; i < n; ++i) {
+    output[i] = vec_a[i] * vec_b[i];
+  }
+  return eIcicleError::SUCCESS;
+}
+
+REGISTER_VECTOR_MUL_BACKEND("CPU", cpu_vector_mul<scalar_t>);
