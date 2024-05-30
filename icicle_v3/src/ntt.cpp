c07eb08587a679efa3dc60d3578fa819047c598e
Yuval Shekel <yshekel@gmail.com>
2024-05-30 14:14:02 +0300

    ntt init domain API

--- icicle_v3/src/ntt.cpp
+++ icicle_v3/src/ntt.cpp
@@ -3,6 +3,7 @@
 
 namespace icicle {
 
+  // NTT
   ICICLE_DISPATCHER_INST(NttDispatcher, ntt, NttImpl);
 
   extern "C" eIcicleError
@@ -15,6 +16,21 @@ namespace icicle {
   eIcicleError ntt(const scalar_t* input, int size, NTTDir dir, NTTConfig<scalar_t>& config, scalar_t* output)
   {
     return CONCAT_EXPAND(FIELD, ntt)(input, size, dir, config, output);
+  }
+
+  // INIT DOMAIN
+  ICICLE_DISPATCHER_INST(NttInitDomainDispatcher, ntt_init_domain, NttInitDomainImpl);
+
+  extern "C" eIcicleError
+  CONCAT_EXPAND(FIELD, ntt_init_domain)(const scalar_t& primitive_root, const ConfigExtension& config)
+  {
+    return NttInitDomainDispatcher::execute(primitive_root, config);
+  }
+
+  template <>
+  eIcicleError ntt_init_domain(const scalar_t& primitive_root, const ConfigExtension& config)
+  {
+    return CONCAT_EXPAND(FIELD, ntt_init_domain)(primitive_root, config);
   }
 
 } // namespace icicle
