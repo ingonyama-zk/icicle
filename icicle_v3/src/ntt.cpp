c09720ae5af5c898075a71a0929d488ef264aadc
Yuval Shekel <yshekel@gmail.com>
2024-05-30 12:14:12 +0300

    refactored apis to be template with specialization

--- icicle_v3/src/ntt.cpp
+++ icicle_v3/src/ntt.cpp
@@ -1,12 +1,20 @@
 #include "icicle/ntt.h"
 #include "icicle/dispatcher.h"
 
-using namespace icicle;
+namespace icicle {
 
-ICICLE_DISPATCHER_INST(NttDispatcher, ntt, NttImpl);
+  ICICLE_DISPATCHER_INST(NttDispatcher, ntt, NttImpl);
 
-extern "C" eIcicleError
-CONCAT_EXPAND(FIELD, ntt)(const scalar_t* input, int size, NTTDir dir, NTTConfig<scalar_t>& config, scalar_t* output)
-{
-  return NttDispatcher::execute(input, size, dir, config, output);
-}
+  extern "C" eIcicleError
+  CONCAT_EXPAND(FIELD, ntt)(const scalar_t* input, int size, NTTDir dir, NTTConfig<scalar_t>& config, scalar_t* output)
+  {
+    return NttDispatcher::execute(input, size, dir, config, output);
+  }
+
+  template <>
+  eIcicleError ntt(const scalar_t* input, int size, NTTDir dir, NTTConfig<scalar_t>& config, scalar_t* output)
+  {
+    return CONCAT_EXPAND(FIELD, ntt)(input, size, dir, config, output);
+  }
+
+} // namespace icicle
