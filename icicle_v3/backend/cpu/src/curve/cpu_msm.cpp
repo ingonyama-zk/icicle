564529a468da20123e7d6cae8ebfa41ba11981e0
Yuval Shekel <yshekel@gmail.com>
2024-06-04 17:03:02 +0300

    msm precompute bases API

--- icicle_v3/backend/cpu/src/curve/cpu_msm.cpp
+++ icicle_v3/backend/cpu/src/curve/cpu_msm.cpp
@@ -9,20 +9,29 @@
 using namespace curve_config;
 using namespace icicle;
 
-eIcicleError cpu_msm(
-  const Device& device,
-  const scalar_t* scalars,
-  const affine_t* bases,
-  int msm_size,
-  const MSMConfig& config,
-  projective_t* results)
+template <typename S, typename A, typename P>
+eIcicleError
+cpu_msm(const Device& device, const S* scalars, const A* bases, int msm_size, const MSMConfig& config, P* results)
 {
-  projective_t res = projective_t::zero();
+  P res = projective_t::zero();
   for (auto i = 0; i < msm_size; ++i) {
-    res = res + projective_t::from_affine(bases[i]) * scalars[i];
+    res = res + P::from_affine(bases[i]) * scalars[i];
   }
   results[0] = res;
   return eIcicleError::SUCCESS;
 }
 
-REGISTER_MSM_BACKEND("CPU", cpu_msm);
+template <typename A>
+eIcicleError cpu_msm_precompute_bases(
+  const Device& device,
+  const A* input_bases,
+  int nof_bases,
+  int precompute_factor,
+  const MsmPreComputeConfig& config,
+  A* output_bases)
+{
+  return eIcicleError::API_NOT_IMPLEMENTED;
+}
+
+REGISTER_MSM_BACKEND("CPU", (cpu_msm<scalar_t, affine_t, projective_t>));
+REGISTER_MSM_PRE_COMPUTE_BASES_BACKEND("CPU", cpu_msm_precompute_bases<affine_t>);
