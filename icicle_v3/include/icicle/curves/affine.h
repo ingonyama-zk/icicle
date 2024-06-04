729d1c0fd9a3277eec55c847d0872e38474b53f5
Yuval Shekel <yshekel@gmail.com>
2024-06-04 19:22:34 +0300

    montgomery conversion G1

--- icicle_v3/include/icicle/curves/affine.h
+++ icicle_v3/include/icicle/curves/affine.h
@@ -32,6 +32,11 @@ public:
     return (xs.x == ys.x) && (xs.y == ys.y);
   }
 
+    friend HOST_DEVICE_INLINE bool operator!=(const Affine& xs, const Affine& ys)
+  {
+    return !(xs==ys);
+  }
+
   friend HOST_INLINE std::ostream& operator<<(std::ostream& os, const Affine& point)
   {
     os << "x: " << point.x << "; y: " << point.y;
