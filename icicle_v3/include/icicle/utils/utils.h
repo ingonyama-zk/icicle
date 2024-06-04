283964a84ae0f925c4dd11a5844e7d30289df1ff
Yuval Shekel <yshekel@gmail.com>
2024-06-04 11:08:30 +0300

    macro for unique identifiers

--- icicle_v3/include/icicle/utils/utils.h
+++ icicle_v3/include/icicle/utils/utils.h
@@ -4,5 +4,6 @@
 
 #define CONCAT_DIRECT(a, b) a##_##b
 #define CONCAT_EXPAND(a, b) CONCAT_DIRECT(a, b) // expand a,b before concatenation
+#define UNIQUE(a)           CONCAT_EXPAND(a, __LINE__)
 
 #endif // ICICLE_UTILS_H
