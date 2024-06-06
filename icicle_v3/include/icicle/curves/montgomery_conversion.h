e2237ec34d5c16e4675af13c898448ba9647b248
Yuval Shekel <yshekel@gmail.com>
2024-06-06 14:05:41 +0300

    montgomery conversion G2

--- icicle_v3/include/icicle/curves/montgomery_conversion.h
+++ icicle_v3/include/icicle/curves/montgomery_conversion.h
@@ -80,4 +80,42 @@ namespace icicle {
     }();                                                                                                               \
   }
 
+#ifdef G2
+  using AffineG2ConvertMontImpl = std::function<eIcicleError(
+    const Device& device,
+    const g2_affine_t* input,
+    size_t n,
+    bool is_into,
+    const ConvertMontgomeryConfig& config,
+    g2_affine_t* output)>;
+
+  void register_affine_g2_convert_montgomery(const std::string& deviceType, AffineG2ConvertMontImpl);
+
+#define REGISTER_AFFINE_G2_CONVERT_MONTGOMERY_BACKEND(DEVICE_TYPE, FUNC)                                               \
+  namespace {                                                                                                          \
+    static bool UNIQUE(_reg_affine_g2_convert_mont) = []() -> bool {                                                   \
+      register_affine_g2_convert_montgomery(DEVICE_TYPE, FUNC);                                                        \
+      return true;                                                                                                     \
+    }();                                                                                                               \
+  }
+
+  using ProjectiveG2ConvertMontImpl = std::function<eIcicleError(
+    const Device& device,
+    const g2_projective_t* input,
+    size_t n,
+    bool is_into,
+    const ConvertMontgomeryConfig& config,
+    g2_projective_t* output)>;
+
+  void register_projective_g2_convert_montgomery(const std::string& deviceType, ProjectiveG2ConvertMontImpl);
+
+#define REGISTER_PROJECTIVE_G2_CONVERT_MONTGOMERY_BACKEND(DEVICE_TYPE, FUNC)                                           \
+  namespace {                                                                                                          \
+    static bool UNIQUE(_reg_projective_g2_convert_mont) = []() -> bool {                                               \
+      register_projective_g2_convert_montgomery(DEVICE_TYPE, FUNC);                                                    \
+      return true;                                                                                                     \
+    }();                                                                                                               \
+  }
+#endif // G2
+
 }; // namespace icicle
