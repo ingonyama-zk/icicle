bf7c9bea2be0ab54782b3f2e05a754fba2b8e24d
Yuval Shekel <yshekel@gmail.com>
2024-06-09 16:16:46 +0300

    CUDA ntt

--- icicle_v3/src/ntt.cpp
+++ icicle_v3/src/ntt.cpp
@@ -38,13 +38,13 @@ namespace icicle {
   ICICLE_DISPATCHER_INST(NttInitDomainDispatcher, ntt_init_domain, NttInitDomainImpl);
 
   extern "C" eIcicleError
-  CONCAT_EXPAND(FIELD, ntt_init_domain)(const scalar_t& primitive_root, const ConfigExtension& config)
+  CONCAT_EXPAND(FIELD, ntt_init_domain)(const scalar_t& primitive_root, const NTTInitDomainConfig& config)
   {
     return NttInitDomainDispatcher::execute(primitive_root, config);
   }
 
   template <>
-  eIcicleError ntt_init_domain(const scalar_t& primitive_root, const ConfigExtension& config)
+  eIcicleError ntt_init_domain(const scalar_t& primitive_root, const NTTInitDomainConfig& config)
   {
     return CONCAT_EXPAND(FIELD, ntt_init_domain)(primitive_root, config);
   }
@@ -60,4 +60,16 @@ namespace icicle {
     return CONCAT_EXPAND(FIELD, ntt_release_domain)();
   }
 
+  /*************************** GET ROOT OF UNITY ***************************/
+  extern "C" scalar_t CONCAT_EXPAND(FIELD, get_root_of_unity)(uint64_t max_size)
+  {
+    const auto log_max_size = static_cast<uint32_t>(std::ceil(std::log2(max_size)));
+    return scalar_t::omega(log_max_size);
+  }
+
+  template <>
+  scalar_t get_root_of_unity(uint64_t max_size)
+  {
+    return CONCAT_EXPAND(FIELD, get_root_of_unity)(max_size);
+  }
 } // namespace icicle
