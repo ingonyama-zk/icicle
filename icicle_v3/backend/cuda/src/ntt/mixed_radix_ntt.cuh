84d0624ff0761d390cc2d88ef2dceef689a7a1b0
Yuval Shekel <yshekel@gmail.com>
2024-06-13 16:57:44 +0300

    fixed cuda ntt headers

--- icicle_v3/backend/cuda/src/ntt/mixed_radix_ntt.cuh
+++ icicle_v3/backend/cuda/src/ntt/mixed_radix_ntt.cuh
@@ -1,13 +1,3 @@
-#pragma once
-
-#include <stdint.h>
-#include "icicle/ntt.h" // for enum Ordering
-
-enum class NttAlgorithm : int { Auto, Radix2, MixedRadix };
-
-// backend specific flags
-#define CUDA_NTT_FAST_TWIDDLES_MODE "fast_twiddles"
-#define CUDA_NTT_ALGORITHM          "ntt_algorithm"
 
 namespace mxntt {
 
