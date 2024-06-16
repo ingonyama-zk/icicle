edd7ed46ff50347c03765503db671be2ccf81897
Yuval Shekel <yshekel@gmail.com>
2024-06-16 18:57:48 +0300

    rust runtime crate progress

--- wrappers/rust_v3/icicle-runtime/src/lib.rs
+++ wrappers/rust_v3/icicle-runtime/src/lib.rs
@@ -1,26 +1,9 @@
 pub mod device;
 pub mod errors;
 pub mod runtime;
+pub mod tests;
 
 // Re-export the types for easier access
 pub use device::{Device, DeviceProperties};
 pub use errors::eIcicleError;
 pub use runtime::*;
-
-use std::ffi::CString;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_set_device() {
-        // load backends to process
-        let backend_install_dir = env!("DEFAULT_BACKEND_INSTALL_DIR");
-        assert_eq!(load_backend(&backend_install_dir, true), eIcicleError::Success);
-
-        let devtype = String::from("CPU");
-        let dev = Device::new(&devtype, 0);
-        assert_eq!(set_device(&dev), eIcicleError::Success);
-    }
-}
