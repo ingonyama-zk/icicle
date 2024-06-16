88de8bd226acde2007791ea5961412e3282e702b
Yuval Shekel <yshekel@gmail.com>
2024-06-16 18:57:48 +0300

    rust runtime crate progress

--- wrappers/rust_v3/icicle-runtime/src/device.rs
+++ wrappers/rust_v3/icicle-runtime/src/device.rs
@@ -1,10 +1,10 @@
-use std::ffi::{CStr, CString};
+use std::ffi::CString;
 use std::os::raw::c_char;
 
 #[repr(C)]
 pub struct Device {
-    pub device_type: *const std::os::raw::c_char,
-    pub id: i32,
+    device_type: *const c_char,
+    id: i32,
 }
 
 #[repr(C)]
@@ -16,10 +16,26 @@ pub struct DeviceProperties {
 
 impl Device {
     pub fn new(device_type: &str, id: i32) -> Device {
+        // Note that the CString is released when device is dropped.
         let c_string = CString::new(device_type).expect("CString::new failed");
         Device {
             device_type: c_string.into_raw(),
             id,
+        }
+    }
+}
+
+impl Drop for Device {
+    fn drop(&mut self) {
+        if !self
+            .device_type
+            .is_null()
+        {
+            unsafe {
+                // Convert back to CString so it is released
+                let _ = CString::from_raw(self.device_type as *mut c_char);
+                self.device_type = std::ptr::null();
+            }
         }
     }
 }
