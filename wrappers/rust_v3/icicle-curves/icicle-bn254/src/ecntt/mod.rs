e8c161b6693539721b91721892abf344abe4c9ed
Yuval Shekel <yshekel@gmail.com>
2024-06-26 17:11:07 +0300

    rust ecntt

---
+++ wrappers/rust_v3/icicle-curves/icicle-bn254/src/ecntt/mod.rs
@@ -0,0 +1,17 @@
+#![cfg(feature = "ec_ntt")]
+use crate::curve::{CurveCfg, ScalarCfg, ScalarField};
+use icicle_core::ecntt::Projective;
+use icicle_core::impl_ecntt;
+
+impl_ecntt!("bn254", bn254, ScalarField, ScalarCfg, CurveCfg);
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use crate::curve::{CurveCfg, ScalarField};
+
+    use icicle_core::ecntt::tests::*;
+    use icicle_core::impl_ecntt_tests;
+    use std::sync::OnceLock;
+
+    impl_ecntt_tests!(ScalarField, CurveCfg);
+}
