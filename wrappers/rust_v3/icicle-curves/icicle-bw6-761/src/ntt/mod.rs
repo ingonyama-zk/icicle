1ada53183798847cc596f0754421a518ac48d873
Yuval Shekel <yshekel@gmail.com>
2024-06-26 19:20:17 +0300

    bw6-761 rust crate

---
+++ wrappers/rust_v3/icicle-curves/icicle-bw6-761/src/ntt/mod.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+pub(crate) mod tests {
+    use crate::curve::ScalarField;
+    use icicle_core::impl_ntt_tests;
+    use icicle_core::ntt::tests::*;
+    use serial_test::{parallel, serial};
+    use std::sync::OnceLock;
+
+    impl_ntt_tests!(ScalarField);
+}
