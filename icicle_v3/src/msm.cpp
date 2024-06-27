f2236562f2601df6a4d5a49a1c0815b9504578aa
Yuval Shekel <yshekel@gmail.com>
2024-06-27 18:00:24 +0300

    rust msm

--- icicle_v3/src/msm.cpp
+++ icicle_v3/src/msm.cpp
@@ -23,9 +23,9 @@ namespace icicle {
   }
 
 #ifdef G2
-  ICICLE_DISPATCHER_INST(MsmG2Dispatcher, msm_g2, MsmG2Impl);
+  ICICLE_DISPATCHER_INST(MsmG2Dispatcher, g2_msm, MsmG2Impl);
 
-  extern "C" eIcicleError CONCAT_EXPAND(CURVE, msm_g2)(
+  extern "C" eIcicleError CONCAT_EXPAND(CURVE, g2_msm)(
     const scalar_t* scalars, const g2_affine_t* bases, int msm_size, const MSMConfig& config, g2_projective_t* results)
   {
     return MsmG2Dispatcher::execute(scalars, bases, msm_size, config, results);
@@ -35,7 +35,7 @@ namespace icicle {
   eIcicleError msm(
     const scalar_t* scalars, const g2_affine_t* bases, int msm_size, const MSMConfig& config, g2_projective_t* results)
   {
-    return CONCAT_EXPAND(CURVE, msm_g2)(scalars, bases, msm_size, config, results);
+    return CONCAT_EXPAND(CURVE, g2_msm)(scalars, bases, msm_size, config, results);
   }
 #endif // G2
 
@@ -43,50 +43,33 @@ namespace icicle {
   ICICLE_DISPATCHER_INST(MsmPreComputeDispatcher, msm_precompute_bases, MsmPreComputeImpl);
 
   extern "C" eIcicleError CONCAT_EXPAND(CURVE, msm_precompute_bases)(
-    const affine_t* input_bases,
-    int nof_bases,
-    int precompute_factor,
-    const MsmPreComputeConfig& config,
-    affine_t* output_bases)
+    const affine_t* input_bases, int nof_bases, const MSMConfig& config, affine_t* output_bases)
   {
-    return MsmPreComputeDispatcher::execute(input_bases, nof_bases, precompute_factor, config, output_bases);
+    return MsmPreComputeDispatcher::execute(input_bases, nof_bases, config, output_bases);
   }
 
   template <>
-  eIcicleError msm_precompute_bases(
-    const affine_t* input_bases,
-    int nof_bases,
-    int precompute_factor,
-    const MsmPreComputeConfig& config,
-    affine_t* output_bases)
+  eIcicleError
+  msm_precompute_bases(const affine_t* input_bases, int nof_bases, const MSMConfig& config, affine_t* output_bases)
   {
-    return CONCAT_EXPAND(CURVE, msm_precompute_bases)(input_bases, nof_bases, precompute_factor, config, output_bases);
+    return CONCAT_EXPAND(CURVE, msm_precompute_bases)(input_bases, nof_bases, config, output_bases);
   }
 
 #ifdef G2
-  ICICLE_DISPATCHER_INST(MsmG2PreComputeDispatcher, msm_g2_precompute_bases, MsmG2PreComputeImpl);
+  ICICLE_DISPATCHER_INST(MsmG2PreComputeDispatcher, g2_msm_precompute_bases, MsmG2PreComputeImpl);
 
-  extern "C" eIcicleError CONCAT_EXPAND(CURVE, msm_g2_precompute_bases)(
-    const g2_affine_t* input_bases,
-    int nof_bases,
-    int precompute_factor,
-    const MsmPreComputeConfig& config,
-    g2_affine_t* output_bases)
+  extern "C" eIcicleError CONCAT_EXPAND(CURVE, g2_msm_precompute_bases)(
+    const g2_affine_t* input_bases, int nof_bases, const MSMConfig& config, g2_affine_t* output_bases)
   {
-    return MsmG2PreComputeDispatcher::execute(input_bases, nof_bases, precompute_factor, config, output_bases);
+    return MsmG2PreComputeDispatcher::execute(input_bases, nof_bases, config, output_bases);
   }
 
 #ifndef G1_AFFINE_SAME_TYPE_AS_G2_AFFINE
   template <>
   eIcicleError msm_precompute_bases(
-    const g2_affine_t* input_bases,
-    int nof_bases,
-    int precompute_factor,
-    const MsmPreComputeConfig& config,
-    g2_affine_t* output_bases)
+    const g2_affine_t* input_bases, int nof_bases, const MSMConfig& config, g2_affine_t* output_bases)
   {
-    return CONCAT_EXPAND(CURVE, msm_g2_precompute_bases)(
-      input_bases, nof_bases, precompute_factor, config, output_bases);
+    return CONCAT_EXPAND(CURVE, g2_msm_precompute_bases)(input_bases, nof_bases, config, output_bases);
   }
 #endif // !G1_AFFINE_SAME_TYPE_AS_G2_AFFINE
 #endif // G2
--- wrappers/rust_v3/icicle-core/src/lib.rs
+++ wrappers/rust_v3/icicle-core/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod curve;
 pub mod ecntt;
 pub mod field;
+pub mod msm;
 pub mod ntt;
 pub mod vec_ops;
 
