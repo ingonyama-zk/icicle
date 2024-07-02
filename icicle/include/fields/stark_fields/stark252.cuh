605b3d2fc68fdebc6f455acfc53aa7645d04c52d
DmytroTym <dmytrotym1@gmail.com>
2024-07-02 22:43:49 +0300

    Correct stark252 rou

--- icicle/include/fields/stark_fields/stark252.cuh
+++ icicle/include/fields/stark_fields/stark252.cuh
@@ -11,8 +11,8 @@ namespace stark252 {
                                            0x00000000, 0x00000000, 0x00000011, 0x08000000};
     PARAMS(modulus)
 
-    static constexpr storage<8> rou = {0x00000000, 0x00000000, 0x00000000, 0x00000000,
-                                       0x00000000, 0x00000000, 0x00000011, 0x08000000};
+    static constexpr storage<8> rou = {0x42f8ef94, 0x6070024f, 0xe11a6161, 0xad187148,
+                                       0x9c8b0fa5, 0x3f046451, 0x87529cfa, 0x005282db};
     TWIDDLES(modulus, rou)
   };
 
