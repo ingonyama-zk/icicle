e088469726f576b477f03821802be235448e532b
ChickenLover <romangg81@gmail.com>
2024-07-09 16:30:41 +0300

    add m31 test for poseidon2

--- icicle/src/poseidon2/test_m31.cu
+++ icicle/src/poseidon2/test_m31.cu
@@ -9,6 +9,9 @@
 #include "poseidon2/poseidon2.cuh"
 using namespace poseidon2;
 
+#include "fields/field_config.cuh"
+using namespace field_config;
+
 #include "hash/hash.cuh"
 
 #define T 16
@@ -28,24 +31,36 @@ int main(int argc, char* argv[])
   Poseidon2<scalar_t> poseidon(T, T, MdsType::DEFAULT_MDS, DiffusionStrategy::DEFAULT_DIFFUSION, ctx);
   END_TIMER(timer_const, "Load poseidon constants");
 
-  START_TIMER(allocation_timer);
-  // Prepare input data of [0, 1, 2 ... (number_of_blocks * arity) - 1]
   int number_of_blocks = argc > 1 ? 1 << atoi(argv[1]) : 1024;
-  scalar_t input = scalar_t::zero();
   scalar_t* in_ptr = static_cast<scalar_t*>(malloc(number_of_blocks * T * sizeof(scalar_t)));
+  scalar_t* out_ptr = static_cast<scalar_t*>(malloc(number_of_blocks * sizeof(scalar_t)));
+  scalar_t input = scalar_t::zero();
+
+  hash::SpongeConfig cfg = hash::default_sponge_config();
+
+  size_t number_of_repetitions = argc > 2 ? 1 << atoi(argv[2]) : 32;
+
+  // Prepare input data of [0, 1, 2 ... (number_of_blocks * arity) - 1]
   for (uint32_t i = 0; i < number_of_blocks * T; i++) {
     in_ptr[i] = input;
     input = input + scalar_t::one();
   }
-  END_TIMER(allocation_timer, "Allocate mem and fill input");
 
-  scalar_t* out_ptr = static_cast<scalar_t*>(malloc(number_of_blocks * sizeof(scalar_t)));
-
-  hash::SpongeConfig cfg = hash::default_sponge_config();
-
-  START_TIMER(poseidon_timer);
+  // Warm up
   poseidon.hash_many(in_ptr, out_ptr, number_of_blocks, T, 1, cfg);
-  END_TIMER(poseidon_timer, "Poseidon")
+
+  auto total_time_start = std::chrono::high_resolution_clock::now();
+  size_t avg_time = 0;
+  for (int i = 0; i < number_of_repetitions; i++) {
+    auto poseidon_start = std::chrono::high_resolution_clock::now();
+    poseidon.hash_many(in_ptr, out_ptr, number_of_blocks, T, 1, cfg);
+    avg_time += FpMilliseconds(std::chrono::high_resolution_clock::now() - poseidon_start).count();
+  }
+  auto total_time = FpMilliseconds(std::chrono::high_resolution_clock::now() - total_time_start).count();
+
+  std::cout << "Block size: " << number_of_blocks << std::endl;
+  std::cout << "Total time: " << total_time << " ms" << std::endl;
+  std::cout << "Avg time: " << avg_time / number_of_repetitions << " ms" << std::endl;
 
   // for (int i = 0; i < number_of_blocks; i++) {
   //   std::cout << "{";
