765dac7927fc09525c069ede9d5b284dde469aa2
hadaringonyama <hadar@ingonyama.com>
2024-08-06 13:33:24 +0300

    add-sub 64 works

--- icicle_v3/include/icicle/fields/host_math.h
+++ icicle_v3/include/icicle/fields/host_math.h
@@ -5,52 +5,60 @@
 #include "icicle/fields/storage.h"
 namespace host_math {
 
-  // return x + y with uint32_t operands
-  static constexpr __host__ uint32_t add(const uint32_t x, const uint32_t y) { return x + y; }
+  // return x + y with T operands
+  template <typename T>
+  static constexpr __host__ T add(const T x, const T y) { return x + y; }
 
-  // return x + y + carry with uint32_t operands
-  static constexpr __host__ uint32_t addc(const uint32_t x, const uint32_t y, const uint32_t carry)
+  // return x + y + carry with T operands
+  template <typename T>
+  static constexpr __host__ T addc(const T x, const T y, const T carry)
   {
     return x + y + carry;
   }
 
-  // return x + y and carry out with uint32_t operands
-  static constexpr __host__ uint32_t add_cc(const uint32_t x, const uint32_t y, uint32_t& carry)
+  // return x + y and carry out with T operands
+  template <typename T>
+  static constexpr __host__ T add_cc(const T x, const T y, T& carry)
   {
-    uint32_t result = x + y;
+    T result = x + y;
     carry = x > result;
     return result;
   }
 
-  // return x + y + carry and carry out  with uint32_t operands
-  static constexpr __host__ uint32_t addc_cc(const uint32_t x, const uint32_t y, uint32_t& carry)
+  // return x + y + carry and carry out  with T operands
+  template <typename T>
+  static constexpr __host__ T addc_cc(const T x, const T y, T& carry)
   {
-    const uint32_t result = x + y + carry;
+    const T result = x + y + carry;
     carry = carry && x >= result || !carry && x > result;
     return result;
   }
 
-  // return x - y with uint32_t operands
-  static constexpr __host__ uint32_t sub(const uint32_t x, const uint32_t y) { return x - y; }
+  // return x - y with T operands
+  template <typename T>
+  static constexpr __host__ T sub(const T x, const T y) { return x - y; }
 
-  //    return x - y - borrow with uint32_t operands
-  static constexpr __host__ uint32_t subc(const uint32_t x, const uint32_t y, const uint32_t borrow)
+  //    return x - y - borrow with T operands
+  template <typename T>
+  static constexpr __host__ T subc(const T x, const T y, const T borrow)
   {
     return x - y - borrow;
   }
 
-  //    return x - y and borrow out with uint32_t operands
-  static constexpr __host__ uint32_t sub_cc(const uint32_t x, const uint32_t y, uint32_t& borrow)
+  //    return x - y and borrow out with T operands
+  template <typename T>
+  static constexpr __host__ T sub_cc(const T x, const T y, T& borrow)
   {
-    uint32_t result = x - y;
+    T result = x - y;
     borrow = x < result;
     return result;
   }
 
-  //    return x - y - borrow and borrow out with uint32_t operands
-  static constexpr __host__ uint32_t subc_cc(const uint32_t x, const uint32_t y, uint32_t& borrow)
+  //    return x - y - borrow and borrow out with T operands
+  template <typename T>
+  static constexpr __host__ T subc_cc(const T x, const T y, T& borrow)
   {
-    const uint32_t result = x - y - borrow;
+    const T result = x - y - borrow;
     borrow = borrow && x <= result || !borrow && x < result;
     return result;
   }
@@ -79,7 +87,8 @@ namespace host_math {
 
     constexpr HOST_INLINE carry_chain() : index(0) {}
 
-    constexpr HOST_INLINE uint32_t add(const uint32_t x, const uint32_t y, uint32_t& carry)
+    template <typename T>
+    constexpr HOST_INLINE T add(const T x, const T y, T& carry)
     {
       index++;
       if (index == 1 && OPS_COUNT == 1 && !CARRY_IN && !CARRY_OUT)
@@ -92,7 +101,8 @@ namespace host_math {
         return host_math::addc(x, y, carry);
     }
 
-    constexpr HOST_INLINE uint32_t sub(const uint32_t x, const uint32_t y, uint32_t& carry)
+    template <typename T>
+    constexpr HOST_INLINE T sub(const T x, const T y, T& carry)
     {
       index++;
       if (index == 1 && OPS_COUNT == 1 && !CARRY_IN && !CARRY_OUT)
@@ -123,11 +133,8 @@ namespace host_math {
 
   template <unsigned NLIMBS_A, unsigned NLIMBS_B = NLIMBS_A>
   static HOST_INLINE void
-  multiply_raw_64(const storage<NLIMBS_A>& as, const storage<NLIMBS_B>& bs, storage<NLIMBS_A + NLIMBS_B>& rs)
+  multiply_raw_64(const uint64_t* a64, const uint64_t* b64, uint64_t* r64)
   {
-    const uint64_t* a64 = as.limbs64;
-    const uint64_t* b64 = bs.limbs64;
-    uint64_t* r64 = rs.limbs64;
     for (unsigned i = 0; i < NLIMBS_B/2; i++) {
       uint64_t carry = 0;
       for (unsigned j = 0; j < NLIMBS_A/2; j++)
@@ -136,10 +143,53 @@ namespace host_math {
     }
   }
 
-template <unsigned NLIMBS_A, unsigned NLIMBS_B = NLIMBS_A, bool IS_CONSTEXPR = false>
+  template <unsigned NLIMBS_A, unsigned NLIMBS_B = NLIMBS_A>
+  static HOST_INLINE void
+  multiply_raw_64(const storage<NLIMBS_A>& as, const storage<NLIMBS_B>& bs, storage<NLIMBS_A + NLIMBS_B>& rs)
+  {
+    const uint64_t* a64 = as.limbs64;
+    const uint64_t* b64 = bs.limbs64;
+    uint64_t* r64 = rs.limbs64;
+    multiply_raw_64<NLIMBS_A, NLIMBS_B>(a64, b64, r64);
+  }
+
+  // template <unsigned NLIMBS>
+  // static HOST_INLINE void
+  // multiply_karatsuba_64(const storage<NLIMBS>& as, const storage<NLIMBS>& bs, storage<NLIMBS + NLIMBS>& rs)
+  // {
+  //   const uint64_t* a64 = as.limbs64;
+  //   const uint64_t* b64 = bs.limbs64;
+  //   uint64_t* r64 = rs.limbs64;
+  //   multiply_raw_64(a64, b64, r64);
+  //   multiply_raw_64(&a64[NLIMBS/2 >> 1], &b64[NLIMBS/2 >> 1], &r64[NLIMBS/2]);
+  //   alignas(16) uint64_t middle_part[NLIMBS/2];
+  //   alignas(16) uint64_t diffs[NLIMBS/2];
+  //   // uint64_t middle_part[NLIMBS/2];
+  //   // uint64_t diffs[NLIMBS/2];
+  //   // Differences of halves \f$ a_{hi} - a_{lo}; b_{lo} - b_{hi} \$f are written into `diffs`, signs written to
+  //   // `carry1` and `carry2`.
+  //   uint32_t carry1 = add_sub_u32_device<(TLC >> 1), true, true>(&a[TLC >> 1], a, diffs);
+  //   uint32_t carry2 = add_sub_u32_device<(TLC >> 1), true, true>(b, &b[TLC >> 1], &diffs[TLC >> 1]);
+  //   // Compute the "middle part" of Karatsuba: \f$ a_{lo} \cdot b_{hi} + b_{lo} \cdot a_{hi} \f$.
+  //   // This is where the assumption about unset high bit of `a` and `b` is relevant.
+  //   multiply_and_add_short_raw_device(diffs, &diffs[TLC >> 1], middle_part, r, &r[TLC]);
+  //   // Corrections that need to be performed when differences are negative.
+  //   // Again, carry doesn't need to be propagated due to unset high bits of `a` and `b`.
+  //   if (carry1)
+  //     add_sub_u32_device<(TLC >> 1), true, false>(&middle_part[TLC >> 1], &diffs[TLC >> 1], &middle_part[TLC >> 1]);
+  //   if (carry2) add_sub_u32_device<(TLC >> 1), true, false>(&middle_part[TLC >> 1], diffs, &middle_part[TLC >> 1]);
+  //   // Now that middle part is fully correct, it can be added to the result.
+  //   add_sub_u32_device<TLC, false, true>(&r[TLC >> 1], middle_part, &r[TLC >> 1]);
+
+  //   // Carry from adding middle part has to be propagated to the highest limb.
+  //   for (size_t i = TLC + (TLC >> 1); i < 2 * TLC; i++)
+  //     r[i] = ptx::addc_cc(r[i], 0);
+  // }
+
+template <unsigned NLIMBS_A, unsigned NLIMBS_B = NLIMBS_A, bool USE_32 = false>
 static constexpr HOST_INLINE void
 multiply_raw(const storage<NLIMBS_A>& as, const storage<NLIMBS_B>& bs, storage<NLIMBS_A + NLIMBS_B>& rs) {
-  if (IS_CONSTEXPR){
+  if (USE_32){
     multiply_raw_32<NLIMBS_A, NLIMBS_B>(as,bs,rs);
     return;
   }
@@ -165,7 +215,7 @@ multiply_raw(const storage<NLIMBS_A>& as, const storage<NLIMBS_B>& bs, storage<N
 
   template <unsigned NLIMBS, bool SUBTRACT, bool CARRY_OUT>
   static constexpr HOST_INLINE uint32_t
-  add_sub_limbs(const storage<NLIMBS>& xs, const storage<NLIMBS>& ys, storage<NLIMBS>& rs)
+  add_sub_limbs_32(const storage<NLIMBS>& xs, const storage<NLIMBS>& ys, storage<NLIMBS>& rs)
   {
     const uint32_t* x = xs.limbs;
     const uint32_t* y = ys.limbs;
@@ -175,6 +225,32 @@ multiply_raw(const storage<NLIMBS_A>& as, const storage<NLIMBS_B>& bs, storage<N
     for (unsigned i = 0; i < NLIMBS; i++)
       r[i] = SUBTRACT ? chain.sub(x[i], y[i], carry) : chain.add(x[i], y[i], carry);
     return CARRY_OUT ? carry : 0;
+  }
+
+    template <unsigned NLIMBS, bool SUBTRACT, bool CARRY_OUT>
+  static constexpr HOST_INLINE uint64_t
+  add_sub_limbs_64(const storage<NLIMBS>& xs, const storage<NLIMBS>& ys, storage<NLIMBS>& rs)
+  {
+    const uint64_t* x = xs.limbs64;
+    const uint64_t* y = ys.limbs64;
+    uint64_t* r = rs.limbs64;
+    uint64_t carry = 0;
+    carry_chain<NLIMBS, false, CARRY_OUT> chain;
+    for (unsigned i = 0; i < NLIMBS/2; i++)
+      r[i] = SUBTRACT ? chain.sub(x[i], y[i], carry) : chain.add(x[i], y[i], carry);
+    return CARRY_OUT ? carry : 0;
+  }
+
+  template <unsigned NLIMBS, bool SUBTRACT, bool CARRY_OUT, bool USE_32 = false, typename T = uint64_t>
+  static constexpr HOST_INLINE T
+  add_sub_limbs(const storage<NLIMBS>& xs, const storage<NLIMBS>& ys, storage<NLIMBS>& rs)
+  {
+    if (USE_32){
+      return add_sub_limbs_32<NLIMBS,SUBTRACT,CARRY_OUT>(xs,ys,rs);
+    }
+    else{
+      return add_sub_limbs_64<NLIMBS,SUBTRACT,CARRY_OUT>(xs,ys,rs);
+    }
   }
 
   template <unsigned NLIMBS, unsigned BITS>
@@ -213,7 +289,7 @@ multiply_raw(const storage<NLIMBS_A>& as, const storage<NLIMBS_B>& bs, storage<N
     }
   }
 
-  template <unsigned NLIMBS_NUM, unsigned NLIMBS_DENOM, unsigned NLIMBS_Q = (NLIMBS_NUM - NLIMBS_DENOM)>
+  template <unsigned NLIMBS_NUM, unsigned NLIMBS_DENOM, unsigned NLIMBS_Q = (NLIMBS_NUM - NLIMBS_DENOM), bool USE_32 = false, typename T = uint64_t>
   static constexpr HOST_INLINE void integer_division(
     const storage<NLIMBS_NUM>& num, const storage<NLIMBS_DENOM>& denom, storage<NLIMBS_Q>& q, storage<NLIMBS_DENOM>& r)
   {
@@ -222,7 +298,7 @@ multiply_raw(const storage<NLIMBS_A>& as, const storage<NLIMBS_B>& bs, storage<N
       for (int bit_idx = 31; bit_idx >= 0; bit_idx--) {
         r = left_shift<NLIMBS_DENOM, 1>(r);
         r.limbs[0] |= ((num.limbs[limb_idx] >> bit_idx) & 1);
-        uint32_t c = add_sub_limbs<NLIMBS_DENOM, true, true>(r, denom, temp);
+        T c = add_sub_limbs<NLIMBS_DENOM, true, true, USE_32, T>(r, denom, temp);
         if (limb_idx < NLIMBS_Q & !c) {
           r = temp;
           q.limbs[limb_idx] |= 1 << bit_idx;
