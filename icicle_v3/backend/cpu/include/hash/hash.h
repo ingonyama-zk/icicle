f5d9cc27bd15280cc60f2e895b8fa8fa306bc9e6
aviadingo <aviad@ingonyama.com>
2024-08-12 14:56:58 +0300

    added general purpose hasher class

---
+++ icicle_v3/backend/cpu/include/hash/hash.h
@@ -0,0 +1,131 @@
+#pragma once
+#ifndef HASH_H
+#define HASH_H
+
+#include <cassert>
+#include <vector>
+#include <stdexcept>
+#include <cstdint>
+
+/**
+ * @namespace hash
+ * Includes classes and methods for describing hash functions.
+ */
+namespace hash {
+
+  /**
+   * @struct HashConfig
+   * Encodes hash operations parameters.
+   */
+  struct HashConfig {
+    bool is_async; /**< Whether to run the hash operations asynchronously. In this CPU version, it's not used. */
+  };
+
+  /**
+   * A function that returns the default value of HashConfig.
+   * @return Default value of HashConfig.
+   */
+  static HashConfig default_hash_config()
+  {
+    HashConfig config = {
+      false, // is_async
+    };
+    return config;
+  }
+
+  /**
+   * @class Hasher
+   *
+   * An interface containing methods for hashing
+   *
+   * @tparam PreImage type of inputs elements
+   * @tparam Image type of state elements. Also used to describe the type of hash output
+   */
+  template <typename PreImage, typename Image>
+  class Hasher
+  {
+  public:
+    /// @brief the width of permutation state
+    const unsigned int width;
+
+    /// @brief how many elements a state can fit per 1 permutation. Used with domain separation.
+    const unsigned int preimage_max_length;
+
+    /// @brief portion of the state to absorb input into, or squeeze output from
+    const unsigned int rate;
+
+    /// @brief start squeezing from this offset. Used with domain separation.
+    const unsigned int offset;
+
+    Hasher(unsigned int width, unsigned int preimage_max_length, unsigned int rate, unsigned int offset)
+        : width(width), preimage_max_length(preimage_max_length), rate(rate), offset(offset)
+    {
+      assert(
+        rate * sizeof(PreImage) <= preimage_max_length * sizeof(Image) &&
+        "Input rate can not be bigger than preimage max length");
+    }
+
+    virtual void hash_2d(
+      const std::vector<std::vector<PreImage>>* inputs,
+      Image* states,
+      unsigned int number_of_inputs,
+      unsigned int output_len,
+      uint64_t number_of_rows) const
+    {
+      throw std::runtime_error("Absorb 2d is not implemented for this hash");
+    }
+
+    virtual void compress_and_inject(
+      const std::vector<std::vector<PreImage>>* matrices_to_inject,
+      unsigned int number_of_inputs,
+      uint64_t number_of_rows,
+      const Image* prev_layer,
+      Image* next_layer,
+      unsigned int digest_elements) const
+    {
+      throw std::runtime_error("Compress and inject is not implemented for this hash");
+    }
+
+    void compress_many(
+      const PreImage* input,
+      Image* output,
+      unsigned int number_of_states,
+      unsigned int output_len,
+      const HashConfig& cfg) const
+    {
+      hash_many((const PreImage*)(input), output, number_of_states, width, output_len, cfg);
+    }
+
+    virtual void run_hash_many_kernel(
+      const std::vector<PreImage>& input,
+      std::vector<Image>& output,
+      unsigned int number_of_states,
+      unsigned int input_len,
+      unsigned int output_len) const
+    {
+      throw std::runtime_error("Hash many kernel is not implemented for this hash");
+    }
+
+    virtual void run_hash(
+      const PreImage* input,
+      Image* output,
+      size_t input_len,
+      size_t output_len) const
+    {
+      throw std::runtime_error("Hash many kernel is not implemented for this hash");
+    }
+
+    void hash_many(
+      const PreImage* input,
+      Image* output,
+      unsigned int number_of_states,
+      unsigned int input_len,
+      unsigned int output_len,
+      const HashConfig& cfg) const
+    {
+      run_hash_many_kernel(input, output, number_of_states, input_len, output_len);
+    }
+  };
+} // namespace hash
+
+#endif
---
+++ icicle_v3/backend/cpu/src/hash/blake2s/test_blake2s_integ.c
@@ -0,0 +1,79 @@
+
+
+#include <iostream>
+#include <cstring>
+#include <chrono>
+#include <string>
+#include "hash/blake2/blake2.h"
+#include "hash/blake2/blake2-impl.h"
+#include "hash/hash.h"
+#include <chrono>
+
+
+// namespace blake2s_hash {
+//   extern "C" cudaError_t blake2s_hasher(
+//     BYTE* input, BYTE* output, WORD number_of_blocks, WORD input_block_size, WORD output_block_size, HashConfig& config)
+//   {
+//     return Blake2s().hash_many(input, output, number_of_blocks, input_block_size, output_block_size, config);
+//   }
+// }
+
+using namespace blake2s_hash;
+
+#define START_TIMER(timer) auto timer##_start = std::chrono::high_resolution_clock::now();
+#define END_TIMER(timer, msg) \
+  printf("%s: %.0f us\n", msg, FpMicroseconds(std::chrono::high_resolution_clock::now() - timer##_start).count());
+
+// Ensure BLAKE2S_KEYBYTES, BLAKE2S_OUTBYTES, and blake2s function are defined appropriately
+
+
+
+
+int main(void)
+{
+    using FpMilliseconds = std::chrono::duration<float, std::chrono::milliseconds::period>;
+    using FpMicroseconds = std::chrono::duration<float, std::chrono::microseconds::period>;
+    
+    // Set key to an empty string
+    const char *key = "";  // Empty string as the key
+    size_t keylen = 0;     // Length of the key is 0
+
+    // The string to hash
+    char *test_string_char = "0123456789";
+    size_t test_string_len = strlen(test_string_char);
+    const BYTE *test_string = (BYTE*)test_string_char;
+
+    // Test simple API
+    START_TIMER(blake_ref)
+    {
+        uint8_t hash[BLAKE2S_OUTBYTES];
+        // Pass the empty key with keylen = 0
+        Blake2s().run_hash(test_string, hash, test_string_len, BLAKE2S_OUTBYTES);
+
+        // Expected hash value for "0123456789" with an empty key as a string
+        const char *expected_hash_str = "410381eb72313f23f9f62478d62ec7635f4166ab5e53a20af5c9e8f7ee445de8";
+
+        // Convert computed hash to a string
+        char computed_hash_str[BLAKE2S_OUTBYTES * 2 + 1]; // Two characters per byte + null terminator
+        for (size_t i = 0; i < BLAKE2S_OUTBYTES; i++) {
+            sprintf(&computed_hash_str[i * 2], "%02x", hash[i]);
+        }
+        computed_hash_str[BLAKE2S_OUTBYTES * 2] = '\0'; // Null terminator
+
+        // Print the computed and expected hash strings
+        std::cout << "Computed hash: " << computed_hash_str << std::endl;
+        std::cout << "Expected hash: " << expected_hash_str << std::endl;
+
+        if (strcmp(computed_hash_str, expected_hash_str) != 0) {
+            std::cerr << "Hash mismatch!" << std::endl;
+            goto fail;
+        }
+    }
+    END_TIMER(blake_ref, "Blake Ref Timer")
+
+    puts("ok");
+    return 0;
+fail:
+    puts("error");
+    return -1;
+}
