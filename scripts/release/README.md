5ed3ce99555ee2ce01548d6a90910a3581d31682
Yuval Shekel <yshekel@gmail.com>
2024-08-28 17:28:55 +0300

    install cmake in ubi dockers

--- scripts/release/README.md
+++ scripts/release/README.md
@@ -13,12 +13,6 @@ To build the Docker images for each distribution and CUDA version, use the follo
 cd ./scripts/release
 # Ubuntu 22.04, CUDA 12.2.2
 docker build -t icicle-release-ubuntu22-cuda122 -f Dockerfile.ubuntu22 .
-
-# Ubuntu 20.04, CUDA 12.2.2
-docker build -t icicle-release-ubuntu20-cuda122 -f Dockerfile.ubuntu20 .
-
-# ubi7 (rhel7,centos compatible), CUDA 12.2.2
-docker build -t icicle-release-centos7-cuda122 -f Dockerfile.ubi7 .
 ```
 
 
@@ -27,27 +21,21 @@ docker build -t icicle-release-centos7-cuda122 -f Dockerfile.ubi7 .
 To build the Icicle libraries inside a Docker container and output the tar file to the `release_output` directory:
 
 ```bash
-# from icicel root dir
+# from icicle root dir
 mkdir -p release_output
 # ubuntu 22
 docker run --rm --gpus all                  \
             -v ./icicle:/icicle             \
             -v ./release_output:/output     \
             -v ./scripts:/scripts           \
-            icicle-release-ubuntu22-cuda122 bash /scripts/release/build_release_and_tar.sh icicle30 ubuntu22 cuda122
-
-# ubuntu 20
-docker run --rm --gpus all                  \
-            -v ./icicle:/icicle             \
-            -v ./release_output:/output     \
-            -v ./scripts:/scripts           \
-            icicle-release-ubuntu20-cuda122 bash /scripts/release/build_release_and_tar.sh icicle30 ubuntu20 cuda122
+            icicle-release-ubuntu22-cuda122 bash /scripts/release/build_release_and_tar.sh icicle30 ubuntu22 cuda122          
 ```
 
 This command executes the `build_release_and_tar.sh` script inside the Docker container, which provides the build environment. It maps the source code and output directory to the container, ensuring the generated tar file is available on the host system.
 
 You can replace `icicle-release-ubuntu22-cuda122` with another Docker image tag to build in the corresponding environment.
-Make sure to pass corresponding OS and CUDA version in the params `icicle30 ubuntu22 cuda122`. For example for centos7 it would be `icicle30 centos7 cuda122`.
+Make sure to pass corresponding OS and CUDA version in the params `icicle30 ubuntu22 cuda122`. For example for ubi9 it would be `icicle30 ubi9 cuda122`.
+See `build_all.sh` script for reference.
 
 
 ## Build full release
--- scripts/release/build_all.sh
+++ scripts/release/build_all.sh
@@ -2,18 +2,23 @@
 
 set -e 
 
+# from root of icicle
+# Check if both directories exist in the current working directory
+if [[ ! -d "./icicle" || ! -d "./scripts" ]]; then
+  echo "Usage: The current directory must contain both 'icicle' and 'scripts' directories. Retry from icicle root dir."
+  exit 1
+fi
+
 # Build dockers
 
 # Ubuntu 22.04, CUDA 12.2.2
-docker build -t icicle-release-ubuntu22-cuda122 -f Dockerfile.ubuntu22 .
+docker build -t icicle-release-ubuntu22-cuda122 -f ./scripts/release/Dockerfile.ubuntu22 .
 # Ubuntu 20.04, CUDA 12.2.2
-docker build -t icicle-release-ubuntu20-cuda122 -f Dockerfile.ubuntu20 .
-# ubi7 (rhel compatible), CUDA 12.2.2
-docker build -t icicle-release-ubi7-cuda122 -f Dockerfile.ubi7 .
+docker build -t icicle-release-ubuntu20-cuda122 -f ./scripts/release/Dockerfile.ubuntu20 .
 # ubi8 (rhel compatible), CUDA 12.2.2
-docker build -t icicle-release-ubi8-cuda122 -f Dockerfile.ubi8 .
+docker build -t icicle-release-ubi8-cuda122 -f ./scripts/release/Dockerfile.ubi8 .
 # ubi7 (rhel compatible), CUDA 12.2.2
-docker build -t icicle-release-ubi9-cuda122 -f Dockerfile.ubi9 .
+docker build -t icicle-release-ubi9-cuda122 -f ./scripts/release/Dockerfile.ubi9 .
 
 # compile and tar release in each
 
@@ -33,21 +38,14 @@ docker run --rm --gpus all                  \
             -v ./scripts:/scripts           \
             icicle-release-ubuntu20-cuda122 bash /scripts/release/build_release_and_tar.sh icicle30 ubuntu20 cuda122
 
-# ubi 7
-docker run --rm --gpus all                  \
-            -v ./icicle:/icicle             \
-            -v ./release_output:/output     \
-            -v ./scripts:/scripts           \
-            icicle-release-ubi7-cuda122 bash /scripts/release/build_release_and_tar.sh icicle30 ubi7 cuda122
-
-# ubi 8
+# ubi 8 (rhel compatible)
 docker run --rm --gpus all                  \
             -v ./icicle:/icicle             \
             -v ./release_output:/output     \
             -v ./scripts:/scripts           \
             icicle-release-ubi8-cuda122 bash /scripts/release/build_release_and_tar.sh icicle30 ubi8 cuda122
 
-# ubi 9
+# ubi 9 (rhel compatible)
 docker run --rm --gpus all                  \
             -v ./icicle:/icicle             \
             -v ./release_output:/output     \
