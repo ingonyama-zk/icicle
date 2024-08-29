7a62131f2f5ce87fa01e3257d00d5fcf6d0ee07e
ChickenLover <romangg81@gmail.com>
2024-08-29 16:47:55 +0700

    add two extensions for m31

--- wrappers/rust/icicle-fields/icicle-m31/src/field.rs
+++ wrappers/rust/icicle-fields/icicle-m31/src/field.rs
@@ -6,27 +6,41 @@ use icicle_runtime::memory::{DeviceSlice, HostOrDeviceSlice};
 use icicle_runtime::stream::IcicleStream;
 
 pub(crate) const SCALAR_LIMBS: usize = 1;
-pub(crate) const EXTENSION_LIMBS: usize = 4;
+pub(crate) const COMPLEX_EXTENSION_LIMBS: usize = 2;
+pub(crate) const QUARTIC_EXTENSION_LIMBS: usize = 4;
 
 impl_scalar_field!("m31", m31, SCALAR_LIMBS, ScalarField, ScalarCfg);
 impl_scalar_field!(
-    "m31_extension",
-    m31_extension,
-    EXTENSION_LIMBS,
-    ExtensionField,
-    ExtensionCfg
+    "m31_complex_extension",
+    m31_complex_extension,
+    COMPLEX_EXTENSION_LIMBS,
+    ComplexExtensionField,
+    ComplexExtensionCfg
+);
+impl_scalar_field!(
+    "m31_quartic_extension",
+    m31_quartic_extension,
+    QUARTIC_EXTENSION_LIMBS,
+    QuarticExtensionField,
+    QuarticExtensionCfg
 );
 
 #[cfg(test)]
 mod tests {
-    use super::{ExtensionField, ScalarField};
+    use super::{ComplexExtensionField, QuarticExtensionField, ScalarField};
     use icicle_core::impl_field_tests;
     use icicle_core::tests::*;
 
     impl_field_tests!(ScalarField);
-    mod extension {
+    mod complex_extension {
         use super::*;
 
-        impl_field_tests!(ExtensionField);
+        impl_field_tests!(ComplexExtensionField);
+    }
+
+    mod quartic_extension {
+        use super::*;
+
+        impl_field_tests!(QuarticExtensionField);
     }
 }
--- wrappers/rust/icicle-fields/icicle-m31/src/vec_ops/mod.rs
+++ wrappers/rust/icicle-fields/icicle-m31/src/vec_ops/mod.rs
@@ -1,4 +1,6 @@
-use crate::field::{ExtensionCfg, ExtensionField, ScalarCfg, ScalarField};
+use crate::field::{
+    ComplexExtensionCfg, ComplexExtensionField, QuarticExtensionCfg, QuarticExtensionField, ScalarCfg, ScalarField,
+};
 
 use icicle_core::impl_vec_ops_field;
 use icicle_core::vec_ops::{VecOps, VecOpsConfig};
@@ -6,19 +8,36 @@ use icicle_runtime::errors::eIcicleError;
 use icicle_runtime::memory::HostOrDeviceSlice;
 
 impl_vec_ops_field!("m31", m31, ScalarField, ScalarCfg);
-impl_vec_ops_field!("m31_extension", m31_extension, ExtensionField, ExtensionCfg);
+impl_vec_ops_field!(
+    "m31_complex_extension",
+    m31_complex_extension,
+    ComplexExtensionField,
+    ComplexExtensionCfg
+);
+impl_vec_ops_field!(
+    "m31_quartic_extension",
+    m31_quartic_extension,
+    QuarticExtensionField,
+    QuarticExtensionCfg
+);
 
 #[cfg(test)]
 pub(crate) mod tests {
-    use crate::field::{ExtensionField, ScalarField};
+    use crate::field::{ComplexExtensionField, QuarticExtensionField, ScalarField};
     use icicle_core::impl_vec_ops_tests;
     use icicle_core::vec_ops::tests::*;
 
     impl_vec_ops_tests!(ScalarField);
 
-    mod extension {
+    mod complex_extension {
         use super::*;
 
-        impl_vec_ops_tests!(ExtensionField);
+        impl_vec_ops_tests!(ComplexExtensionField);
+    }
+
+    mod quartic_extension {
+        use super::*;
+
+        impl_vec_ops_tests!(QuarticExtensionField);
     }
 }
