62bf3009e83659c1d970554060074a451c8e3a52
mickeyasa <100796045+mickeyasa@users.noreply.github.com>
2024-08-29 09:39:59 +0300

    code review

--- icicle_v3/backend/cpu/src/hash/poseidon/cpu_poseidon.cpp
+++ icicle_v3/backend/cpu/src/hash/poseidon/cpu_poseidon.cpp
@@ -1,1 +1,4 @@
 #include "cpu_poseidon.h"
+
+
+// Miki: remove this file
--- icicle_v3/backend/cpu/src/hash/poseidon/cpu_poseidon.h
+++ icicle_v3/backend/cpu/src/hash/poseidon/cpu_poseidon.h
@@ -1,5 +1,5 @@
 // #include "icicle_v3/include/icicle/fields/field.h"
-#include "/home/administrator/users/danny/github/icicle/icicle_v3/include/icicle/fields/field.h"
+#include "/home/administrator/users/danny/github/icicle/icicle_v3/include/icicle/fields/field.h" // Miki: fix path
 #include "hash.h"
 #include "icicle/fields/field_config.h"
 
@@ -14,7 +14,7 @@ namespace icicle {
 // template <typename S = scalar_t>
 template <typename S>
 class Poseidon : public Hash {
-  private:
+  private:// Miki: remove private down - public should appear first
     const unsigned int arity;     ///< Arity of this Poseidon hash.
     const unsigned int alpha;     ///< Alpha of this Poseidon hash S-box.
     const unsigned int nof_total_rounds;     ///< Total number of rounds (full + partial) of this Poseidon hash.
@@ -25,13 +25,16 @@ class Poseidon : public Hash {
                                   ///< Number of such constants is nof_total_rounds - 2 * nof_full_rounds_half.
     const S* full_round_matrix;     ///< Full rounds MDS matrix.
     const S* partial_round_matrix;  ///< Partial rounds MDS matrix.
-
+// Miki: add to member variables m_
   public:
     Poseidon (
       const unsigned int arity,
       const unsigned int alpha,
-      const unsigned int nof_total_rounds,
-      const unsigned int nof_full_rounds_half,
+      const unsigned int nof_total_rounds,    // Miki: don't you want nof_partial_rounds
+      const unsigned int nof_full_rounds_half,  // Miki: is it half or full?
+      // Miki: do we want a different number of full rounds for the begining and the end? check with Kartik
+
+      // Miki: wrap all the constants in a single struct and we'll provide them part of icicle
       const S* full_rounds_constants,
       const S* partial_rounds_constants,
       const S* full_round_matrix,
@@ -44,6 +47,7 @@ class Poseidon : public Hash {
      * @param output_limbs Pointer to the Poseidon hash output limbs.
      * @return Error code of type eIcicleError.
      */
+    // Miki: needs to be virtual
     eIcicleError run_single_hash (const limb_t *input_limbs, limb_t *output_limbs, const HashConfig& config) const;
 
     /**
@@ -53,8 +57,10 @@ class Poseidon : public Hash {
      * @param nof_hashes Number of hashes to run.
      * @return Error code of type eIcicleError.
      */
+    // Miki: needs to be virtual
     eIcicleError run_multiple_hash(const limb_t *input_limbs, limb_t *output_limbs, int nof_hashes, const HashConfig& config, const limb_t *side_input_limbs = nullptr) const;
 
+    // Miki: should be private
     void field_vec_matrix_mul(const S* vector, const S* matrix, S* result, const int nof_rows, const int num_columns) const;
 
 };
@@ -69,6 +75,7 @@ Poseidon<S>::Poseidon (
   const S* partial_rounds_constants,
   const S* full_round_matrix,
   const S* partial_round_matrix
+  // Miki: (0,0,0,) is wrong
   ) : Hash(0, 0, 0), arity(arity), alpha(alpha), nof_total_rounds(nof_total_rounds),
       nof_full_rounds_half(nof_full_rounds_half), full_rounds_constants(full_rounds_constants),
       partial_rounds_constants(partial_rounds_constants), full_round_matrix(full_round_matrix),
@@ -79,21 +86,23 @@ eIcicleError Poseidon<S>::run_single_hash (const limb_t* input_limbs, limb_t* ou
   // Casting from limbs to scalar.
   S* out_fields  = (S*)(output_limbs);
   S* in_field_op = (S*)(input_limbs);
-  const S* tmp_full_rounds_constants = full_rounds_constants;
-  const S* tmp_partial_rounds_constants = partial_rounds_constants;
+  const S* tmp_full_rounds_constants = full_rounds_constants; // Miki: why is that
+  const S* tmp_partial_rounds_constants = partial_rounds_constants; // Miki: why is that
   // Copy input scalar to the output (as a temp storage) to be used in the rounds.
   // *out_fields are used as a temp storage alog the calculations in this function.
-  for (int arity_idx=0; arity_idx<arity; arity_idx++) {
+  for (int arity_idx=0; arity_idx<arity; arity_idx++) {    // Miki: there is a perliminary calculation that is missing - you can use this calc to send result to output and avoid this for loop
     *out_fields++ = *in_field_op++;
   }
   // Upper full rounds.
   for (int full_rounds_half_idx=0; full_rounds_half_idx<nof_full_rounds_half; full_rounds_half_idx++) {
     for (int arity_idx=0; arity_idx<arity; arity_idx++) {
       // S box
+      // Miki: use array syntaxt out_fields[ariti_idx]
       *out_fields = S::pow(*out_fields, alpha);
       // Add round constants
       *out_fields = *out_fields++ + *tmp_full_rounds_constants++;
     }
+    // Miki: you didnt reset out_field so its out of memory range
     // Multiplication by matrix
     field_vec_matrix_mul(out_fields, full_round_matrix, out_fields, arity, arity);
   }
@@ -104,8 +113,11 @@ eIcicleError Poseidon<S>::run_single_hash (const limb_t* input_limbs, limb_t* ou
     // Add round constants
     *out_fields = *out_fields++ + *tmp_partial_rounds_constants++;
     // Multiplication by matrix
+    // Miki: its a sparse matrix you don't need n^2 calculations - only n
     field_vec_matrix_mul(out_fields, partial_round_matrix, out_fields, arity, arity);
   }
+
+  // Miki: use a function for partial round and call it twice  
   // Bottom full rounds.
   for (int full_rounds_half_idx=0; full_rounds_half_idx<nof_full_rounds_half; full_rounds_half_idx++) {
     for (int arity_idx=0; arity_idx<arity; arity_idx++) {
@@ -133,6 +145,7 @@ void Poseidon<S>::field_vec_matrix_mul(const S* vec_in, const S* matrix_in, S* r
   for (int i = 0; i < num_columns; i++) {
     *result == S::from(0);
     for (int j = 0; j < nof_rows; j++) {
+      // Miki: use array syntaxt 
       *result = *result + *vec_in * *matrix_in;
       vec_in++;
       matrix_in++;
