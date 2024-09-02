e25ab67f9de7c6125c4c9a88244573de0f007f84
Jeremy Felder <jeremy.felder1@gmail.com>
2024-09-02 08:33:52 +0300

    Run go generate for lib name revert

--- wrappers/golang/curves/bls12381/main.go
+++ wrappers/golang/curves/bls12381/main.go
@@ -1,4 +1,4 @@
 package bls12381
 
-// #cgo LDFLAGS: -L${SRCDIR}/../../../../build/lib -libcicle_field_bls12_381 -libcicle_curve_bls12_381 -lstdc++ -Wl,-rpath=${SRCDIR}/../../../../build/lib
+// #cgo LDFLAGS: -L${SRCDIR}/../../../../build/lib -licicle_field_bls12_381 -licicle_curve_bls12_381 -lstdc++ -Wl,-rpath=${SRCDIR}/../../../../build/lib
 import "C"
--- wrappers/golang/curves/grumpkin/main.go
+++ wrappers/golang/curves/grumpkin/main.go
@@ -1,4 +1,4 @@
 package grumpkin
 
-// #cgo LDFLAGS: -L${SRCDIR}/../../../../build/lib -libcicle_field_grumpkin -libcicle_curve_grumpkin -lstdc++ -Wl,-rpath=${SRCDIR}/../../../../build/lib
+// #cgo LDFLAGS: -L${SRCDIR}/../../../../build/lib -licicle_field_grumpkin -licicle_curve_grumpkin -lstdc++ -Wl,-rpath=${SRCDIR}/../../../../build/lib
 import "C"
