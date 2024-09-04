985ea4577f5ae6c7fe3e74190bd2213119cbcd71
Yuval Shekel <yshekel@gmail.com>
2024-09-04 17:14:21 +0300

    keccak C api

--- icicle/src/hash/keccak.cpp
+++ icicle/src/hash/keccak.cpp
@@ -9,14 +9,15 @@ namespace icicle {
   // Keccak 256
   ICICLE_DISPATCHER_INST(Keccak256Dispatcher, keccak_256_factory, KeccakFactoryImpl);
 
-  // TODO Yuval expose C API
+  extern "C" Hash* create_keccak_256_hash_c_api(uint64_t total_input_limbs)
+  {
+    return new Hash(create_keccak_256_hash(total_input_limbs));
+  }
 
   Hash create_keccak_256_hash(uint64_t total_input_limbs)
   {
     std::shared_ptr<HashBackend> backend;
-
-    // TODO Yuval: what to do with the return value??
-    auto err = Keccak256Dispatcher::execute(total_input_limbs, backend);
+    ICICLE_CHECK(Keccak256Dispatcher::execute(total_input_limbs, backend));
     Hash keccak{backend};
     return keccak;
   }
@@ -24,14 +25,15 @@ namespace icicle {
   // Keccak 512
   ICICLE_DISPATCHER_INST(Keccak512Dispatcher, keccak_512_factory, KeccakFactoryImpl);
 
-  // TODO Yuval expose C API
+  extern "C" Hash* create_keccak_512_hash_c_api(uint64_t total_input_limbs)
+  {
+    return new Hash(create_keccak_512_hash(total_input_limbs));
+  }
 
   Hash create_keccak_512_hash(uint64_t total_input_limbs)
   {
     std::shared_ptr<HashBackend> backend;
-
-    // TODO Yuval: what to do with the return value??
-    auto err = Keccak512Dispatcher::execute(total_input_limbs, backend);
+    ICICLE_CHECK(Keccak512Dispatcher::execute(total_input_limbs, backend));
     Hash keccak{backend};
     return keccak;
   }
@@ -39,14 +41,15 @@ namespace icicle {
   // Sha3 256
   ICICLE_DISPATCHER_INST(Sah3_256Dispatcher, sha3_256_factory, KeccakFactoryImpl);
 
-  // TODO Yuval expose C API
+  extern "C" Hash* create_sha3_256_hash_c_api(uint64_t total_input_limbs)
+  {
+    return new Hash(create_sha3_256_hash(total_input_limbs));
+  }
 
   Hash create_sha3_256_hash(uint64_t total_input_limbs)
   {
     std::shared_ptr<HashBackend> backend;
-
-    // TODO Yuval: what to do with the return value??
-    auto err = Keccak256Dispatcher::execute(total_input_limbs, backend);
+    ICICLE_CHECK(Keccak256Dispatcher::execute(total_input_limbs, backend));
     Hash keccak{backend};
     return keccak;
   }
@@ -54,14 +57,15 @@ namespace icicle {
   // Keccak 512
   ICICLE_DISPATCHER_INST(Sah3_512Dispatcher, sha3_512_factory, KeccakFactoryImpl);
 
-  // TODO Yuval expose C API
+  extern "C" Hash* create_sha3_512_hash_c_api(uint64_t total_input_limbs)
+  {
+    return new Hash(create_sha3_512_hash(total_input_limbs));
+  }
 
   Hash create_sha3_512_hash(uint64_t total_input_limbs)
   {
     std::shared_ptr<HashBackend> backend;
-
-    // TODO Yuval: what to do with the return value??
-    auto err = Keccak512Dispatcher::execute(total_input_limbs, backend);
+    ICICLE_CHECK(Keccak512Dispatcher::execute(total_input_limbs, backend));
     Hash keccak{backend};
     return keccak;
   }
