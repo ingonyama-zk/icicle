4dc4b2f46936728baacfb38709f4c834fda409d1
Yuval Shekel <yshekel@gmail.com>
2024-09-04 18:00:22 +0300

    minor

--- icicle/src/hash/keccak.cpp
+++ icicle/src/hash/keccak.cpp
@@ -4,15 +4,8 @@
 
 namespace icicle {
 
-  /*************************** Keccak ***************************/
-
   // Keccak 256
   ICICLE_DISPATCHER_INST(Keccak256Dispatcher, keccak_256_factory, KeccakFactoryImpl);
-
-  extern "C" Hash* create_keccak_256_hash_c_api(uint64_t total_input_limbs)
-  {
-    return new Hash(create_keccak_256_hash(total_input_limbs));
-  }
 
   Hash create_keccak_256_hash(uint64_t total_input_limbs)
   {
@@ -25,11 +18,6 @@ namespace icicle {
   // Keccak 512
   ICICLE_DISPATCHER_INST(Keccak512Dispatcher, keccak_512_factory, KeccakFactoryImpl);
 
-  extern "C" Hash* create_keccak_512_hash_c_api(uint64_t total_input_limbs)
-  {
-    return new Hash(create_keccak_512_hash(total_input_limbs));
-  }
-
   Hash create_keccak_512_hash(uint64_t total_input_limbs)
   {
     std::shared_ptr<HashBackend> backend;
@@ -40,11 +28,6 @@ namespace icicle {
 
   // Sha3 256
   ICICLE_DISPATCHER_INST(Sah3_256Dispatcher, sha3_256_factory, KeccakFactoryImpl);
-
-  extern "C" Hash* create_sha3_256_hash_c_api(uint64_t total_input_limbs)
-  {
-    return new Hash(create_sha3_256_hash(total_input_limbs));
-  }
 
   Hash create_sha3_256_hash(uint64_t total_input_limbs)
   {
@@ -57,17 +40,31 @@ namespace icicle {
   // Keccak 512
   ICICLE_DISPATCHER_INST(Sah3_512Dispatcher, sha3_512_factory, KeccakFactoryImpl);
 
-  extern "C" Hash* create_sha3_512_hash_c_api(uint64_t total_input_limbs)
-  {
-    return new Hash(create_sha3_512_hash(total_input_limbs));
-  }
-
   Hash create_sha3_512_hash(uint64_t total_input_limbs)
   {
     std::shared_ptr<HashBackend> backend;
     ICICLE_CHECK(Keccak512Dispatcher::execute(total_input_limbs, backend));
     Hash keccak{backend};
     return keccak;
+  }
+
+  /*************************** C API ***************************/
+
+  extern "C" Hash* create_keccak_256_hash_c_api(uint64_t total_input_limbs)
+  {
+    return new Hash(create_keccak_256_hash(total_input_limbs));
+  }
+  extern "C" Hash* create_keccak_512_hash_c_api(uint64_t total_input_limbs)
+  {
+    return new Hash(create_keccak_512_hash(total_input_limbs));
+  }
+  extern "C" Hash* create_sha3_256_hash_c_api(uint64_t total_input_limbs)
+  {
+    return new Hash(create_sha3_256_hash(total_input_limbs));
+  }
+  extern "C" Hash* create_sha3_512_hash_c_api(uint64_t total_input_limbs)
+  {
+    return new Hash(create_sha3_512_hash(total_input_limbs));
   }
 
 } // namespace icicle
