1c57852c5f6c02ab7253688ad0755ae23b95ae44
Yuval Shekel <yshekel@gmail.com>
2024-09-04 14:52:17 +0300

    introduce base hash C++ API

---
+++ icicle/tests/test_hash_api.cpp
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <random>
+
+#include "icicle/runtime.h"
+#include "icicle/utils/log.h"
+#include "icicle/hash/hash.h"
+
+// using namespace field_config;
+using namespace icicle;
+
+using FpMicroseconds = std::chrono::duration<float, std::chrono::microseconds::period>;
+#define START_TIMER(timer) auto timer##_start = std::chrono::high_resolution_clock::now();
+#define END_TIMER(timer, msg, enable)                                                                                  \
+  if (enable)                                                                                                          \
+    printf(                                                                                                            \
+      "%s: %.3f ms\n", msg, FpMicroseconds(std::chrono::high_resolution_clock::now() - timer##_start).count() / 1000);
+
+static bool VERBOSE = true;
+static inline std::string s_main_target;
+static inline std::string s_reference_target;
+
+class HashApiTest : public ::testing::Test
+{
+public:
+  // SetUpTestSuite/TearDownTestSuite are called once for the entire test suite
+  static void SetUpTestSuite()
+  {
+#ifdef BACKEND_BUILD_DIR
+    setenv("ICICLE_BACKEND_INSTALL_DIR", BACKEND_BUILD_DIR, 0 /*=replace*/);
+#endif
+    icicle_load_backend_from_env_or_default();
+
+    const bool is_cuda_registered = is_device_registered("CUDA");
+    if (!is_cuda_registered) { ICICLE_LOG_ERROR << "CUDA device not found. Testing CPU vs CPU"; }
+    s_main_target = is_cuda_registered ? "CUDA" : "CPU";
+    s_reference_target = "CPU";
+  }
+  static void TearDownTestSuite()
+  {
+    // make sure to fail in CI if only have one device
+    // ICICLE_ASSERT(is_device_registered("CUDA")) << "missing CUDA backend";
+  }
+
+  // SetUp/TearDown are called before and after each test
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+TEST_F(HashApiTest, TodoRename) {
+
+    ICICLE_LOG_INFO << "hello world from hash test";
+
+}
