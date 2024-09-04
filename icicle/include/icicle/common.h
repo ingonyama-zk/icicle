d399cee29844f33ab2c0f8371926492759218360
Yuval Shekel <yshekel@gmail.com>
2024-09-04 17:07:47 +0300

    add keccak256,512 and sha3-256,512 frontend APIs and CPU mock implementation

--- icicle/include/icicle/common.h
+++ icicle/include/icicle/common.h
@@ -1,8 +1,8 @@
-#pragma once 
+#pragma once
 
 namespace icicle {
 
-// Define limb type for hashing
-using limb_t = uint32_t; 
+  // Define limb type for hashing
+  using limb_t = uint32_t;
 
 } // namespace icicle
