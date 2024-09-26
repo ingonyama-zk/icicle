f0819551aacd7680103b108e5fbd297f6ff2481f
Yuval Shekel <yuval.shekel@ingonyama.com>
2024-09-26 13:22:53 +0300

    async copy

--- examples/rust/arkworks/src/main.rs
+++ examples/rust/arkworks/src/main.rs
@@ -1,11 +1,14 @@
 use ark_bn254::Fr; // Fr is the scalar field of BN254
-use ark_ff::{BigInteger, PrimeField, UniformRand}; // Added BigInteger trait import
+use ark_ff::{PrimeField, UniformRand}; // Added BigInteger trait import
 use rand::thread_rng;
 use std::time::Instant;
 
 use icicle_bn254::curve::ScalarField as IcicleBn254Scalar;
 use icicle_core::traits::{FieldImpl, MontgomeryConvertible};
-use icicle_runtime::{memory::HostSlice, stream::IcicleStream};
+use icicle_runtime::{
+    memory::{DeviceVec, HostSlice},
+    stream::IcicleStream,
+};
 
 fn randomize_ark_scalars(size: usize) -> Vec<Fr> {
     let mut rng = thread_rng();
@@ -42,47 +45,70 @@ where
 }
 
 // Function to copy a slice of Arkworks scalar elements to Icicle scalar elements.
-fn copy_ark_scalars_slice<T, I>(ark_scalars: &[T]) -> Vec<I>
+fn copy_ark_scalars_slice_async<T, I>(ark_scalars: &[T], stream: &IcicleStream) -> DeviceVec<I>
 where
     T: PrimeField,
     I: FieldImpl + MontgomeryConvertible,
 {
-    ark_scalars
-        .iter()
-        .map(|ark_scalar| {
-            let ark_bytes: Vec<u8> = ark_scalar
-                .to_base_prime_field_elements()
-                .map(|x| x.into_bigint().to_bytes_le()) // Get little-endian byte representation
-                .flatten()
-                .collect();
+    // SAFETY: Reinterpreting Arkworks field elements as Icicle-specific scalars
+    let icicle_scalars = unsafe { &*(&ark_scalars[..] as *const _ as *const [I]) };
 
-            // Convert the bytes into Icicle scalar type
-            I::from_bytes_le(&ark_bytes)
-        })
-        .collect() // Collect all Icicle scalars into a Vec
+    // Create a HostSlice from the mutable slice
+    let icicle_host_slice = HostSlice::from_slice(&icicle_scalars[..]);
+
+    let mut icicle_scalars = DeviceVec::<I>::device_malloc_async(ark_scalars.len(), &stream).unwrap();
+    icicle_scalars
+        .copy_from_host(&icicle_host_slice)
+        .unwrap();
+
+    // Convert from Montgomery representation using the Icicle type's conversion method
+    I::from_mont(&mut icicle_scalars[..], &stream);
+
+    // Return the mutable slice of Icicle scalars
+    icicle_scalars
+}
+
+fn copy_ark_scalars_slice<T, I>(ark_scalars: &[T]) -> DeviceVec<I>
+where
+    T: PrimeField,
+    I: FieldImpl + MontgomeryConvertible,
+{
+    copy_ark_scalars_slice_async(ark_scalars, &IcicleStream::default())
 }
 
 fn main() {
     //============================================================================================//
     //================================ Part 1: transmute ark scalars =============================//
     //============================================================================================//
-    let mut ark_scalars = incremental_ark_scalars(1 << 15);
+    let mut ark_scalars = incremental_ark_scalars(1 << 20);
     // println!("Ark scalars (incremental): {:?}\n", ark_scalars);
 
-    let start_transmute = Instant::now(); // Start timing
+    // Note that this is reusing the ark-works owned memory and mutates in places (convert from Montgomery representation
+    let start_transmute = Instant::now(); // Start timing                                          )
     let icicle_scalars: &mut [IcicleBn254Scalar] = transmute_ark_scalars_slice(&mut ark_scalars);
     let duration_transmute = start_transmute.elapsed(); // End timing
+    println!("Time taken for transmuting: {:?}", duration_transmute);
 
     // println!("ICICLE elements (transmuted): {:?}\n", icicle_scalars);
-    println!("Time taken for transmuting: {:?}", duration_transmute);
 
     //============================================================================================//
     //================================ Part 2: copy ark scalars ==================================//
     //============================================================================================//
     let start_copy = Instant::now(); // Start timing
-    let icicle_scalars: Vec<IcicleBn254Scalar> = copy_ark_scalars_slice(&ark_scalars);
+    let icicle_scalars: DeviceVec<IcicleBn254Scalar> = copy_ark_scalars_slice(&ark_scalars);
     let duration_copy = start_copy.elapsed(); // End timing
+    println!("Time taken for copying: {:?}", duration_copy);
+
+    // Can dispatch the copy and convert to a stream without blocking the CPU thread (for supported device such as CUDA)
+    let start_copy = Instant::now(); // Start timing
+    let mut stream = IcicleStream::create().unwrap();
+    let icicle_scalars: DeviceVec<IcicleBn254Scalar> = copy_ark_scalars_slice_async(&ark_scalars, &stream);
+    let duration_copy = start_copy.elapsed(); // End timing
+    println!("Time taken for dispatching async copy: {:?}", duration_copy);
 
     // println!("ICICLE scalar (copied): {:?}\n", icicle_scalars);
-    println!("Time taken for copying: {:?}", duration_copy);
+
+    stream
+        .destroy()
+        .unwrap();
 }
