47ca703948ef8b00e08e997a8571c344d1b12eb6
Yuval Shekel <yuval.shekel@ingonyama.com>
2024-09-26 12:54:37 +0300

    copy ark scalars

--- examples/rust/arkworks/src/main.rs
+++ examples/rust/arkworks/src/main.rs
@@ -1,8 +1,10 @@
 use ark_bn254::Fr; // Fr is the scalar field of BN254
-use ark_ff::UniformRand; // Trait that provides the `rand` method
+use ark_ec::short_weierstrass::{Affine as ArkAffine, Projective as ArkProjective, SWCurveConfig};
+use ark_ec::AffineRepr;
+use ark_ff::{BigInteger, PrimeField, UniformRand}; // Trait that provides the `rand` method
 use rand::thread_rng;
 
-use icicle_bn254::curve::ScalarField as IcicleBn254Scalar;
+use icicle_bn254::curve::{G1Affine as IcicleBn254G1, ScalarField as IcicleBn254Scalar};
 use icicle_core::traits::FieldImpl;
 use icicle_core::traits::MontgomeryConvertible;
 use icicle_runtime::{memory::HostSlice, stream::IcicleStream};
@@ -23,7 +25,8 @@ fn incremental_ark_scalars(size: usize) -> Vec<Fr> {
 }
 
 // Generic function to convert Arkworks field elements to Icicle format and return a mutable slice
-fn transmute_ark_to_icicle_scalars<T, I>(ark_scalars: &mut [T]) -> &mut [I]
+// This is a zero-copy method where the memory owned by arkworks can be modified read/write in icicle
+fn transmute_ark_scalars_slice<T, I>(ark_scalars: &mut [T]) -> &mut [I]
 where
     T: ark_ff::PrimeField,                // Arkworks field element must implement PrimeField
     I: FieldImpl + MontgomeryConvertible, // Icicle scalar must implement a trait to convert from Arkworks type
@@ -41,6 +44,33 @@ where
     icicle_scalars
 }
 
+// Alternatively can copy and own the memory in icicle
+// Function to copy a slice of Arkworks scalar elements to Icicle scalar elements.
+fn copy_ark_scalars_slice<T, I>(ark_scalars: &[T]) -> Vec<I>
+where
+    T: PrimeField,
+    I: FieldImpl + MontgomeryConvertible,
+{
+    ark_scalars
+        .iter()
+        .map(|ark_scalar| {
+            // Convert each scalar element to bytes in little-endian form
+            let ark_bytes: Vec<u8> = ark_scalar
+                .to_base_prime_field_elements()
+                .map(|x| x.into_bigint().to_bytes_le()) // Get little-endian byte representation
+                .flatten()
+                .collect();
+
+            // Convert the bytes into Icicle scalar type
+            I::from_bytes_le(&ark_bytes)
+        })
+        .collect() // Collect all Icicle scalars into a Vec
+}
+
+// fn convert_ark_to_icicle_affine<T>(ark_affine: &mut [T]) {
+//     // IcicleBn254G1
+// }
+
 fn main() {
     //============================================================================================//
     //================================ Part 1: transmute ark scalars =============================//
@@ -49,6 +79,14 @@ fn main() {
     let mut ark_scalars = incremental_ark_scalars(10);
     println!("ark elements: {:?}\n", ark_scalars);
 
-    let icicle_scalars = transmute_ark_to_icicle_scalars::<Fr, IcicleBn254Scalar>(&mut ark_scalars);
+    let icicle_scalars: &mut [IcicleBn254Scalar] = transmute_ark_scalars_slice(&mut ark_scalars);
     println!("ICICLE elements: {:?}\n", icicle_scalars);
+
+    //============================================================================================//
+    //================================ Part 2: copy ark scalars =============================//
+    //============================================================================================//
+    let ark_scalars = [Fr::from(5 as u64)];
+    println!("ark scalar=: {:?}\n", ark_scalars);
+    let icicle_scalars: Vec<IcicleBn254Scalar> = copy_ark_scalars_slice(&ark_scalars);
+    println!("ICICLE scalar=: {:?}\n", icicle_scalars);
 }
