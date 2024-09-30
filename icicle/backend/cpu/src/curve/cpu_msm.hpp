1da4536c60b179145e7bf6620abeb7261b690367
yshekel <yshekel@gmail.com>
2024-09-30 18:44:15 +0300

    Arkworks conversion example (#619)
    This PR:
    (1) introduces a new Rust example for converting Arkworks types to
    ICICLE and back. It is meant to be used as reference for users using
    ICICLE.
    (2) update to/from Montgomery trait and implementations to support
    HostOrDeviceSlice instead of DeviceSlice only so that it can work for
    host data as well.
    (3) choose better c for cpu msm for way faster cpu (for non precompute
    case)

--- icicle/backend/cpu/src/curve/cpu_msm.hpp
+++ icicle/backend/cpu/src/curve/cpu_msm.hpp
@@ -191,6 +191,14 @@ public:
   void run_msm(
     const scalar_t* scalars, const A* bases, const unsigned int msm_size, const unsigned int batch_idx, P* results);
 
+  static unsigned get_optimal_c(unsigned msm_size, bool precompute_factor)
+  {
+    // This seems to be working well but not clear why
+    // TODO Koren: find optimal c. Maybe can use a lookup table if the logic is not clear but consistent for intel/ARM
+    return precompute_factor > 1 ? std::max((int)std::log2(msm_size) - 1, 8)
+                                 : std::max((int)std::log2(msm_size) - 5, 8);
+  }
+
 private:
   TasksManager<EcAddTask<A, P>> manager; // Tasks manager for multithreading
 
@@ -591,10 +599,10 @@ eIcicleError cpu_msm(
   const Device& device, const scalar_t* scalars, const A* bases, int msm_size, const MSMConfig& config, P* results)
 {
   int c = config.c;
-  if (c < 1) { c = std::max((int)std::log2(msm_size) - 1, 8); }
+  if (c < 1) { c = Msm<A, P>::get_optimal_c(msm_size, config.precompute_factor); }
   if (scalar_t::NBITS % c == 0) {
-    ICICLE_LOG_DEBUG << "Currerntly c (" << c << ") mustn't divide scalar width (" << scalar_t::NBITS
-                     << ") without remainder.\n";
+    ICICLE_LOG_WARNING << "MSM c (" << c << ") mustn't divide scalar width (" << scalar_t::NBITS
+                       << ") without remainder. Incrementing to a higher c";
   }
   while (scalar_t::NBITS % c == 0) {
     c++;
@@ -638,7 +646,7 @@ eIcicleError cpu_msm_precompute_bases(
   A* output_bases) // Pre assigned?
 {
   int c = config.c;
-  if (c < 1) { c = std::max((int)std::log2(nof_bases) - 1, 8); }
+  if (c < 1) { c = Msm<A, P>::get_optimal_c(nof_bases, config.precompute_factor); }
   if (scalar_t::NBITS % c == 0) {
     ICICLE_LOG_ERROR << "Currerntly c (" << c << ") mustn't divide scalar width (" << scalar_t::NBITS
                      << ") without remainder.\n";
