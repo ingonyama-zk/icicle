edb0f85751509fb7f8816a395b1255edd34cc813
emirsoyturk <emir@ingonyama.com>
2024-11-04 15:46:03 +0200

    compare with stwo result

--- wrappers/rust/icicle-hash/src/blake2s/tests.rs
+++ wrappers/rust/icicle-hash/src/blake2s/tests.rs
@@ -9,27 +9,31 @@ pub(crate) mod tests {
     use crate::blake2s::{blake2s, build_blake2s_merkle_tree};
 
     #[test]
-    fn blake2s_hash_test() {
+    fn single_hash_test() {
         let config = HashConfig::default();
-        let input_block_len = 136;
-        let number_of_hashes = 1024;
-        let output_block_len = 32;
 
-        let preimages = vec![1u8; number_of_hashes * input_block_len];
-        let mut digests = vec![0u8; number_of_hashes * 32];
+        let preimages = b"a";
+        let mut digests = vec![0u8; 1 * 32];
 
-        let preimages_slice = HostSlice::from_slice(&preimages);
+        let preimages_slice = HostSlice::from_slice(preimages);
         let digests_slice = HostSlice::from_mut_slice(&mut digests);
 
         blake2s(
             preimages_slice,
-            input_block_len as u32,
-            number_of_hashes as u32,
-            output_block_len as u32,
+            1 as u32,
+            1 as u32,
+            32 as u32,
             digests_slice,
             &config,
         )
         .unwrap();
+
+        let hex_string: String = digests_slice.iter().map(|byte| format!("{:02x}", byte)).collect();
+
+        assert_eq!(
+            hex_string,
+            "4a0d129873403037c2cd9b9048203687f6233fb6738956e0349bd4320fec3e90"
+        );
     }
 
     #[test]
