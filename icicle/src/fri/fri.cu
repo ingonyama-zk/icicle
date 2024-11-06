66998d23264a18f6cfaa0392d2b852d465b6ba3b
Jeremy Felder <jeremy.felder1@gmail.com>
2024-11-06 13:17:51 +0200

    Fix thread and block for fri kernels

--- icicle/src/fri/fri.cu
+++ icicle/src/fri/fri.cu
@@ -109,8 +109,8 @@ namespace fri {
       d_folded_eval = folded_eval;
     }
 
-    uint64_t num_threads = 256;
-    uint64_t num_blocks = (n / 2 + num_threads - 1) / num_threads;
+    uint64_t num_threads = max(1, min(unsigned(n), 256));
+    uint64_t num_blocks = (n + num_threads - 1) / num_threads;
     fold_line_kernel<<<num_blocks, num_threads, 0, stream>>>(d_eval, d_domain_xs, alpha, d_folded_eval, n);
 
     // Move folded_eval back to host if requested
@@ -132,37 +132,24 @@ namespace fri {
   {
     CHK_INIT_IF_RETURN();
 
-    // std::ostringstream oss;
-
     cudaStream_t& stream = cfg.ctx.stream;
     // Allocate and move circle domain evals to device if necessary
     E* d_eval;
     if (!cfg.are_evals_on_device) {
-      // oss << "inputs: " << n << " ";
-      // for (int i = 0; i < n; i++) {
-      //   oss << eval[i] << " ";
-      // }
-
       auto data_size = sizeof(E) * n;
       CHK_IF_RETURN(cudaMallocAsync(&d_eval, data_size, stream));
       CHK_IF_RETURN(cudaMemcpyAsync(d_eval, eval, data_size, cudaMemcpyHostToDevice, stream));
     } else {
-      // oss << "inputs are on device: " << n << " ";
       d_eval = eval;
     }
 
     // Allocate and move domain's elements to device if necessary
     S* d_domain_ys;
     if (!cfg.are_domain_elements_on_device) {
-      // oss << " domain: " << n / 2 << " ";
-      // for (int i = 0; i < n / 2; i++) {
-      //   oss << domain_ys[i] << " ";
-      // }
       auto data_size = sizeof(S) * n / 2;
       CHK_IF_RETURN(cudaMallocAsync(&d_domain_ys, data_size, stream));
       CHK_IF_RETURN(cudaMemcpyAsync(d_domain_ys, domain_ys, data_size, cudaMemcpyHostToDevice, stream));
     } else {
-      // oss << "domain els are on device: " << n / 2 << " ";
       d_domain_ys = domain_ys;
     }
 
@@ -177,52 +164,22 @@ namespace fri {
     E alpha_sq = alpha * alpha;
     auto alpha_ptr = (const uint32_t*)alpha;
     auto alpha_sqr_ptr = (const uint32_t*)alpha_sq;
-    // printf(
-    //   "alpha: (r: 0x%08x im1: 0x%08x im2: 0x%08x im3: 0x%08x) alpha_sq: (r: 0x%08x im1: 0x%08x im2: 0x%08x im3: "
-    //   "0x%08x)\n",
-    //   alpha_ptr[0], alpha_ptr[1], alpha_ptr[2], alpha_ptr[3], alpha_sqr_ptr[0], alpha_sqr_ptr[1], alpha_sqr_ptr[2],
-    //   alpha_sqr_ptr[3]);
 
-    ///////
-    // E val_qm_values[3] = {{1, 2, 3, 4}, {2, 1, 0, 3}, {2, 0, 0, 2}};
-    // printf("\n==========\n");
-    // for (int i = 0; i < 3; ++i) {
-    //   E val_qm = val_qm_values[i];
-    //   // E val_qm = {val_qm_values[i][0], val_qm_values[i][1], val_qm_values[i][2], val_qm_values[i][3]};
-    //   E val_qm_sq = val_qm * val_qm;
-    //   alpha_ptr = (const uint32_t*)val_qm;
-    //   alpha_sqr_ptr = (const uint32_t*)val_qm_sq;
-
-    //   printf(
-    //     "\nval_qm: (r: 0x%08x im1: 0x%08x im2: 0x%08x im3: 0x%08x) val_sqr: (r: 0x%08x%08x%08x%08x)\n==========\n",
-    //     alpha_ptr[0], alpha_ptr[1], alpha_ptr[2], alpha_ptr[3], alpha_sqr_ptr[0], alpha_sqr_ptr[1],
-    //     alpha_sqr_ptr[2], alpha_sqr_ptr[3]);
-    // }
-
-    uint64_t num_threads = 256;
-    uint64_t num_blocks = (n / 2 + num_threads - 1) / num_threads;
-
+    uint64_t num_threads = max(1, min(unsigned(n), 256));
+    uint64_t num_blocks = (n + num_threads - 1) / num_threads;
     fold_circle_into_line_kernel<<<num_blocks, num_threads, 0, stream>>>(
       d_eval, d_domain_ys, alpha, alpha_sq, d_folded_eval, n);
 
     // Move folded_evals back to host if requested
     if (!cfg.are_results_on_device) {
-      // oss << " folded_eval:";
-      // for (int i = 0; i < n / 2; i++) {
-      //   oss << folded_eval[i] << " ";
-      // }
       CHK_IF_RETURN(cudaMemcpyAsync(folded_eval, d_folded_eval, sizeof(E) * n / 2, cudaMemcpyDeviceToHost, stream));
       CHK_IF_RETURN(cudaFreeAsync(d_folded_eval, stream));
-    } else {
-      // oss << "result els are on device: " << n / 2 << " ";
     }
     if (!cfg.are_domain_elements_on_device) { CHK_IF_RETURN(cudaFreeAsync(d_domain_ys, stream)); }
     if (!cfg.are_evals_on_device) { CHK_IF_RETURN(cudaFreeAsync(d_eval, stream)); }
 
     // Sync if stream is default stream
     if (stream == 0) CHK_IF_RETURN(cudaStreamSynchronize(stream));
-
-    // printf("output: %s\n", oss.str().c_str());
 
     return CHK_LAST();
   }
