36314e490bc13c0545930c0aea05c500a575253c
Stas Polonsky <stas@ingonyama.com>
2024-11-12 15:39:39 -0300

    android: curve.cmake

--- icicle/cmake/curve.cmake
+++ icicle/cmake/curve.cmake
@@ -58,7 +58,14 @@ function(setup_curve_target CURVE CURVE_INDEX FEATURES_STRING)
   # Add additional feature handling calls here
 
   set_target_properties(icicle_curve PROPERTIES OUTPUT_NAME "icicle_curve_${CURVE}")
-  target_link_libraries(icicle_curve PUBLIC icicle_device icicle_field pthread)
+  
+  if(NOT ANDROID)
+    target_link_libraries(icicle_curve PUBLIC icicle_device icicle_field pthread)
+  else()
+    # Android doesn't need pthread, it's already included in the system
+    target_link_libraries(icicle_curve PUBLIC icicle_device icicle_field)
+    target_link_libraries(icicle_curve PRIVATE ${log-lib})
+  endif()
 
   # Ensure CURVE is defined in the cache for backends to see
   set(CURVE "${CURVE}" CACHE STRING "")
