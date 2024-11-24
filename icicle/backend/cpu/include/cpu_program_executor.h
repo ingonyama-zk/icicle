6ca80e95f81820c3ceb99f2e47e5953dc8ab82b6
mickeyasa <100796045+mickeyasa@users.noreply.github.com>
2024-11-24 16:41:08 +0200

    format

--- icicle/backend/cpu/include/cpu_program_executor.h
+++ icicle/backend/cpu/include/cpu_program_executor.h
@@ -7,25 +7,26 @@
 
 namespace icicle {
 
-template <typename S>
-class CpuProgramExecutor {
+  template <typename S>
+  class CpuProgramExecutor
+  {
   public:
-    CpuProgramExecutor(Program<S>& program) : 
-      m_program(program),
-      m_variable_ptrs(program.get_nof_vars()),
-      m_intermidites(program.m_nof_intermidiates) {
-        // initialize m_variable_ptrs vector
-        int variable_ptrs_idx = program.m_nof_inputs + program.m_nof_outputs;
-        for (int idx=0; idx<program.m_nof_constants; ++idx) {
-          m_variable_ptrs[variable_ptrs_idx++] = &(program.m_constants[idx]);
-        }
-        for (int idx=0; idx<program.m_nof_intermidiates; ++idx) {
-          m_variable_ptrs[variable_ptrs_idx++] = &(m_intermidites[idx]);
-        }
+    CpuProgramExecutor(Program<S>& program)
+        : m_program(program), m_variable_ptrs(program.get_nof_vars()), m_intermidites(program.m_nof_intermidiates)
+    {
+      // initialize m_variable_ptrs vector
+      int variable_ptrs_idx = program.m_nof_inputs + program.m_nof_outputs;
+      for (int idx = 0; idx < program.m_nof_constants; ++idx) {
+        m_variable_ptrs[variable_ptrs_idx++] = &(program.m_constants[idx]);
       }
+      for (int idx = 0; idx < program.m_nof_intermidiates; ++idx) {
+        m_variable_ptrs[variable_ptrs_idx++] = &(m_intermidites[idx]);
+      }
+    }
 
     // execute the program an return a program to the result
-    void execute () {
+    void execute()
+    {
       const std::byte* instruction;
       for (InstructionType instruction : m_program.m_instructions) {
         const int func_select = (instruction & 0xFF);
@@ -33,61 +34,63 @@ class CpuProgramExecutor {
       }
     }
 
+    std::vector<S*> m_variable_ptrs;
 
-  std::vector <S*> m_variable_ptrs;
-  
   private:
     Program<S> m_program;
-    std::vector <S>  m_intermidites;
+    std::vector<S> m_intermidites;
 
     // exe functions
-    void exe_add(const InstructionType instruction) {
+    void exe_add(const InstructionType instruction)
+    {
       const std::byte* inst_arr = reinterpret_cast<const std::byte*>(&instruction);
-      *m_variable_ptrs[(int)inst_arr[3]] = *m_variable_ptrs[(int)inst_arr[1]] + *m_variable_ptrs[(int)inst_arr[2]]; 
+      *m_variable_ptrs[(int)inst_arr[3]] = *m_variable_ptrs[(int)inst_arr[1]] + *m_variable_ptrs[(int)inst_arr[2]];
     }
 
-
-    void exe_mult(const InstructionType instruction) {
+    void exe_mult(const InstructionType instruction)
+    {
       const std::byte* inst_arr = reinterpret_cast<const std::byte*>(&instruction);
-      *m_variable_ptrs[(int)inst_arr[3]] = *m_variable_ptrs[(int)inst_arr[1]] * *m_variable_ptrs[(int)inst_arr[2]]; 
+      *m_variable_ptrs[(int)inst_arr[3]] = *m_variable_ptrs[(int)inst_arr[1]] * *m_variable_ptrs[(int)inst_arr[2]];
     }
 
-    void exe_sub(const InstructionType instruction) {
+    void exe_sub(const InstructionType instruction)
+    {
       const std::byte* inst_arr = reinterpret_cast<const std::byte*>(&instruction);
-      *m_variable_ptrs[(int)inst_arr[3]] = *m_variable_ptrs[(int)inst_arr[1]] - *m_variable_ptrs[(int)inst_arr[2]]; 
+      *m_variable_ptrs[(int)inst_arr[3]] = *m_variable_ptrs[(int)inst_arr[1]] - *m_variable_ptrs[(int)inst_arr[2]];
     }
 
-    void exe_inverse(const InstructionType instruction) {
+    void exe_inverse(const InstructionType instruction)
+    {
       const std::byte* inst_arr = reinterpret_cast<const std::byte*>(&instruction);
-      *m_variable_ptrs[(int)inst_arr[3]] = S::inverse(*m_variable_ptrs[(int)inst_arr[1]]); 
+      *m_variable_ptrs[(int)inst_arr[3]] = S::inverse(*m_variable_ptrs[(int)inst_arr[1]]);
     }
 
-    void exe_predef_ab_minus_c(const InstructionType instruction) {
+    void exe_predef_ab_minus_c(const InstructionType instruction)
+    {
       const std::byte* inst_arr = reinterpret_cast<const std::byte*>(&instruction);
       const S& a = *m_variable_ptrs[0];
       const S& b = *m_variable_ptrs[1];
       const S& c = *m_variable_ptrs[2];
-      *m_variable_ptrs[3] = a*b - c;
+      *m_variable_ptrs[3] = a * b - c;
     }
-    void exe_predef_eq_x_ab_minus_c(const InstructionType instruction) {
+    void exe_predef_eq_x_ab_minus_c(const InstructionType instruction)
+    {
       const std::byte* inst_arr = reinterpret_cast<const std::byte*>(&instruction);
       const S& a = *m_variable_ptrs[0];
       const S& b = *m_variable_ptrs[1];
       const S& c = *m_variable_ptrs[2];
       const S& eq = *m_variable_ptrs[3];
-      *m_variable_ptrs[4] = eq*(a*b - c);
+      *m_variable_ptrs[4] = eq * (a * b - c);
     }
 
-    using FunctionPtr = void(CpuProgramExecutor::*)(const InstructionType);
+    using FunctionPtr = void (CpuProgramExecutor::*)(const InstructionType);
     inline static const FunctionPtr m_function_arr[] = {
-      &CpuProgramExecutor::exe_add,  // OP_ADD
-      &CpuProgramExecutor::exe_mult, // OP_MULT
-      &CpuProgramExecutor::exe_sub,  // OP_SUB
-      &CpuProgramExecutor::exe_inverse,   // OP_INV      
+      &CpuProgramExecutor::exe_add,     // OP_ADD
+      &CpuProgramExecutor::exe_mult,    // OP_MULT
+      &CpuProgramExecutor::exe_sub,     // OP_SUB
+      &CpuProgramExecutor::exe_inverse, // OP_INV
       // pre defined functions
-      &CpuProgramExecutor::exe_predef_ab_minus_c,
-      &CpuProgramExecutor::exe_predef_eq_x_ab_minus_c
-    };
-};
+      &CpuProgramExecutor::exe_predef_ab_minus_c, &CpuProgramExecutor::exe_predef_eq_x_ab_minus_c};
+  };
 
 } // namespace icicle
--- icicle/include/icicle/program/symbol.h
+++ icicle/include/icicle/program/symbol.h
@@ -5,83 +5,81 @@
 
 namespace icicle {
 
-/**
- * @brief Enum to represent all the operation supported by the Symbol class.
- *
- */
-enum OpCode{
-  OP_ADD = 0,
-  OP_MULT,
-  OP_SUB,
-  OP_INV,
+  /**
+   * @brief Enum to represent all the operation supported by the Symbol class.
+   *
+   */
+  enum OpCode {
+    OP_ADD = 0,
+    OP_MULT,
+    OP_SUB,
+    OP_INV,
 
-  NOF_OPERATIONS,
+    NOF_OPERATIONS,
 
-  OP_INPUT,
-  OP_CONST,
-};
+    OP_INPUT,
+    OP_CONST,
+  };
 
-/**
- * @brief a node at the data-flow grapsh (DFG) that represent a single operation
- *
- * This class contains a data about an operation done by the Symbol class.
- * It saves oll the data to describe the operation done and point to its operands.
- * At any given time a Sybol contains an Operation instance that represent all the 
- * calculation history done so far to reach the current value.
- */
+  /**
+   * @brief a node at the data-flow grapsh (DFG) that represent a single operation
+   *
+   * This class contains a data about an operation done by the Symbol class.
+   * It saves oll the data to describe the operation done and point to its operands.
+   * At any given time a Sybol contains an Operation instance that represent all the
+   * calculation history done so far to reach the current value.
+   */
 
-template <typename S>
-class Operation {
+  template <typename S>
+  class Operation
+  {
   public:
-    OpCode      m_opcode;
-    std::shared_ptr<Operation<S> >  m_operand1; // 1st operand if exist
-    std::shared_ptr<Operation<S> >  m_operand2; // 2nd operand if exist
+    OpCode m_opcode;
+    std::shared_ptr<Operation<S>> m_operand1; // 1st operand if exist
+    std::shared_ptr<Operation<S>> m_operand2; // 2nd operand if exist
 
     // optinal parameters:
-    std::unique_ptr<S>          m_constant; // for OP_CONST: const value
+    std::unique_ptr<S> m_constant; // for OP_CONST: const value
 
     // implementation:
-    int                         m_mem_addr; // location at the input vectors  
+    int m_mem_addr; // location at the input vectors
 
     // Constructor
-    Operation<S> (OpCode opcode, 
-                  std::shared_ptr<Operation<S> > operand1, 
-                  std::shared_ptr<Operation<S> > operand2 = nullptr, 
-                  std::unique_ptr<S> constant = nullptr,
-                  int mem_addr = -1) :
-      m_opcode(opcode),
-      m_operand1(operand1),
-      m_operand2(operand2),
-      m_mem_addr(mem_addr),
-      m_constant(std::move(constant)) {}
+    Operation<S>(
+      OpCode opcode,
+      std::shared_ptr<Operation<S>> operand1,
+      std::shared_ptr<Operation<S>> operand2 = nullptr,
+      std::unique_ptr<S> constant = nullptr,
+      int mem_addr = -1)
+        : m_opcode(opcode), m_operand1(operand1), m_operand2(operand2), m_mem_addr(mem_addr),
+          m_constant(std::move(constant))
+    {
+    }
 
-
-    bool was_visited(bool set_as_visit) {
+    bool was_visited(bool set_as_visit)
+    {
       const bool was_visited = (m_visit_idx == s_last_visit);
-      if (set_as_visit) {
-        m_visit_idx = s_last_visit;
-      }
+      if (set_as_visit) { m_visit_idx = s_last_visit; }
       return was_visited;
     }
 
-    static void reset_visit() {
-      s_last_visit++;
-    }
+    static void reset_visit() { s_last_visit++; }
 
   private:
     unsigned int m_visit_idx = 0;
     static inline unsigned int s_last_visit = 1;
-};
+  };
 
-/**
- * @brief The basic variable used to describe a program.
- *
- * This class is used by the end user to describe a functionality later on provided
- * to execute by different backends.
- * This enables the user to constructs lamda.
- */
-template <typename S>
-class Symbol {
+  /**
+   * @brief The basic variable used to describe a program.
+   *
+   * This class is used by the end user to describe a functionality later on provided
+   * to execute by different backends.
+   * This enables the user to constructs lamda.
+   */
+  template <typename S>
+  class Symbol
+  {
   public:
     // constructor
     Symbol() : m_operation(nullptr) {}
@@ -90,64 +88,73 @@ class Symbol {
     Symbol(const Symbol& other) : m_operation(other.m_operation) {}
 
     // constructor init
-    Symbol(const S& constant) : m_operation(std::make_shared<Operation<S> >(OpCode::OP_CONST, nullptr, nullptr, std::make_unique<S>(constant))) {}
+    Symbol(const S& constant)
+        : m_operation(std::make_shared<Operation<S>>(OpCode::OP_CONST, nullptr, nullptr, std::make_unique<S>(constant)))
+    {
+    }
 
     // operands
-    Symbol operator=(const Symbol& operand) { return assign(operand);}
-    Symbol operator=(const S& operand) { return assign(Symbol (operand));}
-    Symbol operator+(const Symbol& operand) const { return add(operand);}
-    Symbol operator+(const S& operand) const { return add(Symbol (operand));}
-    Symbol operator+=(const Symbol& operand) { return assign(add(operand));}
-    Symbol operator+=(const S& operand) { return assign(add(Symbol(operand)));}
-    Symbol operator-(const Symbol& operand) const { return sub(operand);}
-    Symbol operator-(const S& operand) const { return sub(Symbol(operand));}
-    Symbol operator-=(const Symbol& operand) { return assign(sub(operand));}
-    Symbol operator-=(const S& operand) { return assign(sub(Symbol(operand)));}
-    Symbol operator*(const Symbol& operand) const { return multiply(operand);}
-    Symbol operator*(const S& operand) const { return multiply(Symbol(operand));}
-    Symbol operator*=(const Symbol& operand) { return assign(multiply(operand));}
-    Symbol operator*=(const S& operand) { return assign(multiply(Symbol(operand)));}
-    Symbol operator!() const { return inverse();}
+    Symbol operator=(const Symbol& operand) { return assign(operand); }
+    Symbol operator=(const S& operand) { return assign(Symbol(operand)); }
+    Symbol operator+(const Symbol& operand) const { return add(operand); }
+    Symbol operator+(const S& operand) const { return add(Symbol(operand)); }
+    Symbol operator+=(const Symbol& operand) { return assign(add(operand)); }
+    Symbol operator+=(const S& operand) { return assign(add(Symbol(operand))); }
+    Symbol operator-(const Symbol& operand) const { return sub(operand); }
+    Symbol operator-(const S& operand) const { return sub(Symbol(operand)); }
+    Symbol operator-=(const Symbol& operand) { return assign(sub(operand)); }
+    Symbol operator-=(const S& operand) { return assign(sub(Symbol(operand))); }
+    Symbol operator*(const Symbol& operand) const { return multiply(operand); }
+    Symbol operator*(const S& operand) const { return multiply(Symbol(operand)); }
+    Symbol operator*=(const Symbol& operand) { return assign(multiply(operand)); }
+    Symbol operator*=(const S& operand) { return assign(multiply(Symbol(operand))); }
+    Symbol operator!() const { return inverse(); }
 
-    void set_as_input(int input_idx) {
-      m_operation = std::make_shared<Operation<S> >(OP_INPUT, nullptr, nullptr, nullptr, input_idx);
+    void set_as_input(int input_idx)
+    {
+      m_operation = std::make_shared<Operation<S>>(OP_INPUT, nullptr, nullptr, nullptr, input_idx);
     }
 
     // assign
-    Symbol assign(const Symbol& other) {
+    Symbol assign(const Symbol& other)
+    {
       m_operation = other.m_operation;
       return *this;
     }
 
     // add
-    Symbol add(const Symbol& operand) const {
+    Symbol add(const Symbol& operand) const
+    {
       Symbol rv;
-      rv.m_operation = std::make_shared<Operation<S> >(OP_ADD, m_operation, operand.m_operation);
+      rv.m_operation = std::make_shared<Operation<S>>(OP_ADD, m_operation, operand.m_operation);
       return rv;
     }
 
     // multiply
-    Symbol multiply(const Symbol& operand) const {
+    Symbol multiply(const Symbol& operand) const
+    {
       Symbol rv;
-      rv.m_operation = std::make_shared<Operation<S> >(OP_MULT, m_operation, operand.m_operation);
+      rv.m_operation = std::make_shared<Operation<S>>(OP_MULT, m_operation, operand.m_operation);
       return rv;
     }
 
-    // sub 
-    Symbol sub(const Symbol& operand) const {
+    // sub
+    Symbol sub(const Symbol& operand) const
+    {
       Symbol rv;
-      rv.m_operation = std::make_shared<Operation<S> >(OP_SUB, m_operation, operand.m_operation);
+      rv.m_operation = std::make_shared<Operation<S>>(OP_SUB, m_operation, operand.m_operation);
       return rv;
     }
 
     // inverse
-    Symbol inverse() {
+    Symbol inverse()
+    {
       Symbol rv;
-      rv.m_operation = std::make_shared<Operation<S> >(OP_INV, m_operation);
+      rv.m_operation = std::make_shared<Operation<S>>(OP_INV, m_operation);
       return rv;
     }
 
-    std::shared_ptr<Operation< S > > m_operation;
-};
+    std::shared_ptr<Operation<S>> m_operation;
+  };
 
 } // namespace icicle
