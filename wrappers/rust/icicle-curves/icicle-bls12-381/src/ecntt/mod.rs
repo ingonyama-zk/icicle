7dc51ee2362ee7ca1bb0928d8989a6ded62cf065
Jeremy Felder <jeremy.felder1@gmail.com>
2024-12-10 13:02:20 +0200

    Remove test curve; Fix 381 compilation

--- wrappers/rust/icicle-curves/icicle-bls12-381/src/ecntt/mod.rs
+++ wrappers/rust/icicle-curves/icicle-bls12-381/src/ecntt/mod.rs
@@ -6,7 +6,7 @@ impl_ecntt!("bls12_381", bls12_381, ScalarField, Bls12381Curve);
 
 #[cfg(test)]
 pub(crate) mod tests {
-    use crate::curve::{Bls12377Curve, ScalarField};
+    use crate::curve::{Bls12381Curve, ScalarField};
 
     use icicle_core::ecntt::tests::*;
     use icicle_core::impl_ecntt_tests;
