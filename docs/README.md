a9a368aca2badf5480194935fb17e1c33cdc8ad5
Jeremy Felder <jeremy.felder1@gmail.com>
2024-12-10 11:43:53 +0200

    Update docs README; Fix randome leaf index out of range; Add example to CI; Formatting
    Signed-off-by: Jeremy Felder <jeremy.felder1@gmail.com>

--- docs/README.md
+++ docs/README.md
@@ -1,39 +1,97 @@
-# Website
+# ICICLE Developer Docs
 
-This website is built using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
+The developer docs for ICICLE is a static website built using [Docusaurus](https://docusaurus.io/).
 
-### Installation
+## Requirements
 
-```
-$ npm i
-```
+Docusaurus is written in Typescript and distributed as npm packages. npm is a prerequisite as is node.js
 
-### Local Development
+If node.js or npm aren't installed, its suggested to use [nvm](https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating) to [install both](https://github.com/nvm-sh/nvm?tab=readme-ov-file#usage) at the same time.
 
-```
-$ npm start
-```
-
-This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
-
-### Build
+## Install
 
 ```
-$ npm run build
+npm install
 ```
 
-This command generates static content into the `build` directory and can be served using any static contents hosting service.
+## Versioning
 
-### Deployment
+ICICLE docs are versioned, keeping the latest set of docs for previous major versions and the latest 4 sets of docs for the current major version.
 
-Using SSH:
+The [docs](./docs/) directory holds the next version's docs
+All **released** versions are under the [versioned_docs](./versioned_docs/) directory.
+
+### Releasing new versions
+
+In order to create a new version, run the following:
+
+```sh
+npm run docusaurus docs:version <version to create>
+```
+
+For example:
+
+Assuming the next version is 5.6.0, we would run the following:
+
+```sh
+npm run docusaurus docs:version 5.6.0
+```
+
+This command will:
+
+1. Add a new version for the specified `<version to create>` in the [versions file](./versions.json)
+2. Create a directory under [versioned_docs](./versioned_docs/) with the name `version-<version to create>` and copies everything in the [docs](./docs/) directory there.
+3. Create a file under [versioned_sidebars](./versioned_sidebars/) with the name `version-<version to create>-sidebars.json` and copies the current [sidebar.ts](./sidebars.ts) file there after converting it to a json object.
+
+### Removing old versions
+
+1. Remove the version from versions.json.
+
+[
+  "3.2.0",
+  "3.1.0",
+  "3.0.0",
+  "2.8.0",
+- "1.10.1"
+]
+
+
+2. Delete the versioned docs directory for that version. Example: versioned_docs/version-1.10.1.
+3. Delete the versioned sidebars file. Example: versioned_sidebars/version-1.10.1-sidebars.json.
+
+## Local development
+
+To render the site, run the following
 
 ```
-$ USE_SSH=true npm run deploy
+npm start
 ```
 
-Not using SSH:
+This command starts a local development server and opens up a browser window on port 3000. Most changes are reflected live (hot reloaded) without having to restart the server.
 
+By default, the next version's docs are not rendered. In order to view any changes in the next version's docs, update the following in the [config file](./docusaurus.config.ts):
+
+```ts
+const ingoPreset = {
+  docs: {
+    .
+    .
+    includeCurrentVersion: false, // Update this to true to render the next verion's docs
+    .
+    .
+    .
+
+  },
+  .
+  .
+  .
+} satisfies Preset.Options
 ```
-$ GIT_USER=<Your GitHub username> npm run deploy
-```
+
+### Updating docs in old versions
+
+In order to update docs for old versions, the files under the specific version's [versioned_docs](./versioned_docs/) directory must be updated.
+
+### Updating docs across versions
+
+If docs need updating across multiple versions, including future versions, they need to be updated in each previous version's [versioned_docs](./versioned_docs/) and the next version's docs under the [docs](./docs/) directory
--- examples/rust/poseidon2/src/main.rs
+++ examples/rust/poseidon2/src/main.rs
@@ -14,13 +14,12 @@ pub fn hash_test<F:FieldImpl>(
     config: HashConfig,
     hash: Hasher,
 ) {
-let input_slice = HostSlice::from_slice(&test_vec);
-let out_init:F = F::zero();
-let mut binding = [out_init];
-let out_init_slice = HostSlice::from_mut_slice(&mut binding);
-hash.hash(input_slice, &config, out_init_slice).unwrap();
-println!("computed digest: {:?} ",out_init_slice.as_slice().to_vec()[0]);
-
+    let input_slice = HostSlice::from_slice(&test_vec);
+    let out_init:F = F::zero();
+    let mut binding = [out_init];
+    let out_init_slice = HostSlice::from_mut_slice(&mut binding);
+    hash.hash(input_slice, &config, out_init_slice).unwrap();
+    println!("computed digest: {:?} ",out_init_slice.as_slice().to_vec()[0]);
 }
 
 pub fn compute_binary_tree<F:FieldImpl>(
@@ -29,20 +28,19 @@ pub fn compute_binary_tree<F:FieldImpl>(
     hasher: Hasher,
     compress: Hasher,
     mut tree_config: MerkleTreeConfig,
-) -> MerkleTree
-{
-let tree_height: usize = test_vec.len().ilog2() as usize;
-tree_config.padding_policy = PaddingPolicy::ZeroPadding;
-let layer_hashes: Vec<&Hasher> = std::iter::once(&hasher)
-    .chain(std::iter::repeat(&compress).take(tree_height))
-    .collect();
-//binary tree
-let vec_slice: &mut HostSlice<F> = HostSlice::from_mut_slice(&mut test_vec[..]);
-let merkle_tree: MerkleTree = MerkleTree::new(&layer_hashes, leaf_size, 0).unwrap();
+) -> MerkleTree {
+    let tree_height: usize = test_vec.len().ilog2() as usize;
+    tree_config.padding_policy = PaddingPolicy::ZeroPadding;
+    let layer_hashes: Vec<&Hasher> = std::iter::once(&hasher)
+        .chain(std::iter::repeat(&compress).take(tree_height))
+        .collect();
+    //binary tree
+    let vec_slice: &mut HostSlice<F> = HostSlice::from_mut_slice(&mut test_vec[..]);
+    let merkle_tree: MerkleTree = MerkleTree::new(&layer_hashes, leaf_size, 0).unwrap();
 
-let _ = merkle_tree
-    .build(vec_slice,&tree_config);
-merkle_tree
+    let _ = merkle_tree
+        .build(vec_slice,&tree_config);
+    merkle_tree
 }
 
 pub fn main(){
@@ -72,17 +70,17 @@ pub fn main(){
 // t = 24
 // Input state (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23)
 // Output state [785637949, 311566256, 241540729, 1641553353, 851108667, 1648913123, 510139232, 616108837, 707720633, 1357404478, 1539840236, 275323287, 899761440, 732341189, 664618988, 1426148993, 1498654335, 792736017, 1804085503, 402731039, 659103866, 1036635937, 1016617890, 1470732388]
-let t_vec = [2,3,4,8,12,16,20,24];
-let expected_digest_bb:Vec<Frbb> = vec![Frbb::from_u32(833751247),Frbb::from_u32(1850148672),Frbb::from_u32(472702774),Frbb::from_u32(1077997898),Frbb::from_u32(1605336739),Frbb::from_u32(771677727),Frbb::from_u32(1224149815),Frbb::from_u32(311566256)];
-println!("Baby Bear");
-let config = HashConfig::default();
-for (t,digest) in t_vec.iter().zip(expected_digest_bb.iter()){
-    let input_state_bb:Vec<Frbb> = (0..*t).map(Frbb::from_u32).collect();
-    println!("test vector {:?}",input_state_bb);
-    println!("expected digest {:?}",digest);
-    hash_test::<Frbb>(input_state_bb, config.clone(), Poseidon2::new::<Frbb>(*t,None).unwrap());
-    println!(" ");
-}
+    let t_vec = [2,3,4,8,12,16,20,24];
+    let expected_digest_bb:Vec<Frbb> = vec![Frbb::from_u32(833751247),Frbb::from_u32(1850148672),Frbb::from_u32(472702774),Frbb::from_u32(1077997898),Frbb::from_u32(1605336739),Frbb::from_u32(771677727),Frbb::from_u32(1224149815),Frbb::from_u32(311566256)];
+    println!("Baby Bear");
+    let config = HashConfig::default();
+    for (t,digest) in t_vec.iter().zip(expected_digest_bb.iter()){
+        let input_state_bb:Vec<Frbb> = (0..*t).map(Frbb::from_u32).collect();
+        println!("test vector {:?}",input_state_bb);
+        println!("expected digest {:?}",digest);
+        hash_test::<Frbb>(input_state_bb, config.clone(), Poseidon2::new::<Frbb>(*t,None).unwrap());
+        println!(" ");
+    }
 
 // digest = output_state[1]
 // Sage output m31
@@ -111,42 +109,42 @@ for (t,digest) in t_vec.iter().zip(expected_digest_bb.iter()){
 // Input state (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23)
 // Output state [813042329, 956159494, 2017691352, 906353481, 1909737181, 1568930368, 1051192156, 1915448194, 114779228, 1695016063, 56353577, 991257558, 1283398606, 1782986529, 89100699, 1011002020, 71058136, 1382771657, 1734747710, 184579357, 1201113333, 2002016011, 1347833245, 1026595486]
 
-let expected_digest_m31:Vec<Frm31> = vec![Frm31::from_u32(1321841424),Frm31::from_u32(1808522380),Frm31::from_u32(1937028579),Frm31::from_u32(1040745210),
-Frm31::from_u32(495013829),Frm31::from_u32(996460804),Frm31::from_u32(33722680),Frm31::from_u32(956159494)];
-println!("M31");
-let config = HashConfig::default();
-for (t,digest) in t_vec.iter().zip(expected_digest_m31.iter()){
-    let input_state_m31:Vec<Frm31> = (0..*t).map(Frm31::from_u32).collect();
-    println!("test vector {:?}",input_state_m31);
-    println!("expected digest {:?}",digest);
-    hash_test::<Frm31>(input_state_m31, config.clone(), Poseidon2::new::<Frm31>(*t,None).unwrap());
-    println!(" ");
-}
+    let expected_digest_m31:Vec<Frm31> = vec![Frm31::from_u32(1321841424),Frm31::from_u32(1808522380),Frm31::from_u32(1937028579),Frm31::from_u32(1040745210),
+    Frm31::from_u32(495013829),Frm31::from_u32(996460804),Frm31::from_u32(33722680),Frm31::from_u32(956159494)];
+    println!("M31");
+    let config = HashConfig::default();
+    for (t,digest) in t_vec.iter().zip(expected_digest_m31.iter()){
+        let input_state_m31:Vec<Frm31> = (0..*t).map(Frm31::from_u32).collect();
+        println!("test vector {:?}",input_state_m31);
+        println!("expected digest {:?}",digest);
+        hash_test::<Frm31>(input_state_m31, config.clone(), Poseidon2::new::<Frm31>(*t,None).unwrap());
+        println!(" ");
+    }
 
-println!("\n Merkle tree test with poseidon 2: m31");
+    println!("\n Merkle tree test with poseidon 2: m31");
 
 
-// for binary tree Poseidon(t1,t2) -> n1 
-let poseidon_state_size = 2; 
-let leaf_size:u64 = 4;// each leaf is a 32 bit element 32/8 = 4 bytes
+    // for binary tree Poseidon(t1,t2) -> n1 
+    let poseidon_state_size = 2; 
+    let leaf_size:u64 = 4;// each leaf is a 32 bit element 32/8 = 4 bytes
 
-let mut test_vec = vec![Frm31::from_u32(random::<u32>()); 1024* (poseidon_state_size as usize)];   
-println!("Generated random vector of size {:?}", 1024* (poseidon_state_size as usize));
-//to use later for merkle proof
-let mut binding = test_vec.clone();
-let test_vec_slice = HostSlice::from_mut_slice(&mut binding);
-//define hash and compression functions
-let hasher :Hasher = Poseidon2::new::<Frm31>(poseidon_state_size.try_into().unwrap(),None).unwrap();
-let compress: Hasher = Poseidon2::new::<Frm31>((hasher.output_size()*2).try_into().unwrap(),None).unwrap();
-//tree config
-let tree_config = MerkleTreeConfig::default();
-let merk_tree = compute_binary_tree(test_vec.clone(), leaf_size, hasher, compress,tree_config.clone());
-println!("computed Merkle root {:?}", merk_tree.get_root::<Frm31>().unwrap());
+    let mut test_vec = vec![Frm31::from_u32(random::<u32>()); 1024* (poseidon_state_size as usize)];   
+    println!("Generated random vector of size {:?}", 1024* (poseidon_state_size as usize));
+    //to use later for merkle proof
+    let mut binding = test_vec.clone();
+    let test_vec_slice = HostSlice::from_mut_slice(&mut binding);
+    //define hash and compression functions
+    let hasher :Hasher = Poseidon2::new::<Frm31>(poseidon_state_size.try_into().unwrap(),None).unwrap();
+    let compress: Hasher = Poseidon2::new::<Frm31>((hasher.output_size()*2).try_into().unwrap(),None).unwrap();
+    //tree config
+    let tree_config = MerkleTreeConfig::default();
+    let merk_tree = compute_binary_tree(test_vec.clone(), leaf_size, hasher, compress,tree_config.clone());
+    println!("computed Merkle root {:?}", merk_tree.get_root::<Frm31>().unwrap());
 
-let random_test_index = rand::thread_rng().gen_range(0..1024*(leaf_size as usize));
-print!("Generating proof for element {:?} at random test index {:?} ",test_vec[random_test_index], random_test_index);
-let merkle_proof = merk_tree.get_proof::<Frm31>(test_vec_slice, random_test_index.try_into().unwrap(), false, &tree_config).unwrap();
+    let random_test_index = rand::thread_rng().gen_range(0..1024*(poseidon_state_size as usize));
+    print!("Generating proof for element {:?} at random test index {:?} ",test_vec[random_test_index], random_test_index);
+    let merkle_proof = merk_tree.get_proof::<Frm31>(test_vec_slice, random_test_index.try_into().unwrap(), false, &tree_config).unwrap();
 
-assert!(merk_tree.verify(&merkle_proof).unwrap());
-println!("\n Merkle proof verified successfully!");
+    assert!(merk_tree.verify(&merkle_proof).unwrap());
+    println!("\n Merkle proof verified successfully!");
 }
