88f1a183cd2471bda15d7d00c61f021c2075d575
emirsoyturk <emir@ingonyama.com>
2024-12-31 19:12:28 +0200

    rust format

--- wrappers/rust/icicle-fields/icicle-babybear/src/vec_ops/mod.rs
+++ wrappers/rust/icicle-fields/icicle-babybear/src/vec_ops/mod.rs
@@ -1,19 +1,25 @@
 use crate::field::{ExtensionCfg, ExtensionField, ScalarCfg, ScalarField};
 
-use icicle_core::{impl_vec_ops_field, impl_vec_ops_cross_field};
-use icicle_core::vec_ops::{VecOps, VecOpsConfig, CrossVecOps};
+use icicle_core::vec_ops::{CrossVecOps, VecOps, VecOpsConfig};
+use icicle_core::{impl_vec_ops_cross_field, impl_vec_ops_field};
 use icicle_runtime::errors::eIcicleError;
 use icicle_runtime::memory::HostOrDeviceSlice;
 
 impl_vec_ops_field!("babybear", babybear, ScalarField, ScalarCfg);
 impl_vec_ops_field!("babybear_extension", babybear_extension, ExtensionField, ExtensionCfg);
-impl_vec_ops_cross_field!("babybear_extension", babybear_cross, ExtensionField, ScalarField, ExtensionCfg);
+impl_vec_ops_cross_field!(
+    "babybear_extension",
+    babybear_cross,
+    ExtensionField,
+    ScalarField,
+    ExtensionCfg
+);
 
 #[cfg(test)]
 pub(crate) mod tests {
     use crate::field::{ExtensionField, ScalarField};
-    use icicle_core::{impl_vec_ops_tests, impl_cross_vec_ops_tests};
     use icicle_core::vec_ops::tests::*;
+    use icicle_core::{impl_cross_vec_ops_tests, impl_vec_ops_tests};
 
     impl_vec_ops_tests!(ScalarField);
 
