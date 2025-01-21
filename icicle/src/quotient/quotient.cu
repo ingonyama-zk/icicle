1b5d8a0e688b7540e8c24e0ca1de3fab3215441b
suyash67 <suyash@ingonyama.com>
2025-01-21 16:48:43 +0000

    print d_col_ptrs

--- icicle/src/quotient/quotient.cu
+++ icicle/src/quotient/quotient.cu
@@ -376,6 +376,11 @@ namespace quotient {
             CHK_IF_RETURN(cudaMemcpyAsync(d_values_ptrs, h_values_ptrs, sizeof(QF*) * sample_size, cudaMemcpyHostToDevice, stream));
             CHK_IF_RETURN(cudaMemcpyAsync(d_point_ptrs, h_point_ptrs, sizeof(QP*) * sample_size, cudaMemcpyHostToDevice, stream));
 
+            printf("DEVICE: d_col[0]", (*d_columns_ptrs)[0]);
+            printf("DEVICE: d_col[1]", (*d_columns_ptrs)[1]);
+            printf("DEVICE: d_col[2]", (*d_columns_ptrs)[2]);
+            printf("DEVICE: d_col[3]", (*d_columns_ptrs)[3]);
+
             cudaError_t err7 = cudaGetLastError();
             if (err7 != cudaSuccess) {
                 printf("Err 7: %s\n", cudaGetErrorString(err7));
