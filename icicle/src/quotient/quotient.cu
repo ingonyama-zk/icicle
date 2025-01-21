b594e9e7378e6367d77e06fdc5c28b9b95537a04
suyash67 <suyash@ingonyama.com>
2025-01-21 15:22:00 +0000

    mod print

--- icicle/src/quotient/quotient.cu
+++ icicle/src/quotient/quotient.cu
@@ -217,9 +217,14 @@ namespace quotient {
                     uint32_t column_index = samples[i].columns[j];
                     // QF linear_term = scalar_mul<QF>(a, point.y.limbs_storage.limbs[0]) + b;
 
+                    // F col_value = columns[column_index * domain_size + row].limbs_storage.limbs[0];
+
+                    // printf("col_value = %d\n", col_value);
+
+
                     QF value = scalar_mul<QF>(c, columns[column_index * domain_size + row].limbs_storage.limbs[0]);
 
-                    printf("value = %d\n", value);
+                    printf("yuval value = %d\n", value);
 
                     numerator = numerator + random_coefficient;
                 }
