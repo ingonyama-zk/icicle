9a5a559fbb5ccb77f3344465e357f0fea7cc2f2e
suyash67 <suyash@ingonyama.com>
2025-01-21 10:02:20 +0000

    more print

--- icicle/src/quotient/quotient.cu
+++ icicle/src/quotient/quotient.cu
@@ -190,6 +190,7 @@ namespace quotient {
     __global__ void set_columns_and_values_pointers(ColumnSampleBatch<QP, QF> *d_samples, uint32_t **d_columns_ptrs, QF **d_values_ptrs, QP **d_point_ptrs, int sample_size) {
         int i = blockIdx.x * blockDim.x + threadIdx.x;
         if (i < sample_size) {
+            printf("Thread %d: data[%d] = %d\n", i, i, d_samples[i]);
 
             d_samples[i].columns = d_columns_ptrs[i];
             d_samples[i].values = d_values_ptrs[i];
