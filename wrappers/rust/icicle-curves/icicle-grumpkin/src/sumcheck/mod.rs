4d6fc11425e4b495c8391c4a7c72187e00d816ae
Jeremy Felder <jeremy.felder1@gmail.com>
2025-02-13 18:00:14 +0200

    Expose sumcheck API to Rust (#770)

---
+++ wrappers/rust/icicle-curves/icicle-grumpkin/src/sumcheck/mod.rs
@@ -0,0 +1,12 @@
+use crate::curve::{ScalarCfg, ScalarField};
+use icicle_core::impl_sumcheck;
+
+impl_sumcheck!("grumpkin", grumpkin, ScalarField, ScalarCfg);
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use crate::curve::ScalarField;
+    use icicle_core::impl_sumcheck_tests;
+
+    impl_sumcheck_tests!(ScalarField);
+}
