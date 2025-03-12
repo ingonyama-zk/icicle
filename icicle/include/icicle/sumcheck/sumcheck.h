04825a5459423039eb069ff6ef62d181e6e6613e
idanfr-ingo <idan@ingonyama.com>
2025-03-12 08:15:15 +0000

    move limitations to frontend

--- icicle/include/icicle/sumcheck/sumcheck.h
+++ icicle/include/icicle/sumcheck/sumcheck.h
@@ -8,6 +8,10 @@
 #include "icicle/backend/sumcheck_backend.h"
 #include "icicle/sumcheck/sumcheck_transcript.h"
 
+constexpr int MAX_COMBINE_POLY_DEG = 6; // Max degree of combine polynomial supported
+constexpr int MAX_NOF_POLYNIMOALS = 8;  // Max nof polynomials supported
+constexpr int MAX_TOTAL_NOF_VARS = 20;  // Max nof vars allowed in the  of the combine function
+
 namespace icicle {
   template <typename F>
   class Sumcheck;
@@ -61,6 +65,39 @@ namespace icicle {
       const SumcheckConfig& sumcheck_config,
       SumcheckProof<F>& sumcheck_proof /*out*/) const
     {
+      if (sumcheck_config.use_extension_field) {
+        ICICLE_LOG_ERROR << "SumcheckConfig::use_extension_field = true is currently unsupported";
+        return eIcicleError::INVALID_ARGUMENT;
+      }
+
+      // check that the combine function has a legal polynomial degree
+      const int combine_function_poly_degree = combine_function.get_polynomial_degree();
+      if (combine_function_poly_degree < 0) {
+        ICICLE_LOG_ERROR << "Illegal polynomial degree (" << combine_function_poly_degree
+                         << ") for provided combine function";
+        return eIcicleError::INVALID_ARGUMENT;
+      }
+
+      // check that combine polynomial degree does not exceeds the highest allowed degree
+      if (combine_function_poly_degree > MAX_COMBINE_POLY_DEG) {
+        ICICLE_LOG_ERROR << "Too high polynomial degree (" << combine_function_poly_degree
+                         << "). Max allowed degree is " << MAX_COMBINE_POLY_DEG;
+        return eIcicleError::INVALID_ARGUMENT;
+      }
+
+      // check that number of polynomials does not exceeds the max allowed number
+      const int nof_polys = mle_polynomials.size();
+      if (nof_polys > MAX_NOF_POLYNIMOALS) {
+        ICICLE_LOG_ERROR << "Too many polynomials. " << nof_polys << " were given, but maximum number of "
+                         << MAX_NOF_POLYNIMOALS << " is allowed";
+        return eIcicleError::INVALID_ARGUMENT;
+      }
+
+      const int total_nof_vars = combine_function.get_nof_vars();
+      if (total_nof_vars > MAX_TOTAL_NOF_VARS) {
+        ICICLE_LOG_ERROR << "Too complex combine function, such functions are unsupported.";
+        return eIcicleError::INVALID_ARGUMENT;
+      }
       return m_backend->get_proof(
         mle_polynomials, mle_polynomial_size, claimed_sum, combine_function, std::move(transcript_config),
         sumcheck_config, sumcheck_proof);
