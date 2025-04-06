776901689d58253bd2ad829fecec4e161d454c21
hadaringonyama <hadar@ingonyama.com>
2025-04-06 09:37:56 +0300

    fix ntt

--- icicle/tests/test_field_api.cpp
+++ icicle/tests/test_field_api.cpp
@@ -17,73 +17,11 @@ using FieldTestBase = ModArithTestBase;
 // Note: this is testing host arithmetic. Other tests against CPU backend should guarantee correct device arithmetic too
 TYPED_TEST(FieldTest, FieldSanityTest)
 {
-  auto a = TypeParam::rand_host();
-  auto b = TypeParam::rand_host();
-  auto c = TypeParam::rand_host();
-  auto d = TypeParam::rand_host();
-  auto e = TypeParam::rand_host();
-  auto f = TypeParam::rand_host();
-  auto g = TypeParam::rand_host();
-  auto h = TypeParam::rand_host();
-  auto i = TypeParam::rand_host();
-  auto j = TypeParam::rand_host();
-  START_TIMER(mult)
-  for (int k = 0; k < 1000000; k++) {
-    a = a * b;
-    b = b * c;
-    c = c * d;
-    d = d * e;
-    e = e * f;
-    f = f * g;
-    g = g * h;
-    h = h * i;
-    i = i * j;
-    j = j * a;
-  }
-  END_TIMER(mult, "mult timer", true);
-  START_TIMER(add)
-  for (int k = 0; k < 1000000; k++) {
-    a = a + b;
-    b = b + c;
-    c = c + d;
-    d = d + e;
-    e = e + f;
-    f = f + g;
-    g = g + h;
-    h = h + i;
-    i = i + j;
-    j = j + a;
-  }
-  END_TIMER(add, "add timer", true);
-  START_TIMER(sub)
-  for (int k = 0; k < 1000000; k++)
-  {
-    a = a - b;
-    b = b - c;
-    c = c - d;
-    d = d - e;
-    e = e - f;
-    f = f - g;
-    g = g - h;
-    h = h - i;
-    i = i - j;
-    j = j - a;
-  }
-  END_TIMER(sub, "sub timer", true);
-  std::cout << "a " << a <<std::endl;
-  std::cout << "b " << b <<std::endl;
-  std::cout << "c " << c <<std::endl;
-  std::cout << "d " << d <<std::endl;
-  std::cout << "e " << e <<std::endl;
-  std::cout << "f " << f <<std::endl;
-  std::cout << "g " << g <<std::endl;
-  std::cout << "h " << h <<std::endl;
-  std::cout << "i " << i <<std::endl;
-  std::cout << "j " << j <<std::endl;
   a = TypeParam::rand_host();
   b = TypeParam::rand_host();
   auto b_inv = TypeParam::inverse(b);
   auto a_neg = TypeParam::neg(a);
+  ASSERT_EQ(TypeParam::from_montgomery(TypeParam::to_montgomery(a)), a);
   ASSERT_EQ(a + TypeParam::zero(), a);
   ASSERT_EQ(a + b - a, b);
   ASSERT_EQ(b * a * b_inv, a);
