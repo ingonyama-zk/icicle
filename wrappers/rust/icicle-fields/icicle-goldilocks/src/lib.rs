e5df3150745b0e9806dbeacedfaf0fbb12fdd43e
Leon Hibnik <leon@ingonyama.com>
2025-04-08 11:50:23 +0300

    uncomment poseidon2 for goldilock rust

--- wrappers/rust/icicle-fields/icicle-goldilocks/src/lib.rs
+++ wrappers/rust/icicle-fields/icicle-goldilocks/src/lib.rs
@@ -3,7 +3,7 @@ pub mod ntt;
 pub mod polynomials;
 // TODO: uncomment once implemented for goldilocks
 // pub mod poseidon;
-// pub mod poseidon2;
+pub mod poseidon2;
 pub mod program;
 pub mod sumcheck;
 pub mod symbol;
