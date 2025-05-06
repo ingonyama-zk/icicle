5e0387fb528b51e623777a0250d64c35728f16cf
ChickenLover <Romangg81@gmail.com>
2025-05-06 22:14:56 +0700

    Add Goldilocks Ext (#892)

--- wrappers/rust/icicle-fields/icicle-goldilocks/src/field.rs
+++ wrappers/rust/icicle-fields/icicle-goldilocks/src/field.rs
@@ -6,14 +6,27 @@ use icicle_runtime::memory::HostOrDeviceSlice;
 use icicle_runtime::stream::IcicleStream;
 
 pub(crate) const SCALAR_LIMBS: usize = 2; // Goldilocks uses 2 limbs for 64-bit field
+pub(crate) const EXTENSION_LIMBS: usize = 4;
 
 impl_scalar_field!("goldilocks", goldilocks, SCALAR_LIMBS, ScalarField, ScalarCfg);
+impl_scalar_field!(
+    "goldilocks_extension",
+    goldilocks_extension,
+    EXTENSION_LIMBS,
+    ExtensionField,
+    ExtensionCfg
+);
 
 #[cfg(test)]
 mod tests {
-    use super::ScalarField; // No extension field
+    use super::{ExtensionField, ScalarField}; // No extension field
     use icicle_core::impl_field_tests;
     use icicle_core::tests::*;
 
     impl_field_tests!(ScalarField);
+    mod extension {
+        use super::*;
+
+        impl_field_tests!(ExtensionField);
+    }
 }
