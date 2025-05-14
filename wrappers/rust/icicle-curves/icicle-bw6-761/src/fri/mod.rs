0714657a6418c6d7dfaee5e05888ee313e446707
ChickenLover <Romangg81@gmail.com>
2025-05-14 14:50:22 +0700

    Feat/fri poseidon (#896)

--- wrappers/rust/icicle-curves/icicle-bw6-761/src/fri/mod.rs
+++ wrappers/rust/icicle-curves/icicle-bw6-761/src/fri/mod.rs
@@ -1,8 +1,7 @@
 #[cfg(test)]
 mod tests {
-    use icicle_core::impl_fri_tests;
-
     use crate::curve::ScalarField;
-
-    impl_fri_tests!(bw6_761_fri_test, ScalarField, ScalarField);
+    use icicle_core::{impl_fri_test_with_poseidon, impl_fri_tests};
+    impl_fri_tests!(ScalarField, ScalarField);
+    impl_fri_test_with_poseidon!(ScalarField, ScalarField);
 }
