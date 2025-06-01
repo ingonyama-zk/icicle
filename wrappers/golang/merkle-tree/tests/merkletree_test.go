67ef41b52073da40a679fc20b19c04ffe1f9cc30
Jeremy Felder <jeremy.felder1@gmail.com>
2025-06-01 13:13:47 +0300

    Fix runtime and hash/merkle tree tests for metal

--- wrappers/golang/merkle-tree/tests/merkletree_test.go
+++ wrappers/golang/merkle-tree/tests/merkletree_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/core"
 	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/hash"
+	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/internal/test_helpers"
 	merkletree "github.com/ingonyama-zk/icicle/v3/wrappers/golang/merkle-tree"
 	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/runtime"
 	"github.com/stretchr/testify/suite"
@@ -57,7 +58,7 @@ func testMerkleTree(s *suite.Suite) {
 		s.FailNow(fmt.Sprintf("TestMerkleTree: Could not verify merkle tree due to: %v", err))
 	}
 
-	runtime.SetDevice(&devices[1])
+	runtime.SetDevice(&test_helpers.MAIN_DEVICE)
 	d_keccak256, err := hash.NewKeccak256Hasher(uint64(2 * leafElemSize))
 	if err != runtime.Success {
 		s.FailNow(fmt.Sprintf("TestMerkleTree: Could not create keccak hasher due to: %v", err))
@@ -100,7 +101,7 @@ type MerkleTreeTestSuite struct {
 }
 
 func (s *MerkleTreeTestSuite) TestMerkleTree() {
-	s.Run("TestMerkleTree", testWrapper(&s.Suite, testMerkleTree))
+	s.Run("TestMerkleTree", test_helpers.TestWrapper(&s.Suite, testMerkleTree))
 }
 
 func TestSuiteMerkleTree(t *testing.T) {
--- wrappers/golang/runtime/tests/stream_test.go
+++ wrappers/golang/runtime/tests/stream_test.go
@@ -3,47 +3,58 @@ package tests
 import (
 	"testing"
 
+	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/internal/test_helpers"
 	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/runtime"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestCreateStream(t *testing.T) {
-	dev := runtime.CreateDevice("CUDA", 0)
-	assert.True(t, runtime.IsDeviceAvailable(&dev))
-	err := runtime.SetDevice(&dev)
-	assert.Equal(t, runtime.Success, err)
-	_, err = runtime.CreateStream()
-	assert.Equal(t, runtime.Success, err, "Unable to create stream due to %d", err)
+	func testCreateStream(suite *suite.Suite) {
+	test_helpers.ActivateMainDevice()
+
+	_, err := runtime.CreateStream()
+	suite.Equal(runtime.Success, err, "Unable to create stream due to %d", err)
 }
 
-func TestDestroyStream(t *testing.T) {
-	dev := runtime.CreateDevice("CUDA", 0)
-	assert.True(t, runtime.IsDeviceAvailable(&dev))
+func testDestroyStream(suite *suite.Suite) {
+	test_helpers.ActivateMainDevice()
+
 	stream, err := runtime.CreateStream()
-	assert.Equal(t, runtime.Success, err, "Unable to create stream due to %d", err)
+	suite.Equal(runtime.Success, err, "Unable to create stream due to %d", err)
 
 	err = runtime.DestroyStream(stream)
-	assert.Equal(t, runtime.Success, err, "Unable to destroy stream due to %d", err)
+	suite.Equal(runtime.Success, err, "Unable to destroy stream due to %d", err)
 }
 
-func TestSyncStream(t *testing.T) {
-	dev := runtime.CreateDevice("CUDA", 0)
-	assert.True(t, runtime.IsDeviceAvailable(&dev))
-	runtime.SetDevice(&dev)
+func testSyncStream(suite *suite.Suite) {
+	test_helpers.ActivateMainDevice()
 
 	stream, err := runtime.CreateStream()
-	assert.Equal(t, runtime.Success, err, "Unable to create stream due to %d", err)
+	suite.Equal(runtime.Success, err, "Unable to create stream due to %d", err)
 
 	_, err = runtime.MallocAsync(200000, stream)
-	assert.Equal(t, runtime.Success, err, "Unable to allocate device memory due to %d", err)
+	suite.Equal(runtime.Success, err, "Unable to allocate device memory due to %d", err)
 
 	dp, err := runtime.Malloc(20)
-	assert.NotNil(t, dp)
-	assert.Equal(t, runtime.Success, err, "Unable to allocate device memory due to %d", err)
+	suite.NotNil(dp)
+	suite.Equal(runtime.Success, err, "Unable to allocate device memory due to %d", err)
 
 	err = runtime.SynchronizeStream(stream)
-	assert.Equal(t, runtime.Success, err, "Unable to sync stream due to %d", err)
+	suite.Equal(runtime.Success, err, "Unable to sync stream due to %d", err)
 
 	err = runtime.DestroyStream(stream)
-	assert.Equal(t, runtime.Success, err, "Unable to destroy stream due to %d", err)
+	suite.Equal(runtime.Success, err, "Unable to destroy stream due to %d", err)
+}
+
+type StreamTestSuite struct {
+	suite.Suite
+}
+
+func (s *StreamTestSuite) TestCreateStream() {
+	s.Run("TestCreateStream", test_helpers.TestWrapper(&s.Suite, testCreateStream))
+	s.Run("TestDestroyStream", test_helpers.TestWrapper(&s.Suite, testDestroyStream))
+	s.Run("TestSyncStream", test_helpers.TestWrapper(&s.Suite, testSyncStream))
+}
+
+func TestSuiteStream(t *testing.T) {
+	suite.Run(t, new(StreamTestSuite))
 }
