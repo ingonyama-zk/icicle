c9610a73a8b2d9443a515b90e9707cee62d5ac4b
Jeremy Felder <jeremy.felder1@gmail.com>
2025-06-05 16:08:17 +0300

    [FIX] Go runtime, hash, and merkle tree package tests for metal (#925)

--- wrappers/golang/runtime/tests/main_test.go
+++ wrappers/golang/runtime/tests/main_test.go
@@ -1,12 +1,28 @@
 package tests
 
 import (
+	"os"
+	"sync"
 	"testing"
 
+	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/internal/test_helpers"
 	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/runtime"
+	"github.com/stretchr/testify/suite"
 )
 
+func testWrapper(suite *suite.Suite, fn func(*suite.Suite)) func() {
+	return func() {
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		runtime.RunOnDevice(&test_helpers.REFERENCE_DEVICE, func(args ...any) {
+			defer wg.Done()
+			fn(suite)
+		})
+		wg.Wait()
+	}
+}
+
 func TestMain(m *testing.M) {
-	runtime.LoadBackendFromEnvOrDefault()
-	m.Run()
+	test_helpers.LOAD_AND_INIT_MAIN_DEVICE()
+	os.Exit(m.Run())
 }
