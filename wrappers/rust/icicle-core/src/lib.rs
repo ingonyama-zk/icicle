d8b95b3f2bb7f09911a5cf814f273fd9d8a645aa
yshekel <yshekel@gmail.com>
2025-06-15 19:21:56 +0300

    [FEAT] Rust Polynomial-Ring trait and NegacyclicNTT (#943)

--- wrappers/rust/icicle-core/src/lib.rs
+++ wrappers/rust/icicle-core/src/lib.rs
@@ -6,9 +6,11 @@ pub mod fri;
 pub mod hash;
 pub mod merkle;
 pub mod msm;
+pub mod negacyclic_ntt;
 pub mod norm;
 pub mod ntt;
 pub mod pairing;
+pub mod polynomial_ring;
 pub mod polynomials;
 pub mod poseidon;
 pub mod poseidon2;
@@ -16,8 +18,7 @@ pub mod program;
 pub mod rns;
 pub mod sumcheck;
 pub mod symbol;
-pub mod vec_ops;
-
 #[doc(hidden)]
 pub mod tests;
 pub mod traits;
+pub mod vec_ops;
---
+++ wrappers/rust/icicle-rings/icicle-labrador/src/polynomial_ring.rs
@@ -0,0 +1,14 @@
+use crate::ring::ScalarRing;
+use icicle_core::impl_polynomial_ring;
+use icicle_core::traits::FieldImpl;
+
+// Define the Polynomial Ring Zq[X]/X^d+1
+impl_polynomial_ring!(PolyRing, ScalarRing, 64, -1);
+
+#[cfg(test)]
+mod tests {
+    use crate::polynomial_ring::PolyRing;
+    use icicle_core::test_polynomial_ring;
+
+    test_polynomial_ring!(PolyRing);
+}
