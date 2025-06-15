a2bc757d441ac461aa61f19c8945cbecfca54af5
yshekel <yshekel@gmail.com>
2025-06-15 19:21:56 +0300

    [FEAT] Rust Polynomial-Ring trait and NegacyclicNTT (#943)

--- icicle/cmake/target_editor.cmake
+++ icicle/cmake/target_editor.cmake
@@ -30,9 +30,8 @@ function(handle_ring TARGET)
     src/symbol/symbol_api.cpp
     src/balanced_decomposition.cpp
     src/norm.cpp
-    src/jl_projection.cpp
-  )
-  target_link_libraries(${TARGET} PRIVATE icicle_hash) # Random sampling APIs depend on hashing
+    src/jl_projection.cpp)
+  target_link_libraries(${TARGET} INTERFACE icicle_hash)
 endfunction()
 
 function(handle_ntt TARGET FEATURE_LIST)
