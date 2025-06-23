33760fa4f98d1e2153a3ef196daa0ff949bc8023
Jeremy Felder <jeremy.felder1@gmail.com>
2025-06-23 13:05:48 +0000

    Fix babybear rust tests

--- wrappers/rust/icicle-hash/src/tests.rs
+++ wrappers/rust/icicle-hash/src/tests.rs
@@ -10,15 +10,19 @@ mod tests {
     };
     use icicle_core::{
         hash::{HashConfig, Hasher},
-        merkle::{MerkleProof, MerkleTree, MerkleTreeConfig},
+        merkle::{MerkleProof, MerkleTree, MerkleTreeConfig, PaddingPolicy},
+        traits::GenerateRandom,
     };
     use icicle_runtime::{
+        device::Device,
         eIcicleError,
         memory::{DeviceVec, HostSlice},
+        runtime,
         test_utilities,
     };
     use rand::Rng;
     use std::sync::Once;
+    use std::time::Instant;
 
     static INIT: Once = Once::new();
 
@@ -454,5 +458,74 @@ mod tests {
         assert_eq!(err, eIcicleError::Success);
         assert_eq!(cpu_mined_hash, cpu_mined_hash_check);
         assert!(cpu_is_correct);
+    }
+
+    #[test]
+    fn test_merkle_tree_segfault() {
+        initialize();
+        test_utilities::test_set_main_device();
+
+        let n = 18;
+        let field_element_size = 4;
+        for elements_per_leaf in [1, 5] {
+            // calc tree parameters
+            let leaf_size = field_element_size * elements_per_leaf;
+            let mut test_vec = vec![0 as u8; 1 << n];
+            rand::thread_rng().fill(&mut test_vec[..]);
+            let nof_leafs = (((1 << n) + elements_per_leaf - 1) / elements_per_leaf) as u64;
+            let tree_height = if nof_leafs.is_power_of_two() {
+                nof_leafs.ilog2() as usize
+            } else {
+                nof_leafs.ilog2() as usize + 1
+            };
+
+            let build_tree = |main_dev: bool| {
+                if main_dev {
+                    test_utilities::test_set_main_device();
+                } else {
+                    test_utilities::test_set_ref_device();
+                }
+                // define the tree
+                let hasher = Blake2s::new(leaf_size).unwrap();
+                let compress = Blake2s::new(hasher.output_size() * 2).unwrap();
+                let layer_hashes: Vec<&Hasher> = std::iter::once(&hasher)
+                    .chain(std::iter::repeat(&compress).take(tree_height))
+                    .collect();
+
+                println!("Leaf Size: {}", leaf_size);
+                println!("Number of Leafs: {}", nof_leafs);
+                println!("Hasher Output Size: {}", hasher.output_size());
+                println!("Tree Height: {}", tree_height);
+
+                let merkle_tree = MerkleTree::new(&layer_hashes, leaf_size, 0).unwrap();
+                let mut config = MerkleTreeConfig::default();
+                config.padding_policy = PaddingPolicy::ZeroPadding;
+
+                let build_start = Instant::now();
+
+                merkle_tree
+                    .build(HostSlice::from_slice(&test_vec), &config)
+                    .unwrap();
+
+                println!(
+                    "Merkle tree build took: {:?} on device = {}",
+                    build_start.elapsed(),
+                    if main_dev { "Main" } else { "Ref" }
+                );
+
+                let root: &[u8] = merkle_tree
+                    .get_root()
+                    .unwrap();
+
+                let mut root_vec = vec![0u8; hasher.output_size() as usize];
+                root_vec.copy_from_slice(root);
+                return root_vec;
+            };
+
+            // build the tree on main and ref devices and compare the roots
+            let ref_root = build_tree(false);
+            let main_root = build_tree(true);
+            assert_eq!(ref_root, main_root);
+        }
     }
 }
