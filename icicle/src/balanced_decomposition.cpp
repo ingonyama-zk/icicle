6d29bdf198f3eea0ae1964ff15eade20a782651a
Yuval Shekel <yshekel@gmail.com>
2025-06-29 16:41:56 +0300

    fix ffi

--- icicle/src/balanced_decomposition.cpp
+++ icicle/src/balanced_decomposition.cpp
@@ -71,6 +71,30 @@ namespace icicle {
 
   /*********************************** BALANCED DECOMPOSITION/RECOMPOSITION PolyRing ************************/
 
+  extern "C" eIcicleError CONCAT_EXPAND(ICICLE_FFI_PREFIX, poly_ring_decompose_balanced_digits)(
+    const PolyRing* input,
+    size_t input_size,
+    uint32_t base,
+    const VecOpsConfig* config,
+    PolyRing* output,
+    size_t output_size)
+  {
+    return CONCAT_EXPAND(ICICLE_FFI_PREFIX, decompose_balanced_digits)(
+      (const field_t*)input, input_size * PolyRing::d, base, config, (field_t*)output, output_size * PolyRing::d);
+  }
+
+  extern "C" eIcicleError CONCAT_EXPAND(ICICLE_FFI_PREFIX, poly_ring_recompose_from_balanced_digits)(
+    const PolyRing* input,
+    size_t input_size,
+    uint32_t base,
+    const VecOpsConfig* config,
+    PolyRing* output,
+    size_t output_size)
+  {
+    return CONCAT_EXPAND(ICICLE_FFI_PREFIX, recompose_from_balanced_digits)(
+      (const field_t*)input, input_size * PolyRing::d, base, config, (field_t*)output, output_size * PolyRing::d);
+  }
+
   namespace balanced_decomposition {
     template <>
     eIcicleError decompose(
@@ -81,12 +105,10 @@ namespace icicle {
       PolyRing* output,
       size_t output_size)
     {
-      return CONCAT_EXPAND(ICICLE_FFI_PREFIX, decompose_balanced_digits)(
-        (const field_t*)input, input_size * PolyRing::d, base, &config, (field_t*)output, output_size * PolyRing::d);
+      return CONCAT_EXPAND(ICICLE_FFI_PREFIX, poly_ring_decompose_balanced_digits)(
+        input, input_size, base, &config, output, output_size);
     }
-  } // namespace balanced_decomposition
 
-  namespace balanced_decomposition {
     template <>
     eIcicleError recompose(
       const PolyRing* input,
@@ -96,8 +118,9 @@ namespace icicle {
       PolyRing* output,
       size_t output_size)
     {
-      return CONCAT_EXPAND(ICICLE_FFI_PREFIX, recompose_from_balanced_digits)(
-        (const field_t*)input, input_size * PolyRing::d, base, &config, (field_t*)output, output_size * PolyRing::d);
+      return CONCAT_EXPAND(ICICLE_FFI_PREFIX, poly_ring_recompose_from_balanced_digits)(
+        input, input_size, base, &config, output, output_size);
     }
   } // namespace balanced_decomposition
+
 } // namespace icicle
