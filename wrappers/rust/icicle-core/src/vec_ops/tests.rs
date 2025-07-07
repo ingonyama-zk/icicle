6462c3cc2e46524191593d324544345a24b03065
nonam3e <timur@ingonyama.com>
2025-07-07 14:46:46 +0700

    move intermediate_host on heap

--- wrappers/rust/icicle-core/src/vec_ops/tests.rs
+++ wrappers/rust/icicle-core/src/vec_ops/tests.rs
@@ -401,7 +401,7 @@ where
     let cfg = VecOpsConfig::default();
     bit_reverse(input, &cfg, &mut intermediate[..]).unwrap();
 
-    let mut intermediate_host = [F::one(); TEST_SIZE];
+    let mut intermediate_host = vec![F::one(); TEST_SIZE];
     intermediate
         .copy_to_host(HostSlice::from_mut_slice(&mut intermediate_host[..]))
         .unwrap();
