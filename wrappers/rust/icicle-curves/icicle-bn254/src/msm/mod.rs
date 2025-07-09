a4ff1101e36333393f0d0cf40f1679acbe9fa3df
Jeremy Felder <jeremy.felder1@gmail.com>
2025-07-09 19:05:40 +0300

    [FEAT] Allow disabling features from rust bindings (#978)

--- wrappers/rust/icicle-curves/icicle-bn254/src/msm/mod.rs
+++ wrappers/rust/icicle-curves/icicle-bn254/src/msm/mod.rs
@@ -1,4 +1,5 @@
 use crate::curve::CurveCfg;
+#[cfg(feature = "g2")]
 use crate::curve::G2CurveCfg;
 use icicle_core::{
     curve::{Affine, Curve, Projective},
@@ -11,17 +12,20 @@ use icicle_runtime::{
 };
 
 impl_msm!("bn254", bn254, CurveCfg);
+#[cfg(feature = "g2")]
 impl_msm!("bn254_g2", bn254_g2, G2CurveCfg);
 
 #[cfg(test)]
 pub(crate) mod tests {
     use crate::curve::CurveCfg;
+    #[cfg(feature = "g2")]
     use crate::curve::G2CurveCfg;
     use icicle_core::impl_msm_tests;
     use icicle_core::msm::tests::*;
 
     impl_msm_tests!(CurveCfg);
 
+    #[cfg(feature = "g2")]
     mod g2 {
         use super::*;
         impl_msm_tests!(G2CurveCfg);
